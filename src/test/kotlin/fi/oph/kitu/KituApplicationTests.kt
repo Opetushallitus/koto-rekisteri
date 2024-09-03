ee5cd1a322155d2d119a0d0e80c6c10565446d9a
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-09-03 15:09:45 +0300

    feat: add kotlin

---
+++ src/test/kotlin/fi/oph/kitu/KituApplicationTests.kt
@@ -0,0 +1,13 @@
+package fi.oph.kitu
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KituKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
