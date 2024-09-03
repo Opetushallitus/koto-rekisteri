f7fb985a2cca7ee73c5d86dabd285272c1b2eade
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-09-03 12:41:04 +0300

    Generated code

---
+++ src/main/java/fi/oph/kitu/KituApplication.java
@@ -0,0 +1,13 @@
+package fi.oph.kitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KituApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KituApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/oph/kitu/KituApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.oph.kitu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KituApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
