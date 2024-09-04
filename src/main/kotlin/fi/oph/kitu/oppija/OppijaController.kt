677038cfa5b6e8c72aee8978c12bf183ad1f1afd
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2024-09-04 17:18:23 +0300

    Add Oppija controller

---
+++ src/main/kotlin/fi/oph/kitu/oppija/OppijaController.kt
@@ -0,0 +1,19 @@
+package fi.oph.kitu.oppija
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class OppijaController {
+    @Autowired
+    private lateinit var oppijaService: OppijaService
+
+    @GetMapping("/oppija")
+    fun getOppijat(): Iterable<Oppija> = oppijaService.getAll()
+
+    @PostMapping("/oppija")
+    fun addOppija(@RequestBody name: String) = oppijaService.insert(name)
+}
