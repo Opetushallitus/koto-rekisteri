4ff2e594423c744441f0eb7526fa9cb77e74fce3
Ilari Tommiska <ilari.tommiska@reaktor.com>
2024-09-05 13:42:47 +0300

    chore: reformat tabs to spaces

--- src/main/kotlin/fi/oph/kitu/oppija/OppijaRepository.kt
+++ src/main/kotlin/fi/oph/kitu/oppija/OppijaRepository.kt
@@ -7,31 +7,31 @@ import javax.sql.DataSource
 
 @Repository
 class OppijaRepository(dataSource: DataSource) : JdbcTemplate(dataSource) {
-	override fun afterPropertiesSet() {
-		super.afterPropertiesSet()
+    override fun afterPropertiesSet() {
+        super.afterPropertiesSet()
 
-		execute("DROP TABLE IF EXISTS oppija")
-		execute(
-			"""
+        execute("DROP TABLE IF EXISTS oppija")
+        execute(
+            """
             |CREATE TABLE oppija (
             |    id SERIAL PRIMARY KEY,
             |    name TEXT
             |)
             """.trimMargin()
-		)
-	}
+        )
+    }
 
-	fun getAll(): Iterable<Oppija> {
-		return query(
-			"SELECT id, name FROM oppija ORDER BY name, id",
-			RowMapper { rs, _ -> Oppija(rs.getLong("id"), rs.getString("name")) })
-	}
+    fun getAll(): Iterable<Oppija> {
+        return query(
+            "SELECT id, name FROM oppija ORDER BY name, id",
+            RowMapper { rs, _ -> Oppija(rs.getLong("id"), rs.getString("name")) })
+    }
 
-	fun insert(name: String): Oppija? {
-		return queryForObject(
-			"INSERT INTO oppija(name) VALUES (?) RETURNING id, name",
-			{ rs, _ -> Oppija(rs.getLong("id"), rs.getString("name")) },
-			name
-		)
-	}
+    fun insert(name: String): Oppija? {
+        return queryForObject(
+            "INSERT INTO oppija(name) VALUES (?) RETURNING id, name",
+            { rs, _ -> Oppija(rs.getLong("id"), rs.getString("name")) },
+            name
+        )
+    }
 }
