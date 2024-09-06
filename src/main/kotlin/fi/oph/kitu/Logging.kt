3a818d1a3f81bc8bc4ef2312122474419b7027b6
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-09-06 12:29:07 +0300

    feat: add an ugly logger instead

--- src/main/kotlin/fi/oph/kitu/Logging.kt
+++ src/main/kotlin/fi/oph/kitu/Logging.kt
@@ -1,31 +1,35 @@
 package fi.oph.kitu
 
+import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.filter.AbstractRequestLoggingFilter
+import org.springframework.web.filter.OncePerRequestFilter
 
 @Configuration
 class Logging {
     @Bean
     fun logFilter() =
-        object : AbstractRequestLoggingFilter() {
-            init {
-                isIncludeClientInfo = true
-                isIncludeQueryString = true
-            }
-
-            override fun beforeRequest(
+        object : OncePerRequestFilter() {
+            override fun doFilterInternal(
                 request: HttpServletRequest,
-                message: String,
+                response: HttpServletResponse,
+                filterChain: FilterChain,
             ) {
-            }
+                val start = System.currentTimeMillis()
+                val result = filterChain.doFilter(request, response).runCatching { }
+                val end = System.currentTimeMillis()
 
-            override fun afterRequest(
-                request: HttpServletRequest,
-                message: String,
-            ) {
-                servletContext.log("${request.method} ${request.requestURI}: $message")
+                result.fold(
+                    onSuccess = {
+                        logger.info("${request.method} ${request.requestURI}: duration=${end - start}ms status_code=${response.status}")
+                    },
+                    onFailure = fun (err) {
+                        logger.info("${request.method} ${request.requestURI}: duration=${end - start}ms exception=${err.message}")
+                        throw err
+                    },
+                )
             }
         }
 }
