cb6d3ff861450efc4b31ea3f85e86b3b0bc48e4f
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-09-10 15:22:03 +0300

    feat: include statically shared node files in maven build (mvn package)

--- src/main/frontend/next.config.mjs
+++ src/main/frontend/next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+  output: "export",
+
+  // The build fails without this.
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
