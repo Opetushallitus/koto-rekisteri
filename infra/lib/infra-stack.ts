3d94baf15ab96bc473cca307600a6ffd3ec8f0c5
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-09-11 09:36:50 +0300

    feat: create VPC and subnets

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,8 +1,19 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+
+export interface InfraStackProps extends cdk.StackProps {
+  cidrBlock: string;
+  maxAzs: number;
+}
 
 export class InfraStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: InfraStackProps) {
     super(scope, id, props);
+
+    const vpc = new ec2.Vpc(this, "Vpc", {
+      ipAddresses: ec2.IpAddresses.cidr(props.cidrBlock),
+      maxAzs: props.maxAzs,
+    });
   }
 }
