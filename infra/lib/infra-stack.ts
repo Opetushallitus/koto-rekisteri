d61958dc9b15a25e946ec0531fe54bc5c7baf81d
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-09-11 09:47:20 +0300

    feat: add GitHub Actions OIDC provider

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as ec2 from "aws-cdk-lib/aws-ec2";
+import { GithubActionsStack } from "./github-actions-stack";
 
 export interface InfraStackProps extends cdk.StackProps {
   cidrBlock: string;
@@ -10,6 +11,14 @@ export interface InfraStackProps extends cdk.StackProps {
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: InfraStackProps) {
     super(scope, id, props);
+
+    const githubActionsStack = new GithubActionsStack(
+      this,
+      "GithubActionsStack",
+      {
+        env: props.env,
+      },
+    );
 
     const vpc = new ec2.Vpc(this, "Vpc", {
       ipAddresses: ec2.IpAddresses.cidr(props.cidrBlock),
