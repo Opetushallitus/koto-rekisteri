1c7ccb6c4d47e40ab85275ed87e994e98df3474e
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-09-11 09:36:50 +0300

    feat: create VPC and subnets

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -3,12 +3,30 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { InfraStack } from "../lib/infra-stack";
 
+// CIDR allocation strategy:
+// Top: 10.15.0.0/16
+// VPCs: 10.15.0.0/18, 10.15.64.0/18, 10.15.128.0/18, 10.15.192.0/18 (16382 addresses)
+// Subnets: ...
+
 const accounts = {
-  dev: { account: "682033502734", region: "eu-west-1" },
+  dev: {
+    account: "682033502734",
+    region: "eu-west-1",
+  },
+  network: {
+    cidrs: {
+      dev: "10.15.0.0/18",
+    },
+    maxAzs: {
+      dev: 2,
+    },
+  },
 };
 
 const app = new cdk.App();
 
-new InfraStack(app, "InfraStack", {
+const devStack = new InfraStack(app, "InfraStack", {
   env: accounts.dev,
+  cidrBlock: accounts.network.cidrs.dev,
+  maxAzs: accounts.network.maxAzs.dev,
 });
