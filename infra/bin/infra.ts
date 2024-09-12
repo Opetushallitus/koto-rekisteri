27dcb9e2085cf32b4934b987acc6762443fd3d28
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-09-12 10:09:27 +0300

    feat(infra): parametrize environment

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -8,25 +8,45 @@ import { InfraStack } from "../lib/infra-stack";
 // VPCs: 10.15.0.0/18, 10.15.64.0/18, 10.15.128.0/18, 10.15.192.0/18 (16382 addresses)
 // Subnets: (let AWS calculate these for us)
 
-const accounts = {
+const environments = {
   dev: {
     account: "682033502734",
     region: "eu-west-1",
-  },
-  network: {
-    cidrs: {
-      dev: "10.15.0.0/18",
+    network: {
+      cidr: "10.15.0.0/18",
+      maxAzs: 2,
     },
-    maxAzs: {
-      dev: 2,
+  },
+  test: {
+    account: "961341546901",
+    region: "eu-west-1",
+    network: {
+      cidr: "10.15.64.0/18",
+      maxAzs: 3,
     },
   },
 };
 
+type EnvName = keyof typeof environments;
+
 const app = new cdk.App();
 
-const devStack = new InfraStack(app, "InfraStack", {
-  env: accounts.dev,
-  cidrBlock: accounts.network.cidrs.dev,
-  maxAzs: accounts.network.maxAzs.dev,
+const envName = process.env.KITU_ENV;
+
+if (envName === undefined) {
+  throw new Error("KITU_ENV required");
+}
+
+if (environments[envName as EnvName] === undefined) {
+  throw new Error(
+    `KITU_ENV invalid value ${envName}, expected one of ${Object.keys(environments).join(", ")}`,
+  );
+}
+
+const env = environments[envName as EnvName];
+
+new InfraStack(app, "InfraStack", {
+  env,
+  cidrBlock: env.network.cidr,
+  maxAzs: env.network.maxAzs,
 });
