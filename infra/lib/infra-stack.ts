c963cf5c6c523236c322d1314c28b01360220783
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-09-16 16:25:24 +0300

    fix: add DNS records
    # Conflicts:
    #	infra/lib/infra-stack.ts

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,16 +1,19 @@
 import * as cdk from "aws-cdk-lib"
-import {
-  aws_certificatemanager,
-  aws_cloudfront,
-  aws_cloudfront_origins,
-} from "aws-cdk-lib"
 import { Construct } from "constructs"
 import * as ec2 from "aws-cdk-lib/aws-ec2"
 import * as ecsPatterns from "aws-cdk-lib/aws-ecs-patterns"
 import * as ecs from "aws-cdk-lib/aws-ecs"
 import { GithubActionsStack } from "./github-actions-stack"
+import {
+  aws_certificatemanager,
+  aws_cloudfront,
+  aws_cloudfront_origins,
+  aws_route53,
+} from "aws-cdk-lib"
 import { Platform } from "aws-cdk-lib/aws-ecr-assets"
 import { ViewerProtocolPolicy } from "aws-cdk-lib/aws-cloudfront"
+import { RecordTarget } from "aws-cdk-lib/aws-route53"
+import { CloudFrontTarget } from "aws-cdk-lib/aws-route53-targets"
 
 export interface InfraStackProps extends cdk.StackProps {
   certificate: aws_certificatemanager.ICertificate
@@ -62,7 +65,7 @@ export class InfraStack extends cdk.Stack {
       path: "/actuator/health",
     })
 
-    new aws_cloudfront.Distribution(this, "KotoCfn", {
+    const distribution = new aws_cloudfront.Distribution(this, "KotoCfn", {
       defaultBehavior: {
         origin: new aws_cloudfront_origins.LoadBalancerV2Origin(
           service.loadBalancer,
@@ -72,6 +75,25 @@ export class InfraStack extends cdk.Stack {
       },
       domainNames: [props.domainName],
       certificate: props.certificate,
+    })
+
+    const zone = aws_route53.HostedZone.fromLookup(this, "Zone", {
+      domainName: props.domainName,
+    })
+
+    const recordTarget = RecordTarget.fromAlias(
+      new CloudFrontTarget(distribution),
+    )
+
+    new aws_route53.ARecord(this, "RecordIpV4", {
+      recordName: props.domainName,
+      target: recordTarget,
+      zone,
+    })
+    new aws_route53.AaaaRecord(this, "RecordIpV6", {
+      recordName: props.domainName,
+      target: recordTarget,
+      zone,
     })
   }
 }
