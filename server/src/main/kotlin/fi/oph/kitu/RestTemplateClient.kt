550b92ecbca6adf33617aeaedb1388baacbc78d6
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-09-18 17:48:18 +0300

    feat: HTTP GET yleistunniste/hae

---
+++ server/src/main/kotlin/fi/oph/kitu/RestTemplateClient.kt
@@ -0,0 +1,11 @@
+package fi.oph.kitu
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestTemplate
+
+@Configuration
+class RestTemplateClient {
+    @Bean
+    fun restTemplate(): RestTemplate = RestTemplate()
+}
--- server/src/main/kotlin/fi/oph/kitu/oppija/OppijaController.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppija/OppijaController.kt
@@ -1,17 +1,19 @@
 package fi.oph.kitu.oppija
 
 import fi.oph.kitu.generated.api.OppijaControllerApi
-import org.springframework.beans.factory.annotation.Autowired
+import fi.oph.kitu.oppijanumerorekisteri.YleistunnisteHaeRequest
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.RestTemplate
 
 @RestController
-class OppijaController : OppijaControllerApi {
-    @Autowired
-    private lateinit var oppijaService: OppijaService
-
+class OppijaController(
+    private val restTemplate: RestTemplate,
+    private val oppijaService: OppijaService,
+) : OppijaControllerApi {
     override fun getOppijat(): ResponseEntity<List<Oppija>> =
         ResponseEntity(oppijaService.getAll().toList(), HttpStatus.OK)
 
@@ -24,4 +26,29 @@ class OppijaController : OppijaControllerApi {
             if (inserted != null) HttpStatus.CREATED else HttpStatus.BAD_REQUEST,
         )
     }
+
+    @GetMapping("/api/oppija/oppijanumero")
+    fun getOppijanumero(): String {
+        val virkailijaUrl = "https://virkailija.testiopintopolku.fi/yleistunniste/hae"
+        val request =
+            YleistunnisteHaeRequest(
+                "Magdalena Testi",
+                "010866-9260",
+                "Magdalena",
+                "Sallinen-Testi",
+            )
+
+        val response =
+            restTemplate
+                .postForEntity(virkailijaUrl, request, String::class.java)
+
+        return response.body ?: "empty body"
+    }
 }
+
+data class YleistunnisteHaeRequest(
+    val etunimet: String,
+    val hetu: String,
+    val kutsumanimi: String,
+    val sukunimi: String,
+)
