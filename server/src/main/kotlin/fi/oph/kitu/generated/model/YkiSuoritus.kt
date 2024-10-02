da2a9ea335c9907cda1f8d16a9e1eb327f27c895
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-02 16:26:35 +0300

    feat(yki): add järjestäjä to open api model

--- server/src/main/kotlin/fi/oph/kitu/generated/model/YkiSuoritus.kt
+++ server/src/main/kotlin/fi/oph/kitu/generated/model/YkiSuoritus.kt
@@ -5,14 +5,14 @@ import com.fasterxml.jackson.annotation.JsonProperty
 /**
  *
  * @param osallistuja
- * @param tutkinnonSuorittamispaikka
+ * @param jarjestaja
  * @param tutkintokieli
  * @param saadutTaitotasoarviot
  * @param tutkintokertojenAjankohta
  */
 data class YkiSuoritus(
     @get:JsonProperty("osallistuja") val osallistuja: YkiOsallistuja? = null,
-    @get:JsonProperty("tutkinnonSuorittamispaikka") val tutkinnonSuorittamispaikka: kotlin.String? = null,
+    @get:JsonProperty("jarjestaja") val jarjestaja: YkiJarjestaja? = null,
     @get:JsonProperty("tutkintokieli") val tutkintokieli: kotlin.String? = null,
     @get:JsonProperty("saadutTaitotasoarviot") val saadutTaitotasoarviot: kotlin.String? = null,
     @get:JsonProperty("tutkintokertojenAjankohta") val tutkintokertojenAjankohta: kotlin.String? = null,
