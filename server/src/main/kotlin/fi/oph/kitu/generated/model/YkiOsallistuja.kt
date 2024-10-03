35d91fe347bb24a091ab712e6b95e1f8ddb45cb0
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-03 12:38:54 +0300

    feat(yki): more fields

--- server/src/main/kotlin/fi/oph/kitu/generated/model/YkiOsallistuja.kt
+++ server/src/main/kotlin/fi/oph/kitu/generated/model/YkiOsallistuja.kt
@@ -1,19 +1,21 @@
 package fi.oph.kitu.generated.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 
 /**
  *
  * @param oid
  * @param sukunimi
  * @param etunimi
- * @param henkilotunnus
+ * @param henkilotunnus Suomalainen henkilötunnus
  * @param kansalaisuus
  * @param sukupuoli N=Nainen, M=Mies, E=muu/ei ilmoiteta
  * @param katuosoite Osallistujan tarpeellisten yhteystietojen katuosoite
  * @param postinumero Osallistujan tarpeellisten yhteystietojen postinumero
  * @param postitoimipaikka Osallistujan tarpeellisten yhteystietojen postitoimipaikka
- * @param sähköpostiosoite Osallistujan tarpeellisten yhteystietojen sähköpostiosoite
+ * @param sahköpostiosoite Osallistujan tarpeellisten yhteystietojen sähköpostiosoite
  */
 data class YkiOsallistuja(
     @get:JsonProperty("oid") val oid: kotlin.String? = null,
@@ -21,9 +23,28 @@ data class YkiOsallistuja(
     @get:JsonProperty("etunimi") val etunimi: kotlin.String? = null,
     @get:JsonProperty("henkilotunnus") val henkilotunnus: kotlin.String? = null,
     @get:JsonProperty("kansalaisuus") val kansalaisuus: kotlin.String? = null,
-    @get:JsonProperty("sukupuoli") val sukupuoli: kotlin.String? = null,
+    @get:JsonProperty("sukupuoli") val sukupuoli: YkiOsallistuja.Sukupuoli? = null,
     @get:JsonProperty("katuosoite") val katuosoite: kotlin.String? = null,
     @get:JsonProperty("postinumero") val postinumero: kotlin.String? = null,
     @get:JsonProperty("postitoimipaikka") val postitoimipaikka: kotlin.String? = null,
-    @get:JsonProperty("sähköpostiosoite") val sähköpostiosoite: kotlin.String? = null,
-)
+    @get:JsonProperty("sahköpostiosoite") val sahköpostiosoite: kotlin.String? = null,
+) {
+    /**
+     * N=Nainen, M=Mies, E=muu/ei ilmoiteta
+     * Values: N,M,E
+     */
+    enum class Sukupuoli(
+        @get:JsonValue val value: kotlin.String,
+    ) {
+        N("N"),
+        M("M"),
+        E("E"),
+        ;
+
+        companion object {
+            @JvmStatic
+            @JsonCreator
+            fun forValue(value: kotlin.String): Sukupuoli = values().first { it -> it.value == value }
+        }
+    }
+}
--- server/src/main/kotlin/fi/oph/kitu/generated/model/YkiSuoritus.kt
+++ server/src/main/kotlin/fi/oph/kitu/generated/model/YkiSuoritus.kt
@@ -1,19 +1,56 @@
 package fi.oph.kitu.generated.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 
 /**
  *
  * @param osallistuja
  * @param jarjestajaOid
- * @param tutkintokieli
- * @param saadutTaitotasoarviot
- * @param tutkintokertojenAjankohta
+ * @param tutkintokieli ISO 639-3 - koodattu kieli, jota kielitutkinnossa testataan.
+ * @param tekstinYmmartamisenArvosana Tekstin ymmärtämisen arvosana
+ * @param kirjoittamisenArvosana Kirjoittamisen arvosana
+ * @param rakenteidenJaSanastonArvosana Rakenteiden ja sanaston arvosana
+ * @param puheenYmmartamisenArvosana Puheen ymmärtämisen arvosana
+ * @param puhumisenArvosana Puhumisen arvosana
+ * @param yleistasoarvio Yleistasoarvio
+ * @param pvm Suorituksen päivämäärä
  */
 data class YkiSuoritus(
     @get:JsonProperty("osallistuja") val osallistuja: YkiOsallistuja? = null,
     @get:JsonProperty("jarjestajaOid") val jarjestajaOid: YkiJarjestaja? = null,
-    @get:JsonProperty("tutkintokieli") val tutkintokieli: kotlin.String? = null,
-    @get:JsonProperty("saadutTaitotasoarviot") val saadutTaitotasoarviot: kotlin.String? = null,
-    @get:JsonProperty("tutkintokertojenAjankohta") val tutkintokertojenAjankohta: kotlin.String? = null,
-)
+    @get:JsonProperty("tutkintokieli") val tutkintokieli: YkiSuoritus.Tutkintokieli? = null,
+    @get:JsonProperty("tekstinYmmartamisenArvosana") val tekstinYmmartamisenArvosana: kotlin.String? = null,
+    @get:JsonProperty("kirjoittamisenArvosana") val kirjoittamisenArvosana: kotlin.String? = null,
+    @get:JsonProperty("rakenteidenJaSanastonArvosana") val rakenteidenJaSanastonArvosana: kotlin.String? = null,
+    @get:JsonProperty("puheenYmmartamisenArvosana") val puheenYmmartamisenArvosana: kotlin.String? = null,
+    @get:JsonProperty("puhumisenArvosana") val puhumisenArvosana: kotlin.String? = null,
+    @get:JsonProperty("yleistasoarvio") val yleistasoarvio: kotlin.String? = null,
+    @get:JsonProperty("pvm") val pvm: java.time.LocalDate? = null,
+) {
+    /**
+     * ISO 639-3 - koodattu kieli, jota kielitutkinnossa testataan.
+     * Values: DEU,ENG,FIN,FRA,ITA,RUS,SME,SPA,SWE
+     */
+    enum class Tutkintokieli(
+        @get:JsonValue val value: kotlin.String,
+    ) {
+        DEU("DEU"),
+        ENG("ENG"),
+        FIN("FIN"),
+        FRA("FRA"),
+        ITA("ITA"),
+        RUS("RUS"),
+        SME("SME"),
+        SPA("SPA"),
+        SWE("SWE"),
+        ;
+
+        companion object {
+            @JvmStatic
+            @JsonCreator
+            fun forValue(value: kotlin.String): Tutkintokieli = values().first { it -> it.value == value }
+        }
+    }
+}
