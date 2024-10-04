a728f057c1411ed86e0c7f786ff9ada48fad5056
Ilari Tommiska <ilari.tommiska@reaktor.com>
2024-10-04 10:49:12 +0300

    chore: example schedule via properties

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiScheduledTasks.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiScheduledTasks.kt
@@ -3,17 +3,20 @@ package fi.oph.kitu.yki
 import com.github.kagkarlsson.scheduler.task.Task
 import com.github.kagkarlsson.scheduler.task.helper.Tasks
 import com.github.kagkarlsson.scheduler.task.schedule.Schedules
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.time.LocalTime
 
 @Configuration
 @ConditionalOnProperty(name = ["kitu.yki.scheduling.enabled"], matchIfMissing = false)
 class YkiScheduledTasks {
+    @Value("\${kitu.yki.scheduling.import.schedule}")
+    lateinit var ykiImportSchedule: String
+
     @Bean
     fun dailyImport(ykiService: YkiService): Task<Void> =
         Tasks
-            .recurring("YKI-import-daily", Schedules.daily(LocalTime.of(3, 0)))
+            .recurring("YKI-import", Schedules.parseSchedule(ykiImportSchedule))
             .execute { _, _ -> ykiService.importYkiSuoritukset() }
 }
