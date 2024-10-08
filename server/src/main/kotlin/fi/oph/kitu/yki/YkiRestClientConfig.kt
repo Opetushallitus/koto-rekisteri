c13e47a6a25de4742de32f1cd7d048945c62bcbd
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-08 15:36:35 +0300

    feat(yki): convert mock client to proper client
    but still use the mock api

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiRestClientConfig.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiRestClientConfig.kt
@@ -1,15 +1,19 @@
 package fi.oph.kitu.yki
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
 
 @Configuration
-class MockClientConfig {
-    @Bean("mockRestClient")
+class YkiRestClientConfig {
+    @Value("\${kitu.yki.baseUrl}")
+    lateinit var baseUrl: String
+
+    @Bean("ykiRestClient")
     fun restClient(): RestClient =
         RestClient
             .builder()
-            .baseUrl("http://localhost:8080/dev/mock/")
+            .baseUrl(baseUrl)
             .build()
 }
