4cd4591b83c90e239fd40dc16873829fad42a6ff
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-08 15:39:10 +0300

    feat(yki): insert yki suoritus to yki repository

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiService.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiService.kt
@@ -1,15 +1,35 @@
 package fi.oph.kitu.yki
 
+import fi.oph.kitu.generated.model.YkiSuoritus
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.body
 
 @Service
-class YkiService {
+class YkiService(
+    @Qualifier("ykiRestClient")
+    private val ykiRestClient: RestClient,
+    private val repository: YkiRepository,
+) {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     fun importYkiSuoritukset() {
-        logger.info("Running import for \"YKI suoritukset\"")
-        // TODO: the implementation goes here
+        val suoritus =
+            ykiRestClient
+                .get()
+                .uri("/yki")
+                .retrieve()
+                .body<YkiSuoritus>()
+
+        if (suoritus == null) {
+            logger.info("No YKI suoritus found")
+            return
+        }
+
+        repository.insertSuoritus(suoritus)
+        logger.info("YKI Suoritus added to repository")
     }
 }
