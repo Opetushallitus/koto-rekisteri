721616b2dcdeb5c4b0d2d71d507b712daa9e4e2a
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-14 16:32:43 +0300

    feat(yki): generate trigger controller for testing the import

--- server/src/main/kotlin/fi/oph/kitu/Utils.kt
+++ server/src/main/kotlin/fi/oph/kitu/Utils.kt
@@ -2,4 +2,4 @@ package fi.oph.kitu
 
 import org.springframework.web.client.RestClient
 
-fun RestClient.ResponseSpec.csvBody(columnSeparator: String = ","): List<List<String>>? = csv
+fun RestClient.ResponseSpec.csvBody(columnSeparator: String = ","): List<List<String>>? = TODO("todo") // csv
--- server/src/main/kotlin/fi/oph/kitu/dev/MockYkiControllerApi.kt
+++ server/src/main/kotlin/fi/oph/kitu/dev/MockYkiControllerApi.kt
@@ -1,6 +1,7 @@
 package fi.oph.kitu.dev
 
 import fi.oph.kitu.generated.api.YkiControllerApi
+import fi.oph.kitu.yki.YkiService
 import jakarta.annotation.PostConstruct
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -9,18 +10,17 @@ import org.springframework.context.annotation.Profile
 import org.springframework.core.env.Environment
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.context.WebApplicationContext
 import java.time.LocalDate
 import kotlin.system.exitProcess
 
 @RestController
-@RequestMapping("/dev/mock/yki")
 @Profile("local", "e2e")
 class MockYkiControllerApi(
     private val environment: Environment,
     private val applicationContext: WebApplicationContext,
+    private val ykiService: YkiService,
 ) : YkiControllerApi {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
@@ -43,4 +43,9 @@ class MockYkiControllerApi(
             """"1.2.246.562.24.99999999999","Suorittaja","Sulevi",2022-11-12,"fin","KT","1.2.246.562.10.373218511910","Iisalmen kansalaisopisto",2,2,1,3,2,2""",
             HttpStatus.OK,
         )
+
+    override fun triggerSchedule(lastSeen: LocalDate?): ResponseEntity<Unit> {
+        val unit = ykiService.importYkiSuoritukset()
+        return ResponseEntity(unit, HttpStatus.OK)
+    }
 }
