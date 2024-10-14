9dde19050a98cb5559bc253e909e9568f886b22d
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-14 11:58:37 +0300

    wip: start working on with jackson-dataformat-csv

--- server/src/main/kotlin/fi/oph/kitu/Utils.kt
+++ server/src/main/kotlin/fi/oph/kitu/Utils.kt
@@ -1,21 +1,5 @@
 package fi.oph.kitu
 
 import org.springframework.web.client.RestClient
-import org.springframework.web.client.body
 
-fun RestClient.ResponseSpec.csvBody(columnSeparator: String = ","): List<List<String>>? {
-    val response = this.body<String>()
-    if (response.isNullOrBlank()) {
-        return null
-    }
-
-    val lines = response.lines()
-    val csv = mutableListOf<List<String>>()
-
-    for (line in lines) {
-        val column = line.split(columnSeparator)
-        csv.add(column)
-    }
-
-    return csv
-}
+fun RestClient.ResponseSpec.csvBody(columnSeparator: String = ","): List<List<String>>? = csv
