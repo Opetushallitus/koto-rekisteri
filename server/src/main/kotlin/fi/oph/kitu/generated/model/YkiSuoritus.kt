8778678b987ae28a6485fac4d7b0a994f098636c
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-14 16:11:07 +0300

    feat(yki): generate model
    based on solki API description ver 1

--- server/src/main/kotlin/fi/oph/kitu/generated/model/YkiSuoritus.kt
+++ server/src/main/kotlin/fi/oph/kitu/generated/model/YkiSuoritus.kt
@@ -1,25 +1,83 @@
 package fi.oph.kitu.generated.model
 
+import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 
 /**
  *
- * @param osallistuja
- * @param jarjestajaOid
+ * @param suorittajanOid
+ * @param sukunimi
+ * @param etunimet
+ * @param tutkintopaiva
+ * @param tutkintokieli ISO 639-3 - koodattu kieli, jota kielitutkinnossa testataan.
+ * @param tutkintotaso Arvioitava taso. PT = perustaso. Vastaa CEFR A1/A2 KT = keskitaso. Vastaa CEFR B1/B2 YT = Ylin taso. Vastaa CEFR C1/C2
+ * @param jarjestajanOid
+ * @param jarjestajanNimi
  * @param tekstinYmmartamisenArvosana
  * @param kirjoittamisenArvosana
  * @param rakenteidenJaSanastonArvosana
  * @param puheenYmmartamisenArvosana
  * @param puhumisenArvosana
- * @param yleistasoarvio
+ * @param yleisarvosana
  */
 data class YkiSuoritus(
-    @get:JsonProperty("osallistuja") val osallistuja: YkiOsallistuja? = null,
-    @get:JsonProperty("jarjestajaOid") val jarjestajaOid: YkiJarjestaja? = null,
-    @get:JsonProperty("tekstinYmmartamisenArvosana") val tekstinYmmartamisenArvosana: YkiArviointi? = null,
-    @get:JsonProperty("kirjoittamisenArvosana") val kirjoittamisenArvosana: YkiArviointi? = null,
-    @get:JsonProperty("rakenteidenJaSanastonArvosana") val rakenteidenJaSanastonArvosana: YkiArviointi? = null,
-    @get:JsonProperty("puheenYmmartamisenArvosana") val puheenYmmartamisenArvosana: YkiArviointi? = null,
-    @get:JsonProperty("puhumisenArvosana") val puhumisenArvosana: YkiArviointi? = null,
-    @get:JsonProperty("yleistasoarvio") val yleistasoarvio: YkiArviointi? = null,
-)
+    @get:JsonProperty("suorittajanOid") val suorittajanOid: kotlin.String? = null,
+    @get:JsonProperty("sukunimi") val sukunimi: kotlin.String? = null,
+    @get:JsonProperty("etunimet") val etunimet: kotlin.String? = null,
+    @get:JsonProperty("tutkintopaiva") val tutkintopaiva: kotlin.String? = null,
+    @get:JsonProperty("tutkintokieli") val tutkintokieli: YkiSuoritus.Tutkintokieli? = null,
+    @get:JsonProperty("tutkintotaso") val tutkintotaso: YkiSuoritus.Tutkintotaso? = null,
+    @get:JsonProperty("jarjestajanOid") val jarjestajanOid: kotlin.String? = null,
+    @get:JsonProperty("jarjestajanNimi") val jarjestajanNimi: kotlin.String? = null,
+    @get:JsonProperty("tekstinYmmartamisenArvosana") val tekstinYmmartamisenArvosana: java.math.BigDecimal? = null,
+    @get:JsonProperty("kirjoittamisenArvosana") val kirjoittamisenArvosana: java.math.BigDecimal? = null,
+    @get:JsonProperty("rakenteidenJaSanastonArvosana") val rakenteidenJaSanastonArvosana: java.math.BigDecimal? = null,
+    @get:JsonProperty("puheenYmmartamisenArvosana") val puheenYmmartamisenArvosana: java.math.BigDecimal? = null,
+    @get:JsonProperty("puhumisenArvosana") val puhumisenArvosana: java.math.BigDecimal? = null,
+    @get:JsonProperty("yleisarvosana") val yleisarvosana: java.math.BigDecimal? = null,
+) {
+    /**
+     * ISO 639-3 - koodattu kieli, jota kielitutkinnossa testataan.
+     * Values: DEU,ENG,FIN,FRA,ITA,RUS,SME,SPA,SWE
+     */
+    enum class Tutkintokieli(
+        @get:JsonValue val value: kotlin.String,
+    ) {
+        DEU("DEU"),
+        ENG("ENG"),
+        FIN("FIN"),
+        FRA("FRA"),
+        ITA("ITA"),
+        RUS("RUS"),
+        SME("SME"),
+        SPA("SPA"),
+        SWE("SWE"),
+        ;
+
+        companion object {
+            @JvmStatic
+            @JsonCreator
+            fun forValue(value: kotlin.String): Tutkintokieli = values().first { it -> it.value == value }
+        }
+    }
+
+    /**
+     * Arvioitava taso. PT = perustaso. Vastaa CEFR A1/A2 KT = keskitaso. Vastaa CEFR B1/B2 YT = Ylin taso. Vastaa CEFR C1/C2
+     * Values: PT,KT,YT
+     */
+    enum class Tutkintotaso(
+        @get:JsonValue val value: kotlin.String,
+    ) {
+        PT("PT"),
+        KT("KT"),
+        YT("YT"),
+        ;
+
+        companion object {
+            @JvmStatic
+            @JsonCreator
+            fun forValue(value: kotlin.String): Tutkintotaso = values().first { it -> it.value == value }
+        }
+    }
+}
