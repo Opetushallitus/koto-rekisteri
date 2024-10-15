e5b14674520e351ff7ba2dfaf69d53d92b72abc5
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-15 10:54:00 +0300

    feat(yki): use data class ver2

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiService.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiService.kt
@@ -1,6 +1,8 @@
 package fi.oph.kitu.yki
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import fi.oph.kitu.csvBody
+import fi.oph.kitu.generated.model.YkiSuoritus
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
@@ -21,15 +23,48 @@ class YkiService(
                 .get()
                 .uri("/yki")
                 .retrieve()
-                .csvBody()
-                ?.map { Mappers.toYkiSuoritus(it) }
+                .csvBody<Suoritus>()
 
         if (suoritukset == null) {
             logger.info("No YKI suoritukset found")
             return
         }
 
-        repository.insertSuoritukset(suoritukset)
+        val repData: List<YkiSuoritus> = throw NotImplementedError()
+        repository.insertSuoritukset(repData)
         logger.info("YKI Suoritukset was added to repository")
     }
+
+    @JsonPropertyOrder(
+        "suorittajanOppijanumero",
+        "sukunimi",
+        "etunimet",
+        "tutkintopaiva",
+        "tutkintokieli",
+        "tutkintotaso",
+        "jarjestajanTunnusOid",
+        "jarjestajanNimi",
+        "tekstinYmmartaminen",
+        "kirjoittaminen",
+        "rakenteetJaSanasto",
+        "puheenYmmartaminen",
+        "puhuminen",
+        "yleisarvosana",
+    )
+    data class Suoritus(
+        val suorittajanOppijanumero: String,
+        val sukunimi: String,
+        val etunimet: String,
+        val tutkintopaiva: String, // ISO-8601-muodossa
+        val tutkintokieli: String, // ISO 649-2 alpha-3 -muodossa
+        val tutkintotaso: String, // ("PT"=perustaso, "KT"=keskitaso, "YT"=ylin taso)
+        val jarjestajanTunnusOid: String,
+        val jarjestajanNimi: String,
+        val tekstinYmmartaminen: Number,
+        val kirjoittaminen: Number,
+        val rakenteetJaSanasto: Number,
+        val puheenYmmartaminen: Number,
+        val puhuminen: Number,
+        val yleisarvosana: Number,
+    )
 }
