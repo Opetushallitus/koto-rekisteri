5ceac6ff71eb79f940cdd6691b809b49deace14c
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-17 13:56:44 +0300

    fix: create script

--- server/src/main/kotlin/fi/oph/kitu/yki/db/migration/V4__create_yki_table.sql
+++ server/src/main/kotlin/fi/oph/kitu/yki/db/migration/V4__create_yki_table.sql
@@ -1,18 +1,21 @@
+CREATE DOMAIN DATE_ISO649_ALPHA3 AS CHAR(3) CHECK (VALUE ~ '^[A-Z]{3}$');
+CREATE TYPE yki_tutkintotaso AS ENUM ('PT', 'KT', 'YT'); -- ("PT"=perustaso, "KT"=keskitaso, "YT"=ylin taso)
+
 CREATE TABLE yki_suoritus
 (
     id                      SERIAL PRIMARY KEY,
-    suorittajanOppijanumero TEXT    NOT NULL,
-    sukunimi                TEXT    NOT NULL,
-    etunimet                TEXT    NOT NULL,
-    tutkintopaiva           DATE    NOT NULL,                                           -- ISO-8601-muodossa
-    tutkintokieli           CHAR(3) NOT NULL CHECK(tutkintokieli ~ '^[A-Z]{3}$'),       -- ISO 649-2 alpha-3 -muodossa
-    tutkintotaso            CHAR(2) NOT NULL CHECK (tutkintotaso IN ('PT', 'KT', 'YT'), -- ("PT"=perustaso, "KT"=keskitaso, "YT"=ylin taso)
-    jarjestajanTunnusOid    TEXT    NOT NULL,
+    suorittajanOppijanumero TEXT                NOT NULL,
+    sukunimi                TEXT                NOT NULL,
+    etunimet                TEXT                NOT NULL,
+    tutkintopaiva           DATE                NOT NULL,
+    tutkintokieli           DATE_ISO649_ALPHA3  NOT NULL,
+    tutkintotaso            YKI_TUTKINTOTASO    NOT NULL,
+    jarjestajanTunnusOid    TEXT                NOT NULL,
     jarjestajanNimi         TEXT,
     tekstinYmmartaminen     FLOAT,
     kirjoittaminen          FLOAT,
     rakenteetJaSanasto      FLOAT,
     puheenYmmartaminen      FLOAT,
     puhuminen               FLOAT,
-    yleisarvosana           FLOAT,
+    yleisarvosana           FLOAT
 )
