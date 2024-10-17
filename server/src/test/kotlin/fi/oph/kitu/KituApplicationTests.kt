f660f5f3a9f8da3ba98617f8a5353c9750d784ba
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2024-10-17 16:27:33 +0300

    Remove Oppija controller, repository and service

--- server/src/test/kotlin/fi/oph/kitu/KituApplicationTests.kt
+++ server/src/test/kotlin/fi/oph/kitu/KituApplicationTests.kt
@@ -1,35 +1,12 @@
 package fi.oph.kitu
 
-import fi.oph.kitu.oppija.Oppija
-import fi.oph.kitu.oppija.OppijaService
 import fi.oph.kitu.test.DBFixture
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import kotlin.test.assertContentEquals
 
 @SpringBootTest
 class KituApplicationTests : DBFixture() {
-    @Autowired
-    private lateinit var oppijaService: OppijaService
-
     @Test
     fun contextLoads() {
-    }
-
-    @Test
-    fun dbConnectionIsAvailable() {
-        oppijaService.insert("Firstname Lastname")
-        oppijaService.insert("Somebody Else")
-        oppijaService.insert("Another One")
-
-        val actual = oppijaService.getAll()
-        val expected =
-            listOf(
-                Oppija(3L, "Another One"),
-                Oppija(1L, "Firstname Lastname"),
-                Oppija(2L, "Somebody Else"),
-            )
-        assertContentEquals(expected, actual)
     }
 }
