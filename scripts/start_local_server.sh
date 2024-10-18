73b592817544d61eb8074be145fdcb120a8ea3d3
Ilari Tommiska <ilari.tommiska@reaktor.com>
2024-10-18 11:52:32 +0300

    chore: fail server startup if missing required env

--- scripts/start_local_server.sh
+++ scripts/start_local_server.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 source "$( dirname "${BASH_SOURCE[0]}" )/common-functions.sh"
 
 require_command humanlog
+require_env SPRING_PROFILES_ACTIVE
+require_env KOTLIN_POST_PROCESS_FILE
 
 (
   cd "$REPO_ROOT"/server
