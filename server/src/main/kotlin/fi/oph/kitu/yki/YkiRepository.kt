9824c0ac9fecce31df72c9cdbbef01f7200d0cf4
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-18 14:22:54 +0300

    feat(yki): convert repository to CrudRepository

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiRepository.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiRepository.kt
@@ -1,85 +1,8 @@
 package fi.oph.kitu.yki
 
-import org.springframework.jdbc.core.JdbcTemplate
+import fi.oph.kitu.yki.entities.YkiSuoritusEntity
+import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
-import javax.sql.DataSource
 
 @Repository
-class YkiRepository(
-    dataSource: DataSource,
-) : JdbcTemplate(dataSource) {
-    fun insertSuoritukset(suoritukset: List<YkiSuoritus>): List<YkiSuoritus> =
-        suoritukset.mapNotNull { insertSuoritus(it) }
-
-    fun insertSuoritus(suoritus: YkiSuoritus): YkiSuoritus? =
-        queryForObject(
-            """
-            INSERT INTO yki_suoritus(
-                suorittajanOppijanumero,
-                sukunimi,
-                etunimet,
-                tutkintopaiva,
-                tutkintokieli,
-                tutkintotaso,
-                jarjestajanTunnusOid,
-                jarjestajanNimi,
-                tekstinYmmartaminen,
-                kirjoittaminen,
-                rakenteetJaSanasto,
-                puheenYmmartaminen,
-                puhuminen,
-                yleisarvosana
-            ) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?) RETURNING 
-                id,
-                suorittajanOppijanumero,
-                sukunimi,
-                etunimet,
-                tutkintopaiva,
-                tutkintokieli,
-                tutkintotaso,
-                jarjestajanTunnusOid,
-                jarjestajanNimi,
-                tekstinYmmartaminen,
-                kirjoittaminen,
-                rakenteetJaSanasto,
-                puheenYmmartaminen,
-                puhuminen,
-                yleisarvosana
-            ON CONFLICT (suorittajanOppijanumero, tutkintopaiva, tutkintokieli, tutkintotaso)
-            """.trimIndent(),
-            { rs, _ ->
-                YkiSuoritus(
-                    rs.getLong("id"),
-                    rs.getString("suorittajanOppijanumero"),
-                    rs.getString("sukunimi"),
-                    rs.getString("etunimet"),
-                    rs.getString("tutkintopaiva"),
-                    rs.getString("tutkintokieli"),
-                    rs.getString("tutkintotaso"),
-                    rs.getString("jarjestajanTunnusOid"),
-                    rs.getString("jarjestajanNimi"),
-                    rs.getFloat("tekstinYmmartaminen"),
-                    rs.getFloat("kirjoittaminen"),
-                    rs.getFloat("rakenteetJaSanasto"),
-                    rs.getFloat("puheenYmmartaminen"),
-                    rs.getFloat("puhuminen"),
-                    rs.getFloat("yleisarvosana"),
-                )
-            },
-            suoritus.suorittajanOppijanumero,
-            suoritus.suorittajanOppijanumero,
-            suoritus.sukunimi,
-            suoritus.etunimet,
-            suoritus.tutkintopaiva,
-            suoritus.tutkintokieli,
-            suoritus.tutkintotaso,
-            suoritus.jarjestajanTunnusOid,
-            suoritus.jarjestajanNimi,
-            suoritus.tekstinYmmartaminen,
-            suoritus.kirjoittaminen,
-            suoritus.rakenteetJaSanasto,
-            suoritus.puheenYmmartaminen,
-            suoritus.puhuminen,
-            suoritus.yleisarvosana,
-        )
-}
+interface YkiRepository : CrudRepository<YkiSuoritusEntity, Int>
--- server/src/main/resources/db/migration/V5__create_yki_table.sql
+++ server/src/main/resources/db/migration/V5__create_yki_table.sql
@@ -4,24 +4,24 @@ CREATE TYPE yki_tutkintotaso AS ENUM ('PT', 'KT', 'YT'); -- ("PT"=perustaso, "KT
 CREATE TABLE yki_suoritus
 (
     id                      SERIAL PRIMARY KEY,
-    suorittajanOppijanumero TEXT                NOT NULL,
+    suorittajan_oppijanumero TEXT                NOT NULL,
     sukunimi                TEXT                NOT NULL,
     etunimet                TEXT                NOT NULL,
     tutkintopaiva           DATE                NOT NULL,
     tutkintokieli           DATE_ISO649_ALPHA3  NOT NULL,
     tutkintotaso            YKI_TUTKINTOTASO    NOT NULL,
-    jarjestajanTunnusOid    TEXT                NOT NULL,
-    jarjestajanNimi         TEXT,
-    tekstinYmmartaminen     FLOAT,
+    jarjestajan_tunnus_oid    TEXT                NOT NULL,
+    jarjestajan_nimi         TEXT,
+    tekstin_ymmartaminen     FLOAT,
     kirjoittaminen          FLOAT,
-    rakenteetJaSanasto      FLOAT,
-    puheenYmmartaminen      FLOAT,
+    rakenteet_ja_sanasto      FLOAT,
+    puheen_ymmartaminen      FLOAT,
     puhuminen               FLOAT,
     yleisarvosana           FLOAT,
 
     CONSTRAINT unique_suoritus UNIQUE
     (
-        suorittajanOppijanumero,
+        suorittajan_oppijanumero,
         tutkintopaiva,
         tutkintokieli,
         tutkintotaso
