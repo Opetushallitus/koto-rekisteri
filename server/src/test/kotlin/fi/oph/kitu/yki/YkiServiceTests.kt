d520c5329def5284d6ec4b861505ba1de336a512
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-10-23 16:58:44 +0300

    test(yki): add integration test

---
+++ server/src/test/kotlin/fi/oph/kitu/yki/YkiServiceTests.kt
@@ -0,0 +1,47 @@
+package fi.oph.kitu.yki
+
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
+import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
+import org.springframework.web.client.RestClient
+
+@SpringBootTest
+class YkiServiceTests(
+    @Autowired private val ykiRepository: YkiRepository,
+) {
+    @Test
+    fun testImportWorks() {
+        // Arrange
+        val mockRestClientBuilder = RestClient.builder()
+        val mockServer = MockRestServiceServer.bindTo(mockRestClientBuilder).build()
+        mockServer
+            // TODO: real address for Solki API
+            .expect(requestTo("suoritukset"))
+            .andRespond(
+                withSuccess(
+                    """
+                    "1.2.246.562.24.99999999999","Suorittaja","Sulevi",2022-11-12,"fin","KT","1.2.246.562.10.373218511910","Iisalmen kansalaisopisto",2,2,1,3,2,2
+                    """.trimIndent(),
+                    MediaType.TEXT_PLAIN,
+                ),
+            )
+
+        // System under test
+        val ykiService =
+            YkiService(
+                solkiRestClient = mockRestClientBuilder.build(),
+                repository = ykiRepository,
+            )
+
+        // Act
+        // TODO: Don't use dry-run, because with dry-run you don't test ykiRepository
+        ykiService.importYkiSuoritukset(null, true)
+
+        // Assert
+        // No error was thrown
+    }
+}
