4274215443460f6de744258d246ddf434115ecaf
Ilari Tommiska <ilari.tommiska@reaktor.com>
2024-11-11 16:45:49 +0200

    add scheduled task for arvioija import

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiScheduledTasks.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiScheduledTasks.kt
@@ -14,9 +14,18 @@ class YkiScheduledTasks {
     @Value("\${kitu.yki.scheduling.import.schedule}")
     lateinit var ykiImportSchedule: String
 
+    @Value("\${kitu.yki.scheduling.importArvioijat.schedule}")
+    lateinit var ykiImportArvioijatSchedule: String
+
     @Bean
     fun dailyImport(ykiService: YkiService): Task<Void> =
         Tasks
             .recurring("YKI-import", Schedules.parseSchedule(ykiImportSchedule))
             .execute { _, _ -> ykiService.importYkiSuoritukset() }
+
+    @Bean
+    fun arvioijatImport(ykiService: YkiService): Task<Void> =
+        Tasks
+            .recurring("YKI-import-arvioijat", Schedules.parseSchedule(ykiImportArvioijatSchedule))
+            .execute { _, _ -> ykiService.importYkiArvioijat() }
 }
