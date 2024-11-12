50f6acd74af417de972bb30fc62e399c5973ae1d
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2024-11-12 12:21:46 +0200

    Replace YKI mock API with the real one

--- server/src/main/kotlin/fi/oph/kitu/yki/SolkiRestClientConfig.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/SolkiRestClientConfig.kt
@@ -4,16 +4,26 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
+import java.util.Base64
 
 @Configuration
 class SolkiRestClientConfig {
     @Value("\${kitu.yki.baseUrl}")
     private lateinit var baseUrl: String
 
+    @Value("\${kitu.yki.username}")
+    private lateinit var user: String
+
+    @Value("\${kitu.yki.password}")
+    private lateinit var password: String
+
     @Bean("solkiRestClient")
-    fun restClient(): RestClient =
-        RestClient
+    fun restClient(): RestClient {
+        val basicAuthToken = Base64.getEncoder().encodeToString("$user:$password".toByteArray())
+        return RestClient
             .builder()
             .baseUrl(baseUrl)
+            .defaultHeader("Authorization", "Basic $basicAuthToken")
             .build()
+    }
 }
