fc7bf6708b6602091e93ee2bd2d2debb68106818
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2024-11-13 16:52:48 +0200

    Replace react with mustache

---
+++ server/src/main/kotlin/fi/oph/kitu/HomeController.kt
@@ -0,0 +1,11 @@
+package fi.oph.kitu
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+    @GetMapping("/")
+    fun home(): String = "index"
+}
