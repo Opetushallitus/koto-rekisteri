adf1bd5a1cd4de1f91d3c9105668285aaf67d015
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-11-15 12:58:31 +0200

    refactor: take aws secrets out from server start

---
+++ scripts/ensure_aws_secrets.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+current_dir=$( dirname "${BASH_SOURCE[0]}" )
+source "$current_dir/common-functions.sh"
+
+get_secret() {
+  aws secretsmanager get-secret-value --secret-id "$1" --output text --query SecretString
+}
+
+"$current_dir/ensure_aws_profiles.sh"
+require_dev_aws_session
+
+export "KIELITESTI_TOKEN"="$(get_secret "kielitesti-token")"; echo "KIELITESTI_TOKEN exported"
+export "OPPIJANUMERO_PASSWORD"="$(get_secret "oppijanumero-password")"; echo "OPPIJANUMERO_PASSWORD exported"
+export "YKI_API_USER"="$(get_secret "yki-api-user")"; echo "YKI_API_USER exported"
+export "YKI_API_PASSWORD"="$(get_secret "yki-api-password")"; echo "YKI_API_PASSWORD exported"
--- scripts/start_local_server.sh
+++ scripts/start_local_server.sh
@@ -9,19 +9,9 @@ get_secret() {
 }
 
 require_command humanlog
-require_command aws
-
-configure_aws_sso_profiles
-require_dev_aws_session
 
 require_env SPRING_PROFILES_ACTIVE
 require_env KOTLIN_POST_PROCESS_FILE
 
 cd "$REPO_ROOT"/server
-
-KIELITESTI_TOKEN=$(get_secret kielitesti-token); export KIELITESTI_TOKEN
-OPPIJANUMERO_PASSWORD=$(get_secret oppijanumero-password); export OPPIJANUMERO_PASSWORD
-YKI_API_USER=$(get_secret yki-api-user); export YKI_API_USER
-YKI_API_PASSWORD=$(get_secret yki-api-password); export YKI_API_PASSWORD
-
 ./mvnw spring-boot:run | humanlog --truncate-length 9999
