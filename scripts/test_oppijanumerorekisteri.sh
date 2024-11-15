8eec0e6e59f610f5f361ed717ef0ae7cfdd3fa73
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-11-15 12:51:45 +0200

    chore: remove obsolete script

--- scripts/test_oppijanumerorekisteri.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-curl -s -i \
-    -X POST "https://virkailija.testiopintopolku.fi/cas/v1/tickets" \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "username=$1&password=$2"
