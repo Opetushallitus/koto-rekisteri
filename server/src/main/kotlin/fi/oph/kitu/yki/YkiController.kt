03518383f4e42b829cc44ab4a92c42d7fe0f0bd1
Ilari Tommiska <ilari.tommiska@reaktor.com>
2024-11-19 12:58:32 +0200

    add view for arvioijat

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiController.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiController.kt
@@ -10,14 +10,25 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("yki")
 class YkiController(
     private val repository: YkiRepository,
+    private val arvioijatRepository: YkiArvioijaRepository,
 ) {
     @GetMapping("/suoritukset")
-    fun view(
+    fun suorituksetView(
         model: Model,
         response: HttpServletResponse?,
     ): String {
         val suoritukset: List<YkiSuoritusEntity> = repository.findAll().toList()
         model.addAttribute("suoritukset", suoritukset)
         return "yki-suoritukset"
+    }
+
+    @GetMapping("/arvioijat")
+    fun arvioijatView(
+        model: Model,
+        response: HttpServletResponse?,
+    ): String {
+        val arvioijat: List<YkiArvioijaEntity> = arvioijatRepository.findAll().toList()
+        model.addAttribute("arvioijat", arvioijat)
+        return "yki-arvioijat"
     }
 }
