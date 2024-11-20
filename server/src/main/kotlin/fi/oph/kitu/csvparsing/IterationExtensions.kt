d50764c9e81b8a3e7dd068154ea820ec40474b5b
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-11-20 16:14:08 +0200

    refactor(csv): make csv parsing more generic

--- server/src/main/kotlin/fi/oph/kitu/csvparsing/IterationExtensions.kt
+++ server/src/main/kotlin/fi/oph/kitu/csvparsing/IterationExtensions.kt
@@ -1,43 +1,24 @@
 package fi.oph.kitu.csvparsing
 
 import com.fasterxml.jackson.dataformat.csv.CsvMapper
-import com.fasterxml.jackson.dataformat.csv.CsvSchema
-import fi.oph.kitu.yki.YkiSuoritusEntity
 import java.io.StringWriter
 
-fun Iterable<YkiSuoritusEntity>.toCsvString(): String {
-    val mapper = CsvMapper()
+inline fun <reified T> Iterable<T>.toCsvString(
+    columnSeparator: Char = ',',
+    useHeader: Boolean = false,
+    quoteChar: Char = '"',
+): String {
+    val csvMapper = CsvMapper()
     val schema =
-        CsvSchema
-            .builder()
-            .addColumn("id")
-            .addColumn("suorittajanOppijanumero")
-            .addColumn("hetu")
-            .addColumn("sukupuoli")
-            .addColumn("sukunimi")
-            .addColumn("etunimet")
-            .addColumn("kansalaisuus")
-            .addColumn("katuosoite")
-            .addColumn("postinumero")
-            .addColumn("postitoimipaikka")
-            .addColumn("email")
-            .addColumn("tutkintopaiva")
-            .addColumn("tutkintokieli")
-            .addColumn("tutkintotaso")
-            .addColumn("jarjestajanTunnusOid")
-            .addColumn("jarjestajanNimi")
-            .addColumn("arviointipaiva")
-            .addColumn("tekstinYmmartaminen")
-            .addColumn("kirjoittaminen")
-            .addColumn("rakenteetJaSanasto")
-            .addColumn("puheenYmmartaminen")
-            .addColumn("puhuminen")
-            .addColumn("yleisarvosana")
-            .build()
-            .withHeader()
+        csvMapper
+            .typedSchemaFor(T::class.java)
+            .withColumnSeparator(columnSeparator)
+            .withUseHeader(useHeader)
+            .withQuoteChar(quoteChar)
 
     val writer = StringWriter()
-    mapper
+
+    csvMapper
         .writerFor(Iterable::class.java)
         .with(schema)
         .writeValue(writer, this)
