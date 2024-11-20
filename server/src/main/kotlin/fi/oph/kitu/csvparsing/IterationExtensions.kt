bdb9bd2d403144813a8c1976fca3bf39a5cdd089
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-11-20 15:43:28 +0200

    feat(csvparsing): convert yki arviointisuoritus to csv

--- server/src/main/kotlin/fi/oph/kitu/csvparsing/IterationExtensions.kt
+++ server/src/main/kotlin/fi/oph/kitu/csvparsing/IterationExtensions.kt
@@ -1,9 +1,47 @@
 package fi.oph.kitu.csvparsing
 
-fun <T> Iterable<T>.toCsvString(): String =
-    """
-    id,name,description
-    1,Alice,Software Engineer
-    2,Bob,Product Manager
-    3,Charlie,Data Scientist
-    """.trimIndent()
+import com.fasterxml.jackson.dataformat.csv.CsvMapper
+import com.fasterxml.jackson.dataformat.csv.CsvSchema
+import fi.oph.kitu.yki.YkiSuoritusEntity
+import java.io.StringWriter
+
+fun Iterable<YkiSuoritusEntity>.toCsvString(): String {
+    val mapper = CsvMapper()
+    val schema =
+        CsvSchema
+            .builder()
+            .addColumn("id")
+            .addColumn("suorittajanOppijanumero")
+            .addColumn("hetu")
+            .addColumn("sukupuoli")
+            .addColumn("sukunimi")
+            .addColumn("etunimet")
+            .addColumn("kansalaisuus")
+            .addColumn("katuosoite")
+            .addColumn("postinumero")
+            .addColumn("postitoimipaikka")
+            .addColumn("email")
+            .addColumn("tutkintopaiva")
+            .addColumn("tutkintokieli")
+            .addColumn("tutkintotaso")
+            .addColumn("jarjestajanTunnusOid")
+            .addColumn("jarjestajanNimi")
+            .addColumn("arviointipaiva")
+            .addColumn("tekstinYmmartaminen")
+            .addColumn("kirjoittaminen")
+            .addColumn("rakenteetJaSanasto")
+            .addColumn("puheenYmmartaminen")
+            .addColumn("puhuminen")
+            .addColumn("yleisarvosana")
+            .build()
+            .withHeader()
+
+    val writer = StringWriter()
+    mapper
+        .writerFor(Iterable::class.java)
+        .with(schema)
+        .writeValue(writer, this)
+
+    val str = writer.toString()
+    return str
+}
