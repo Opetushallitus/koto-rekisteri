cea9620fb0acc4e24e3d38ff83d5e97f913acc3c
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-11-20 12:57:55 +0200

    feat(scripts): add a script to check whether env vars are exported

---
+++ scripts/check_env-vars.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Parse flags
+verbose=false
+while getopts "X" opt; do
+  case $opt in
+    X) verbose=true ;;
+    *) exit 1 ;;
+  esac
+done
+
+is_env_variable_exported() {
+  local var_name="$1"
+  if env | grep -q "^${var_name}="; then
+    $verbose && echo "[DEBUG] variable ${var_name} is exported"
+    return 0
+  else
+    $verbose && echo "[DEBUG] variable ${var_name} is missing!"
+    return 1
+  fi
+}
+
+amount=0
+
+is_env_variable_exported "KIELITESTI_TOKEN" || amount=$((amount + 1))
+is_env_variable_exported "OPPIJANUMERO_PASSWORD" || amount=$((amount + 1))
+is_env_variable_exported "YKI_API_USER" || amount=$((amount + 1))
+is_env_variable_exported "YKI_API_PASSWORD" || amount=$((amount + 1))
+
+echo "$amount variable(s) are missing."
+exit $amount
+
+
+
