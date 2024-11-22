6884592b6308f5550ccd149184ba0c7b8524bad2
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2024-11-22 14:23:03 +0200

    chore: remove old alarm lambda
    We can use AWS Chatbot instead.

--- infra/lib/alarms-stack.ts
+++ infra/lib/alarms-stack.ts
@@ -1,17 +1,6 @@
 import * as cdk from "aws-cdk-lib"
-import {
-  aws_lambda,
-  aws_lambda_nodejs,
-  aws_sns,
-  PhysicalName,
-  StackProps,
-} from "aws-cdk-lib"
-import { TreatMissingData } from "aws-cdk-lib/aws-cloudwatch"
-import { OutputFormat } from "aws-cdk-lib/aws-lambda-nodejs"
-import { Secret } from "aws-cdk-lib/aws-secretsmanager"
-import { LambdaSubscription } from "aws-cdk-lib/aws-sns-subscriptions"
+import { aws_sns, PhysicalName, StackProps } from "aws-cdk-lib"
 import { Construct } from "constructs"
-import * as path from "node:path"
 
 export interface AlarmsStackProps extends StackProps {}
 
@@ -24,43 +13,5 @@ export class AlarmsStack extends cdk.Stack {
     this.alarmSnsTopic = new aws_sns.Topic(this, "AlarmSnsTopic", {
       topicName: PhysicalName.GENERATE_IF_NEEDED,
     })
-
-    const slackWebhookUrlSecretName = `slack-webhook-url`
-
-    const slackNotifierLambda = new aws_lambda_nodejs.NodejsFunction(
-      this,
-      "SlackNotifierLambda",
-      {
-        runtime: aws_lambda.Runtime.NODEJS_20_X,
-        entry: path.join(__dirname, "lambdas/slackNotifierLambda/index.mts"),
-        handler: "handler",
-        timeout: cdk.Duration.seconds(60),
-        environment: {
-          SLACK_WEBHOOK_URL_SECRET_NAME: slackWebhookUrlSecretName,
-        },
-        bundling: {
-          format: OutputFormat.ESM,
-        },
-      },
-    )
-
-    this.alarmSnsTopic.addSubscription(
-      new LambdaSubscription(slackNotifierLambda),
-    )
-
-    Secret.fromSecretNameV2(
-      this,
-      "SlackWebhookUrlSecret",
-      slackWebhookUrlSecretName,
-    ).grantRead(slackNotifierLambda)
-
-    // Can't send alarms from this to the SNS topic, since that would create an infinite loop.
-    slackNotifierLambda
-      .metricErrors()
-      .createAlarm(this, "SlackNotifierLambdaErrors", {
-        threshold: 1,
-        evaluationPeriods: 1,
-        treatMissingData: TreatMissingData.NOT_BREACHING,
-      })
   }
 }
