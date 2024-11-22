fa91c85c1a4e679055e23b7bed5c86dd8f4d380f
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-11-22 16:57:24 +0200

    refactor(yki): separate api and controller from each others

---
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiApiController.kt
@@ -0,0 +1,33 @@
+package fi.oph.kitu.yki
+
+import fi.oph.kitu.generated.api.YkiControllerApi
+import org.springframework.core.io.Resource
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("yki/api")
+class YkiApiController(
+    private val service: YkiService,
+) : YkiControllerApi {
+    @GetMapping("/suoritukset", produces = ["text/csv"])
+    @ResponseBody
+    override fun getSuorituksetAsCsv(): ResponseEntity<Resource> {
+        val filename = "suoritukset.csv"
+
+        val inputStream = service.generateSuorituksetCsvStream()
+        val resource =
+            org.springframework.core.io
+                .InputStreamResource(inputStream)
+
+        return ResponseEntity
+            .ok()
+            .contentType(MediaType.parseMediaType("text/csv"))
+            .header("Content-Disposition", "attachment; filename=$filename")
+            .body(resource)
+    }
+}
