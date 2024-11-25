e9a65ff4b8b5726a423e636c4f7a45e10960abde
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2024-11-25 17:44:12 +0200

    fix: use a custom HTTP access logger, again
    logback-access doesn't support Spring's native structured logging. Let's implement our own logger at the Spring request/response lifecycle level instead.

--- server/src/main/kotlin/fi/oph/kitu/logging/LoggerExtensions.kt
+++ server/src/main/kotlin/fi/oph/kitu/logging/LoggerExtensions.kt
@@ -1,10 +1,33 @@
 package fi.oph.kitu.logging
 
 import fi.oph.kitu.PeerService
+import io.opentelemetry.semconv.HttpAttributes
+import io.opentelemetry.semconv.UrlAttributes
+import io.opentelemetry.semconv.UserAgentAttributes
+import io.opentelemetry.semconv.incubating.HttpIncubatingAttributes
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.spi.LoggingEventBuilder
 import org.springframework.dao.DuplicateKeyException
+import org.springframework.http.HttpHeaders
 import org.springframework.http.ResponseEntity
 import java.net.http.HttpResponse
+
+fun LoggingEventBuilder.addServletRequest(request: HttpServletRequest): LoggingEventBuilder =
+    add(
+        UrlAttributes.URL_PATH.toString() to request.requestURI,
+        UrlAttributes.URL_QUERY.toString() to request.queryString,
+        UrlAttributes.URL_SCHEME.toString() to (request.scheme ?: "http"),
+        HttpAttributes.HTTP_REQUEST_METHOD.toString() to request.method,
+        UserAgentAttributes.USER_AGENT_ORIGINAL.toString() to request.getHeader(HttpHeaders.USER_AGENT),
+    )
+
+fun LoggingEventBuilder.addServletResponse(response: HttpServletResponse): LoggingEventBuilder =
+    add(
+        HttpAttributes.HTTP_RESPONSE_STATUS_CODE.toString() to response.status,
+        HttpIncubatingAttributes.HTTP_RESPONSE_BODY_SIZE.toString() to
+            response.getHeader(HttpHeaders.CONTENT_LENGTH),
+    )
 
 inline fun <reified T> LoggingEventBuilder.addResponse(
     response: HttpResponse<T>,
