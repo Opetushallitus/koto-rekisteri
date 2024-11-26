b1c207327ce8e30755240d6f277171e27e24f4cc
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-11-26 11:39:46 +0200

    fix(yki): fix paths

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiApiController.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiApiController.kt
@@ -7,9 +7,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
 @RequestMapping("yki/api")
 class YkiApiController(
     private val service: YkiService,
