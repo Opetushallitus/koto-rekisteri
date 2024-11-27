0ba01cfb95f23da7549f996eed8c29ebca0d4ec6
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-11-27 16:34:48 +0200

    feat(yki): add more information about serialization when parsing CSV data

--- server/src/main/kotlin/fi/oph/kitu/csvparsing/CsvConversionExtensions.kt
+++ server/src/main/kotlin/fi/oph/kitu/csvparsing/CsvConversionExtensions.kt
@@ -1,9 +1,10 @@
 package fi.oph.kitu.csvparsing
 
+import com.fasterxml.jackson.databind.exc.InvalidFormatException
 import com.fasterxml.jackson.dataformat.csv.CsvMapper
+import fi.oph.kitu.logging.add
 import fi.oph.kitu.logging.withEvent
 import org.slf4j.LoggerFactory
-import org.springframework.web.client.RestClientException
 import java.io.ByteArrayOutputStream
 
 inline fun <reified T> Iterable<T>.writeAsCsv(
@@ -24,8 +25,11 @@ inline fun <reified T> Iterable<T>.writeAsCsv(
  */
 inline fun <reified T> String.asCsv(args: CsvArgs = CsvArgs()): List<T> {
     if (this.isBlank()) {
+        args.event.add("serialization.isEmptyList" to true)
         return emptyList()
     }
+
+    args.event.add("serialization.isEmptyList" to false)
 
     val csvMapper = getCsvMapper<T>()
     val schema = getSchema<T>(csvMapper, args)
@@ -36,7 +40,14 @@ inline fun <reified T> String.asCsv(args: CsvArgs = CsvArgs()): List<T> {
             .with(schema)
             .readValues<T>(this)
             .readAll()
-    } catch (e: Exception) {
-        throw RestClientException("Could not parse string as CSV into a type ${T::class.java.name}.", e)
+    } catch (e: InvalidFormatException) {
+        args.event.add(
+            "serialization.isInvalidFormatException" to true,
+            "serialization.value" to e.value,
+            "serialization.targetType" to e.targetType,
+            "serialization.path" to e.path,
+        )
+
+        throw e
     }
 }
