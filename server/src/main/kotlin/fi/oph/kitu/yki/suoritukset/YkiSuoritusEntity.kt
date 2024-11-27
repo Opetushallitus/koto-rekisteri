d0019a81673423014501a9faa5711bf884d4c22b
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2024-11-27 16:05:32 +0200

    Define YKI suoritus number types explicitly instead of using Number

--- server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/YkiSuoritusEntity.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/YkiSuoritusEntity.kt
@@ -13,7 +13,7 @@ import java.util.Date
 @Table(name = "yki_suoritus")
 data class YkiSuoritusEntity(
     @Id
-    val id: Number?,
+    val id: Int?,
     val suorittajanOID: String,
     val hetu: String,
     @Enumerated(EnumType.STRING)
@@ -35,15 +35,15 @@ data class YkiSuoritusEntity(
     val jarjestajanTunnusOid: String,
     val jarjestajanNimi: String,
     val arviointipaiva: Date,
-    val tekstinYmmartaminen: Number?,
-    val kirjoittaminen: Number?,
-    val rakenteetJaSanasto: Number?,
-    val puheenYmmartaminen: Number?,
-    val puhuminen: Number?,
-    val yleisarvosana: Number?,
+    val tekstinYmmartaminen: Double?,
+    val kirjoittaminen: Double?,
+    val rakenteetJaSanasto: Double?,
+    val puheenYmmartaminen: Double?,
+    val puhuminen: Double?,
+    val yleisarvosana: Double?,
     val tarkistusarvioinninSaapumisPvm: Date?,
     val tarkistusarvioinninAsiatunnus: String?,
-    val tarkistusarvioidutOsakokeet: Number?,
+    val tarkistusarvioidutOsakokeet: Int?,
     val arvosanaMuuttui: Boolean?,
     val perustelu: String?,
     val tarkistusarvioinninKasittelyPvm: Date?,
