25b538fdbb8452a966e2d946f8ae331e858b7a0f
Ilari Tommiska <ilari.tommiska@reaktor.com>
2024-12-19 10:07:28 +0200

    wip: update koealusta api
    - string formatted quiz results
    - firstnames instead of firstname
    - oppijanumero instead of OID
    - added schoold OID (school organization/toimipaikka OID)

--- server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KoealustaSuorituksetResponse.kt
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KoealustaSuorituksetResponse.kt
@@ -1,22 +1,22 @@
 package fi.oph.kitu.kotoutumiskoulutus
 
-import fi.oph.kitu.oppijanumero.Oppija
-
 data class KoealustaSuorituksetResponse(
     val users: List<User>,
 ) {
     data class User(
-        val firstname: String,
+        val userid: Int,
+        val firstnames: String,
         val lastname: String,
-        val preferredname: String,
-        val OID: String,
-        val SSN: String,
+        val preferredname: String?,
+        val oppijanumero: String?,
+        val SSN: String?,
         val email: String,
         val completions: List<Completion>,
     ) {
         data class Completion(
             val courseid: Int,
             val coursename: String,
+            val schoolOID: String,
             val results: List<Result>,
             val timecompleted: Long,
             val total_evaluation_teacher: String,
@@ -24,19 +24,9 @@ data class KoealustaSuorituksetResponse(
         ) {
             data class Result(
                 val name: String,
-                val quiz_result_system: Double,
-                val quiz_result_teacher: Double,
+                val quiz_result_system: String?,
+                val quiz_result_teacher: String?,
             )
         }
-
-        fun toOppija() =
-            Oppija(
-                etunimet = firstname,
-                hetu = SSN,
-                kutsumanimi = preferredname,
-                sukunimi = lastname,
-                oppijanumero = OID, // Koealusta incorrectly refers oppijanumero as OID
-                henkilo_oid = null,
-            )
     }
 }
