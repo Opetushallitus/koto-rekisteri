31665e6d376526441f3d816541cf8a9787c61d17
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-12-27 15:42:22 +0200

    feat(oppijanumero): throw OppijaNotFoundException, when oppija is not found

--- server/src/main/kotlin/fi/oph/kitu/oppijanumero/OppijanumeroException.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/OppijanumeroException.kt
@@ -1,8 +1,22 @@
 package fi.oph.kitu.oppijanumero
 
-class OppijanumeroException(
+open class OppijanumeroException(
     val oppija: Oppija,
     message: String,
     val oppijanumeroServiceError: OppijanumeroServiceError? = null,
     cause: Throwable? = null,
-) : Throwable(message, cause)
+) : Throwable(message, cause) {
+    class OppijaNotFoundException(
+        oppija: Oppija,
+        message: String = "Oppija not found from oppijanumero-service",
+        oppijanumeroServiceError: OppijanumeroServiceError? = null,
+        cause: Throwable? = null,
+    ) : OppijanumeroException(oppija, message, oppijanumeroServiceError, cause)
+
+    class OppijaNotIdentifiedException(
+        oppija: Oppija,
+        message: String = "Oppija with oid ${oppija.henkilo_oid} is not identified in oppijanumero-service",
+        oppijanumeroServiceError: OppijanumeroServiceError? = null,
+        cause: Throwable? = null,
+    ) : OppijanumeroException(oppija, message, oppijanumeroServiceError, cause)
+}
