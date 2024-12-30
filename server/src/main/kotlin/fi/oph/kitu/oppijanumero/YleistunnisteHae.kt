d95d25c5a5fe3373a61de314e2273af8388998eb
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-12-30 14:24:57 +0200

    fix(oppijanumero): use JsonProperty so that the deserialization is more consistent

--- server/src/main/kotlin/fi/oph/kitu/oppijanumero/YleistunnisteHae.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/YleistunnisteHae.kt
@@ -1,22 +1,33 @@
 package fi.oph.kitu.oppijanumero
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.Date
 
 data class YleistunnisteHaeRequest(
+    @JsonProperty("etunimet")
     val etunimet: String,
+    @JsonProperty("hetu")
     val hetu: String,
+    @JsonProperty("kutsumanimi")
     val kutsumanimi: String,
+    @JsonProperty("sukunimi")
     val sukunimi: String,
 )
 
 data class YleistunnisteHaeResponse(
+    @JsonProperty("oid")
     val oid: String,
+    @JsonProperty("oppijanumero")
     val oppijanumero: String?,
 )
 
 data class OppijanumeroServiceError(
+    @JsonProperty("timestamp")
     val timestamp: Date,
+    @JsonProperty("status")
     val status: Int,
+    @JsonProperty("error")
     val error: String,
+    @JsonProperty("path")
     val path: String,
 )
