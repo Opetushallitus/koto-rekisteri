7eb09c483a308cb36e2db18d1e199e49e5ca34c9
Saku K <6057704+saku-koodari@users.noreply.github.com>
2024-12-31 14:27:34 +0200

    chore(test): add boilerplate code

---
+++ server/src/test/kotlin/fi/oph/kitu/logging/MockLogger.kt
@@ -0,0 +1,415 @@
+package fi.oph.kitu.logging
+
+import org.slf4j.Logger
+import org.slf4j.Marker
+
+class MockLogger : Logger {
+    override fun getName(): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun isTraceEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isTraceEnabled(p0: Marker?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(p0: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: String?,
+        p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: String?,
+        p1: Any?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: String?,
+        vararg p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: String?,
+        p1: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: Marker?,
+        p1: String?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+        p3: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: Marker?,
+        p1: String?,
+        vararg p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun trace(
+        p0: Marker?,
+        p1: String?,
+        p2: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isDebugEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isDebugEnabled(p0: Marker?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(p0: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: String?,
+        p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: String?,
+        p1: Any?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: String?,
+        vararg p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: String?,
+        p1: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: Marker?,
+        p1: String?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+        p3: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: Marker?,
+        p1: String?,
+        vararg p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun debug(
+        p0: Marker?,
+        p1: String?,
+        p2: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isInfoEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isInfoEnabled(p0: Marker?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(p0: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: String?,
+        p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: String?,
+        p1: Any?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: String?,
+        vararg p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: String?,
+        p1: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: Marker?,
+        p1: String?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+        p3: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: Marker?,
+        p1: String?,
+        vararg p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun info(
+        p0: Marker?,
+        p1: String?,
+        p2: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isWarnEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isWarnEnabled(p0: Marker?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(p0: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: String?,
+        p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: String?,
+        vararg p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: String?,
+        p1: Any?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: String?,
+        p1: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: Marker?,
+        p1: String?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+        p3: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: Marker?,
+        p1: String?,
+        vararg p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun warn(
+        p0: Marker?,
+        p1: String?,
+        p2: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun isErrorEnabled(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun isErrorEnabled(p0: Marker?): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(p0: String?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: String?,
+        p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: String?,
+        p1: Any?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: String?,
+        vararg p1: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: String?,
+        p1: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: Marker?,
+        p1: String?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: Marker?,
+        p1: String?,
+        p2: Any?,
+        p3: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: Marker?,
+        p1: String?,
+        vararg p2: Any?,
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun error(
+        p0: Marker?,
+        p1: String?,
+        p2: Throwable?,
+    ) {
+        TODO("Not yet implemented")
+    }
+}
