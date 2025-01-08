05fd20e33f45ab62ae99f20515e475f5da8daa97
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-01-08 13:16:35 +0200

    test(logging): test defaults of EventLogger

--- server/src/test/kotlin/fi/oph/kitu/logging/EventLoggerTests.kt
+++ server/src/test/kotlin/fi/oph/kitu/logging/EventLoggerTests.kt
@@ -3,9 +3,72 @@ package fi.oph.kitu.logging
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 
 class EventLoggerTests {
+    @Test
+    fun `addDefaults do add defaults when running the action`() {
+        val event = MockEvent()
+        val actionToRun = { Thread.sleep(1) }
+
+        // runCatching is eager so it runs the action.
+        val logger = EventLogger(runCatching(actionToRun), event)
+        logger.addDefaults("unit-test")
+
+        val result = logger.getOrThrow()
+        assertNotNull(result)
+
+        val success = event.getValueOrNullByKey<Boolean>("success")
+        assertNotNull(success, "missing success")
+        assertTrue(success, "success should be true")
+
+        val message = event.messages.first { m -> m == "unit-test success" }
+        assertNotNull(message)
+        assertEquals("unit-test success", message, "expected correct message")
+
+        val logsCount = event.logs.size
+        assertNotNull(logsCount, "logs_count should not be null")
+        assertEquals(1, logsCount, "logs_count should be 1")
+    }
+
+    @Test
+    fun `addDefaults do add defaults even if action throws an exception`() {
+        class ExceptionThatThrows(
+            message: String,
+            cause: Throwable? = null,
+        ) : Throwable(message, cause)
+
+        val event = MockEvent()
+
+        // Simulates a situation, where we had an action that Returns a failure.
+        // For example we had an action that throwed an exception and it was caught and wrapped to Result.failure
+        val actionToRun: Result<Unit> =
+            runCatching {
+                throw ExceptionThatThrows("Expected error")
+            }
+
+        val logger = EventLogger(actionToRun, event)
+        logger.addDefaults("unit-test")
+
+        assertThrows<ExceptionThatThrows> {
+            logger.getOrThrow()
+        }
+
+        val success = event.getValueOrNullByKey<Boolean>("success")
+        assertNotNull(success, "missing success")
+        assertFalse(success, "success should be false")
+
+        val message = event.messages.first { m -> m == "unit-test failed" }
+        assertNotNull(message)
+        assertEquals("unit-test failed", message, "expected correct message")
+
+        val logsCount = event.logs.size
+        assertNotNull(logsCount, "logs_count should not be null")
+        assertEquals(1, logsCount, "logs_count should be 1")
+    }
+
     @Test
     fun `withEventAndPerformanceCheck does performance logging when running an action`() {
         val event = MockEvent()
