53acc3d2b790bdecdc3dd3b30134acfe2d45040b
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-01-08 12:19:25 +0200

    feat(oppijanumero): Oppijanumero do not do database loggings anymore

--- server/src/main/kotlin/fi/oph/kitu/oppijanumero/OppijanumeroService.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/OppijanumeroService.kt
@@ -3,7 +3,7 @@ package fi.oph.kitu.oppijanumero
 import com.fasterxml.jackson.databind.ObjectMapper
 import fi.oph.kitu.logging.add
 import fi.oph.kitu.logging.addCondition
-import fi.oph.kitu.logging.withEvent
+import fi.oph.kitu.logging.withEventAndPerformanceCheck
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -29,60 +29,64 @@ class OppijanumeroServiceImpl(
     var useMockData: Boolean = false
 
     override fun getOppijanumero(oppija: Oppija): String =
-        logger.atInfo().withEvent("getOppijanumero") { event ->
-            require(oppija.etunimet.isNotEmpty()) { "etunimet cannot be empty" }
-            require(oppija.hetu.isNotEmpty()) { "hetu cannot be empty" }
-            require(oppija.sukunimi.isNotEmpty()) { "sukunimi cannot be empty" }
-            require(oppija.kutsumanimi.isNotEmpty()) { "kutsumanimi cannot be empty" }
+        logger
+            .atInfo()
+            .withEventAndPerformanceCheck { event ->
+                require(oppija.etunimet.isNotEmpty()) { "etunimet cannot be empty" }
+                require(oppija.hetu.isNotEmpty()) { "hetu cannot be empty" }
+                require(oppija.sukunimi.isNotEmpty()) { "sukunimi cannot be empty" }
+                require(oppija.kutsumanimi.isNotEmpty()) { "kutsumanimi cannot be empty" }
 
-            if (event.addCondition(key = "request.hasOppijanumero", condition = oppija.oppijanumero != null)) {
-                return@withEvent oppija.oppijanumero.toString()
-            }
+                if (event.addCondition(key = "request.hasOppijanumero", condition = oppija.oppijanumero != null)) {
+                    return@withEventAndPerformanceCheck oppija.oppijanumero.toString()
+                }
 
-            if (event.addCondition(key = "useMockData", condition = useMockData)) {
-                return@withEvent "1.2.246.562.24.33342764709"
-            }
+                if (event.addCondition(key = "useMockData", condition = useMockData)) {
+                    return@withEventAndPerformanceCheck "1.2.246.562.24.33342764709"
+                }
 
-            val endpoint = "$serviceUrl/yleistunniste/hae"
-            val httpRequest =
-                HttpRequest
-                    .newBuilder(URI.create(endpoint))
-                    .POST(
-                        HttpRequest.BodyPublishers.ofString(
-                            objectMapper.writeValueAsString(oppija.toYleistunnisteHaeRequest()),
-                        ),
-                    ).header("Content-Type", "application/json")
+                val endpoint = "$serviceUrl/yleistunniste/hae"
+                val httpRequest =
+                    HttpRequest
+                        .newBuilder(URI.create(endpoint))
+                        .POST(
+                            HttpRequest.BodyPublishers.ofString(
+                                objectMapper.writeValueAsString(oppija.toYleistunnisteHaeRequest()),
+                            ),
+                        ).header("Content-Type", "application/json")
 
-            // no need to log sendRequest, because there are request and response logging inside casAuthenticatedService.
-            val stringResponse =
-                casAuthenticatedService
-                    .sendRequest(httpRequest)
-                    .getOrLogAndThrowCasException(event)
+                // no need to log sendRequest, because there are request and response logging inside casAuthenticatedService.
+                val stringResponse =
+                    casAuthenticatedService
+                        .sendRequest(httpRequest)
+                        .getOrLogAndThrowCasException(event)
 
-            if (stringResponse.statusCode() == 404) {
-                throw OppijanumeroException.OppijaNotFoundException(oppija)
-            } else if (stringResponse.statusCode() != 200) {
-                throw OppijanumeroException(
-                    oppija,
-                    "Oppijanumero-service returned unexpected status code ${stringResponse.statusCode()}",
+                if (stringResponse.statusCode() == 404) {
+                    throw OppijanumeroException.OppijaNotFoundException(oppija)
+                } else if (stringResponse.statusCode() != 200) {
+                    throw OppijanumeroException(
+                        oppija,
+                        "Oppijanumero-service returned unexpected status code ${stringResponse.statusCode()}",
+                    )
+                }
+
+                val body = tryConvertToOppijanumeroResponse<YleistunnisteHaeResponse>(oppija, stringResponse)
+                event.add(
+                    "response.hasOppijanumero" to body.oppijanumero.isNullOrEmpty(),
+                    "response.hasOid" to body.oid.isEmpty(),
+                    "response.areOppijanumeroAndOidSame" to (body.oppijanumero == body.oid),
                 )
-            }
 
-            val body = tryConvertToOppijanumeroResponse<YleistunnisteHaeResponse>(oppija, stringResponse)
-            event.add(
-                "response.hasOppijanumero" to body.oppijanumero.isNullOrEmpty(),
-                "response.hasOid" to body.oid.isEmpty(),
-                "response.areOppijanumeroAndOidSame" to (body.oppijanumero == body.oid),
-            )
+                if (body.oppijanumero.isNullOrEmpty()) {
+                    throw OppijanumeroException.OppijaNotIdentifiedException(
+                        oppija.withYleistunnisteHaeResponse(body),
+                    )
+                }
 
-            if (body.oppijanumero.isNullOrEmpty()) {
-                throw OppijanumeroException.OppijaNotIdentifiedException(
-                    oppija.withYleistunnisteHaeResponse(body),
-                )
-            }
-
-            return@withEvent body.oppijanumero
-        }
+                return@withEventAndPerformanceCheck body.oppijanumero
+            }.apply {
+                withDefaultLogging("getOppijanumero")
+            }.getOrThrow()
 
     /**
      * Tries to convert HttpResponse<String> into the given T.
