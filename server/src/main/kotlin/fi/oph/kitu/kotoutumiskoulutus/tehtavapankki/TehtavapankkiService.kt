2947b91ca4c62c911c52c5d8f34f5fe124c4fd02
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-01-20 15:46:21 +0200

    s3 upload

--- server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/tehtavapankki/TehtavapankkiService.kt
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/tehtavapankki/TehtavapankkiService.kt
@@ -4,6 +4,7 @@ import fi.oph.kitu.PeerService
 import fi.oph.kitu.logging.add
 import fi.oph.kitu.logging.addHttpResponse
 import fi.oph.kitu.logging.withEventAndPerformanceCheck
+import io.awspring.cloud.s3.S3Template
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
@@ -11,11 +12,14 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.toEntity
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Service
 class TehtavapankkiService(
     @Qualifier("restClientBuilderForLargeResponses")
     private val restClientBuilder: RestClient.Builder,
+    private val s3Template: S3Template,
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
@@ -25,7 +29,31 @@ class TehtavapankkiService(
     @Value("\${kitu.kotoutumiskoulutus.koealusta.baseurl}")
     lateinit var koealustaBaseUrl: String
 
+    @Value("\${kitu.kotoutumiskoulutus.tehtavapankki.bucket}")
+    lateinit var bucket: String
+
     private val restClient by lazy { restClientBuilder.baseUrl(koealustaBaseUrl).build() }
+
+    /**
+     * 1. Replaces white spaces with underscore.
+     * 2. Replaces any character that isn't a number, letter or underscore with nothing.
+     * 3. Takes first 128 characters from the name-string
+     */
+    fun sanitizeFilename(string: String) =
+        string
+            .replace(' ', '_')
+            .replace(Regex("\\W+"), "")
+            .take(128)
+
+    fun uploadTehtavapankki(response: TehtavapankkiResponse) {
+        val now = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
+        response.questionbanks.forEachIndexed { index, (courseid, coursename, xml) ->
+            val sanitizedCoursename = sanitizeFilename(coursename)
+            val filename = "$courseid-$sanitizedCoursename/$now-$index.xml"
+            val stream = xml.byteInputStream(Charsets.UTF_8)
+            s3Template.upload(bucket, filename, stream)
+        }
+    }
 
     fun importTehtavapankki() =
         logger
@@ -51,6 +79,8 @@ class TehtavapankkiService(
                 event
                     .add("request.token" to koealustaToken)
                     .addHttpResponse(PeerService.Koealusta, uri = "/webservice/rest/server.php", response)
+
+                uploadTehtavapankki(response.body!!)
             }.apply {
                 addDefaults("koealusta.importTehtavapankki")
                 addDatabaseLogs()
