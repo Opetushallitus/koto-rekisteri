9bbde83c5d56e9c625fecf2c32e25f4e1ecd4d0a
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2025-01-31 12:08:33 +0200

    feat: add EnableAsync and fix tests

--- server/src/main/kotlin/fi/oph/kitu/KituApplication.kt
+++ server/src/main/kotlin/fi/oph/kitu/KituApplication.kt
@@ -2,8 +2,10 @@ package fi.oph.kitu
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
 @SpringBootApplication
+@EnableAsync
 class KituApplication
 
 fun main(args: Array<String>) {
