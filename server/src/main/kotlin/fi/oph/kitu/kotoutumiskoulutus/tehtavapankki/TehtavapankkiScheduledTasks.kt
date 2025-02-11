88def71b1dc3afbfed357d53aba4dd30e921bdee
Inari Tommiska <ilari.tommiska@reaktor.com>
2025-02-11 15:17:20 +0200

    don't initialize s3template when running tests on ci

--- server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/tehtavapankki/TehtavapankkiScheduledTasks.kt
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/tehtavapankki/TehtavapankkiScheduledTasks.kt
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 
 @Configuration
+@Profile("!ci & !e2e")
 @ConditionalOnProperty(name = ["kitu.kotoutumiskoulutus.koealusta.scheduling.enabled"], matchIfMissing = false)
 class TehtavapankkiScheduledTasks {
     @Value("\${kitu.kotoutumiskoulutus.koealusta.scheduling.importTehtavapankki.schedule}")
