95a52eb37b38ad58192845fc0bf71fa0a42596b0
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-02-12 15:04:23 +0200

    feat(scripts): run otel collector when starting tmux sess

--- scripts/start_tmux.sh
+++ scripts/start_tmux.sh
@@ -30,24 +30,29 @@ WINDOW="database"
 tmux rename-window -t $SESS_NAME:0 "$WINDOW"
 tmux send-keys -t $SESS_NAME:"$WINDOW.0" "$REPO_ROOT/scripts/start_database_docker.sh" C-m
 
-# Window 1:idea
+# Window 1: OpenTelemetry collector
+WINDOW="otel-exporter"
+tmux new-window -t $SESS_NAME -n "$WINDOW"
+tmux send-keys -t $SESS_NAME:"$WINDOW" "$REPO_ROOT/scripts/start_otel_collector.sh" C-m
+
+# Window 2:idea
 WINDOW="idea"
 tmux new-window -t $SESS_NAME -n "idea"
 tmux send-keys -t $SESS_NAME:"idea" "idea $REPO_ROOT" C-m
 
-# Window 2:springboot
+# Window 3:springboot
 WINDOW="springboot"
 tmux new-window -t $SESS_NAME -n "$WINDOW"
 tmux send-keys -t $SESS_NAME:"$WINDOW" "$REPO_ROOT/scripts/start_local_server.sh" C-m
 
-# Window 3:workspace
+# Window 4:workspace
 WINDOW="workspace"
 tmux new-window -t $SESS_NAME -n "$WINDOW"
 tmux send-keys -t $SESS_NAME:"$WINDOW" "cd $REPO_ROOT" C-m
 tmux send-keys -t $SESS_NAME:"$WINDOW" "git log --decorate=full --graph --all --oneline" C-m
 tmux split-window -h -t "${SESSION-}":"$WINDOW"
 tmux send-keys -t $SESS_NAME:"$WINDOW.1" "cd $REPO_ROOT" C-m
-tmux send-keys -t $SESS_NAME:"$WINDOW.1" "ls -la" C-m
+tmux send-keys -t $SESS_NAME:"$WINDOW.1" "git diff --color | cat" C-m
 tmux send-keys -t $SESS_NAME:"$WINDOW.1" "git status" C-m
 
 tmux attach
