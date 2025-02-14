99b768311b9905fff0c3128169f4c25bd3e94d76
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-02-14 17:27:28 +0200

    randomize suoritustiedot mock

--- server/src/main/kotlin/fi/oph/kitu/dev/CreateMockDataController.kt
+++ server/src/main/kotlin/fi/oph/kitu/dev/CreateMockDataController.kt
@@ -1,7 +1,8 @@
 package fi.oph.kitu.dev
 
-import fi.oph.kitu.random.generateRandomUserOid
-import fi.oph.kitu.yki.Sukupuoli
+import fi.oph.kitu.random.generateRandomOrganizationOid
+import fi.oph.kitu.random.generateRandomPerson
+import fi.oph.kitu.random.getRandomLocalDate
 import fi.oph.kitu.yki.Tutkintokieli
 import fi.oph.kitu.yki.Tutkintotaso
 import fi.oph.kitu.yki.suoritukset.YkiSuoritusEntity
@@ -16,9 +17,8 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.context.WebApplicationContext
-import java.time.Instant
 import java.time.LocalDate
-import java.time.format.DateTimeFormatter
+import java.time.ZoneOffset
 import kotlin.random.Random
 import kotlin.system.exitProcess
 
@@ -42,52 +42,77 @@ class CreateMockDataController(
 
     @GetMapping("/mockdata/yki/suoritus")
     fun createYkiSuoritusMockData(): Iterable<YkiSuoritusEntity> {
-        val datePattern = "yyyy-MM-dd"
-        val dateFormatter = DateTimeFormatter.ofPattern(datePattern)
-        val mockSuoritus =
-            YkiSuoritusEntity(
-                id = null,
-                suorittajanOID = generateRandomUserOid(),
-                hetu = "010180-9026",
-                sukupuoli = Sukupuoli.N,
-                sukunimi = "Öhman-Testi",
-                etunimet = "Ranja Testi",
-                kansalaisuus = "EST",
-                katuosoite = "Testikuja 5",
-                postinumero = "40100",
-                postitoimipaikka = "Testilä",
-                email = "testi@testi.fi",
-                suoritusId = 183424,
-                lastModified = Instant.parse("2024-10-30T13:53:56Z"),
-                tutkintopaiva = LocalDate.parse("2024-09-01", dateFormatter),
-                tutkintokieli = Tutkintokieli.FIN,
-                tutkintotaso = Tutkintotaso.YT,
-                jarjestajanTunnusOid = "1.2.246.562.10.14893989377",
-                jarjestajanNimi = "Jyväskylän yliopisto, Soveltavan kielentutkimuksen keskus",
-                arviointipaiva = LocalDate.parse("2024-11-14", dateFormatter),
-                tekstinYmmartaminen = 5,
-                kirjoittaminen = 4,
-                rakenteetJaSanasto = 3,
-                puheenYmmartaminen = 1,
-                puhuminen = 2,
-                yleisarvosana = 3,
-                tarkistusarvioinninSaapumisPvm = LocalDate.parse("2024-10-01", dateFormatter),
-                tarkistusarvioinninAsiatunnus = "123123",
-                tarkistusarvioidutOsakokeet = 2,
-                arvosanaMuuttui = 1,
-                perustelu = "Tarkistusarvioinnin testi",
-                tarkistusarvioinninKasittelyPvm = LocalDate.parse("2024-10-15", dateFormatter),
-            )
+        val suoritukset = mutableListOf<YkiSuoritusEntity>()
 
-        val suoritukset = mutableListOf(mockSuoritus)
         for (i in 0..1000) {
+            val randomPerson = generateRandomPerson()
+
+            val tutkintopaiva =
+                getRandomLocalDate(
+                    LocalDate.of(2000, 1, 1),
+                    LocalDate.now().minusDays(28),
+                )
+
+            val arviointipaiva =
+                getRandomLocalDate(
+                    tutkintopaiva,
+                    LocalDate.now().minusDays(21),
+                )
+            val tarkistusarvioinninSaapumisPvm =
+                getRandomLocalDate(
+                    arviointipaiva,
+                    LocalDate.now().minusDays(14),
+                )
+            val tarkistusarvioinninKasittelyPvm =
+                getRandomLocalDate(
+                    tarkistusarvioinninSaapumisPvm,
+                    LocalDate.now().minusDays(7),
+                )
+
+            val lastModified =
+                getRandomLocalDate(
+                    tarkistusarvioinninKasittelyPvm,
+                    LocalDate.now().minusDays(3),
+                ).atStartOfDay()
+                    .toInstant(ZoneOffset.UTC)
+
             suoritukset.add(
-                mockSuoritus.copy(
+                YkiSuoritusEntity(
+                    id = null,
+                    suorittajanOID = randomPerson.oppijanumero.toString(),
+                    hetu = randomPerson.hetu,
+                    sukupuoli = randomPerson.sukupuoli,
+                    sukunimi = randomPerson.sukunimi,
+                    etunimet = randomPerson.etunimet,
+                    kansalaisuus = randomPerson.kansalaisuus,
+                    katuosoite = randomPerson.kansalaisuus,
+                    postinumero = randomPerson.postinumero,
+                    postitoimipaikka = randomPerson.postitoimipaikka,
+                    email = randomPerson.email,
                     suoritusId = Random.nextInt(100000, 999999),
-                    lastModified = Instant.now(),
+                    lastModified = lastModified,
+                    tutkintopaiva = tutkintopaiva,
+                    tutkintokieli = Tutkintokieli.entries.toTypedArray().random(),
+                    tutkintotaso = Tutkintotaso.entries.random(),
+                    jarjestajanTunnusOid = generateRandomOrganizationOid().toString(),
+                    jarjestajanNimi = "${randomPerson.postitoimipaikka}n yliopisto",
+                    arviointipaiva = arviointipaiva,
+                    tekstinYmmartaminen = (1..5).random(),
+                    kirjoittaminen = (1..5).random(),
+                    rakenteetJaSanasto = (1..5).random(),
+                    puheenYmmartaminen = (1..5).random(),
+                    puhuminen = (1..5).random(),
+                    yleisarvosana = (1..5).random(),
+                    tarkistusarvioinninSaapumisPvm = tarkistusarvioinninSaapumisPvm,
+                    tarkistusarvioinninAsiatunnus = (0..9999999999999).random().toString(),
+                    tarkistusarvioidutOsakokeet = 2,
+                    arvosanaMuuttui = 1,
+                    perustelu = listOf("Erinomainen", "Hyvä", "Ihan hyvä", "Tyydyttävä", "Huono").random(),
+                    tarkistusarvioinninKasittelyPvm = tarkistusarvioinninKasittelyPvm,
                 ),
             )
         }
+
         return suoritusRepository.saveAll(suoritukset)
     }
 }
--- server/src/main/kotlin/fi/oph/kitu/random/OidGenerator.kt
+++ server/src/main/kotlin/fi/oph/kitu/random/OidGenerator.kt
@@ -8,11 +8,15 @@ import org.ietf.jgss.Oid
  * Note: Node class 240 is the correct class for users,
  * but these values should not be used in production.
  */
-fun generateRandomUserOid(): String =
-    "1.2.246.562.240.${(0..99999999999)
-        .random()
-        .toString()
-        .padStart(11, '0')}"
+fun generateRandomUserOid() =
+    fi.oph.kitu.Oid(
+        Oid(
+            "1.2.246.562.240.${(0..99999999999)
+                .random()
+                .toString()
+                .padStart(11, '0')}",
+        ),
+    )
 
 /**
  * Generates random user OID under node class 1.2.246.562.100.
