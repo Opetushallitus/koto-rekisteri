9e773fa6a595bbea1893a4ffd0185ba65ce3aa36
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-02-14 14:22:50 +0200

    unit tests for oid

--- server/src/test/kotlin/fi/oph/kitu/random/OidGeneratorTests.kt
+++ server/src/test/kotlin/fi/oph/kitu/random/OidGeneratorTests.kt
@@ -1,4 +1,34 @@
 package fi.oph.kitu.random
 
+import fi.oph.kitu.random.generateRandomUserOid
+import org.junit.jupiter.api.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+
 class OidGeneratorTests {
+    @Test
+    fun `generated random user OID`() {
+        val oid = generateRandomUserOid()
+
+        assertNotNull(oid)
+        assertTrue(oid.isNotEmpty())
+        assertTrue(
+            oid.matches(
+                """^1\.2\.246\.562\.240\.\d{11}$""".toRegex(),
+            ),
+        )
+    }
+
+    @Test
+    fun `generated random organization OID`() {
+        val oid = generateRandomOrganizationOid().toString()
+
+        assertNotNull(oid)
+        assertTrue(oid.isNotEmpty())
+        assertTrue(
+            oid.matches(
+                """^1\.2\.246\.562\.100\.\d{11}$""".toRegex(),
+            ),
+        )
+    }
 }
