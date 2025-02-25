e8733f3d824178871186c51ac828703c4f60026d
Inari Tommiska <inari.tommiska@reaktor.com>
2025-02-25 16:00:28 +0200

    generic enum converter for future-proofing

--- server/src/main/kotlin/fi/oph/kitu/webmvc/WebConfig.kt
+++ server/src/main/kotlin/fi/oph/kitu/webmvc/WebConfig.kt
@@ -1,16 +1,21 @@
 package fi.oph.kitu.webmvc
 
-import fi.oph.kitu.yki.suoritukset.StringToYkiSuoritusColumnConverter
+import fi.oph.kitu.yki.suoritukset.YkiSuoritusColumn
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig {
-    @Configuration
-    class WebConfig : WebMvcConfigurer {
-        override fun addFormatters(registry: FormatterRegistry) {
-            registry.addConverter(StringToYkiSuoritusColumnConverter())
+class WebConfig : WebMvcConfigurer {
+    override fun addFormatters(registry: FormatterRegistry) {
+        registry.addEnumConverter<YkiSuoritusColumn>()
+    }
+
+    final inline fun <reified E : Enum<E>> FormatterRegistry.addEnumConverter(ignoreCase: Boolean = true) {
+        this.addConverter(String::class.java, E::class.java) { source ->
+            enumValues<E>().find {
+                it.name.equals(source, ignoreCase)
+            }
         }
     }
 }
