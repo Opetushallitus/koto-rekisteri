2d066cf6372600cf6e19b53bc44285d8aaca640c
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-02-26 16:31:18 +0200

    feat(koto): sort table

--- server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiSuoritusRepository.kt
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiSuoritusRepository.kt
@@ -1,7 +1,10 @@
 package fi.oph.kitu.kotoutumiskoulutus
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface KielitestiSuoritusRepository : CrudRepository<KielitestiSuoritus, Int>
+interface KielitestiSuoritusRepository :
+    CrudRepository<KielitestiSuoritus, Int>,
+    PagingAndSortingRepository<KielitestiSuoritus, Int>
