89e2f0b5288e6b05f6a0f385f6ac0118f22d53f8
Inari Tommiska <inari.tommiska@reaktor.com>
2025-03-03 12:53:35 +0200

    allow cron schedules for tehtavapankki import

--- server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/tehtavapankki/TehtavapankkiScheduledTasks.kt
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/tehtavapankki/TehtavapankkiScheduledTasks.kt
@@ -3,6 +3,7 @@ package fi.oph.kitu.kotoutumiskoulutus.tehtavapankki
 import com.github.kagkarlsson.scheduler.task.Task
 import com.github.kagkarlsson.scheduler.task.helper.Tasks
 import com.github.kagkarlsson.scheduler.task.schedule.Schedules
+import com.github.kagkarlsson.scheduler.task.schedule.Schedules.UnrecognizableSchedule
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
@@ -14,13 +15,17 @@ import org.springframework.context.annotation.Profile
 @ConditionalOnProperty(name = ["kitu.kotoutumiskoulutus.koealusta.scheduling.enabled"], matchIfMissing = false)
 class TehtavapankkiScheduledTasks {
     @Value("\${kitu.kotoutumiskoulutus.koealusta.scheduling.importTehtavapankki.schedule}")
-    lateinit var tehtavapankkiImportSchedule: String
+    var tehtavapankkiImportSchedule: String? = null
 
     @Bean
     fun dailyImportKotoTehtavapankki(tehtavapankkiService: TehtavapankkiService): Task<Void> =
         Tasks
             .recurring(
                 "Koto-import-tehtavapankki",
-                Schedules.parseSchedule(tehtavapankkiImportSchedule),
+                try {
+                    Schedules.parseSchedule(tehtavapankkiImportSchedule)
+                } catch (_: UnrecognizableSchedule) {
+                    Schedules.cron(tehtavapankkiImportSchedule)
+                },
             ).execute { _, _ -> tehtavapankkiService.importTehtavapankki() }
 }
