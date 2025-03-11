32de817d400e51888de0dfbd55d13e380d143573
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-03-11 15:50:13 +0200

    feat(yki): errors - page

--- server/src/main/kotlin/fi/oph/kitu/webmvc/WebConfig.kt
+++ server/src/main/kotlin/fi/oph/kitu/webmvc/WebConfig.kt
@@ -3,6 +3,7 @@ package fi.oph.kitu.webmvc
 import fi.oph.kitu.kotoutumiskoulutus.KielitestiSuoritusColumn
 import fi.oph.kitu.yki.arvioijat.YkiArvioijaColumn
 import fi.oph.kitu.yki.suoritukset.YkiSuoritusColumn
+import fi.oph.kitu.yki.suoritukset.error.YkiSuoritusErrorColumn
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.FormatterRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -12,6 +13,7 @@ class WebConfig : WebMvcConfigurer {
     override fun addFormatters(registry: FormatterRegistry) {
         registry.apply {
             addEnumConverter<YkiSuoritusColumn>()
+            addEnumConverter<YkiSuoritusErrorColumn>()
             addEnumConverter<YkiArvioijaColumn>()
             addEnumConverter<KielitestiSuoritusColumn>()
         }
--- server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorRepository.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorRepository.kt
@@ -1,7 +1,10 @@
 package fi.oph.kitu.yki.suoritukset.error
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface YkiSuoritusErrorRepository : CrudRepository<YkiSuoritusErrorEntity, Long>
+interface YkiSuoritusErrorRepository :
+    CrudRepository<YkiSuoritusErrorEntity, Long>,
+    PagingAndSortingRepository<YkiSuoritusErrorEntity, Long>
