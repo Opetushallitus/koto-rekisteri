30a411f2f3a955e771ce4356db0fa9db28b045e6
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-03-20 16:26:48 +0200

    feat(yki): add keyValues - column

--- server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorMappingService.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorMappingService.kt
@@ -1,11 +1,14 @@
 package fi.oph.kitu.yki.suoritukset.error
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import fi.oph.kitu.csvparsing.CsvExportError
 import org.springframework.stereotype.Service
 import java.time.Instant
 
 @Service
-class YkiSuoritusErrorMappingService {
+class YkiSuoritusErrorMappingService(
+    private val objectMapper: ObjectMapper, 
+) {
     final inline fun <reified T> convertToEntityIterable(
         iterable: Iterable<CsvExportError>,
         created: Instant = Instant.now(),
@@ -14,14 +17,14 @@ class YkiSuoritusErrorMappingService {
     final inline fun <reified T> convertToEntity(
         data: CsvExportError,
         created: Instant = Instant.now(),
-    ): YkiSuoritusErrorEntity =
-        YkiSuoritusErrorEntity(
-            id = null,
-            message = data::class.simpleName!!,
-            context = data.context!!,
-            exceptionMessage = data.exception.message!!,
-            stackTrace = data.exception.stackTrace!!.toString(),
-            created = created,
-            sourceType = T::class.simpleName!!,
-        )
+    ) = YkiSuoritusErrorEntity(
+        id = null,
+        message = data::class.simpleName!!,
+        context = data.context!!,
+        exceptionMessage = data.exception.message!!,
+        stackTrace = data.exception.stackTrace!!.joinToString("\n"),
+        created = created,
+        keyValues = objectMapper.writeValueAsString(data.keyValues),
+        sourceType = T::class.simpleName!!,
+    )
 }
