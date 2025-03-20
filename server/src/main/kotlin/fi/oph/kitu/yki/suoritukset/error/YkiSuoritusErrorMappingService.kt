1a58ceda6db00e071580a99a58659d5591722681
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-03-20 16:26:48 +0200

    feat(yki): add keyValues - column

--- server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorMappingService.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorMappingService.kt
@@ -1,11 +1,14 @@
 package fi.oph.kitu.yki.suoritukset.error
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import fi.oph.kitu.csvparsing.CsvExportError
 import org.springframework.stereotype.Service
 import java.time.Instant
 
 @Service
-class YkiSuoritusErrorMappingService {
+class YkiSuoritusErrorMappingService(
+    private val objectMapper: ObjectMapper,
+) {
     fun convertToEntityIterable(
         iterable: Iterable<CsvExportError>,
         created: Instant = Instant.now(),
@@ -21,5 +24,6 @@ class YkiSuoritusErrorMappingService {
         exceptionMessage = data.exception.message!!,
         stackTrace = data.exception.stackTrace!!.joinToString("\n"),
         created = created,
+        keyValues = objectMapper.writeValueAsString(data.keyValues),
     )
 }
