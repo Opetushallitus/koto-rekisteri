126d3720d5efe734e2f545c02057d9127e129ad9
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-03-26 10:48:45 +0200

    feat(yki-error): refactor sql script

---
+++ server/src/main/resources/db/migration/V28__refactor_yki_suoritus_error_table.sql
@@ -0,0 +1,22 @@
+TRUNCATE TABLE yki_suoritus_error;
+DROP TABLE yki_suoritus_error;
+
+CREATE TABLE yki_suoritus_error
+(
+    id                  SERIAL PRIMARY KEY,
+    oid                 TEXT,
+    hetu                TEXT,
+    nimi                TEXT,
+    last_modified       TIMESTAMP WITH TIME ZONE,
+    virheellinen_kentta TEXT,
+    virheellinen_arvo   TEXT,
+    virheellinen_rivi   TEXT                        NOT NULL,
+    virheen_rivinumero  INTEGER                     NOT NULL,
+    virheen_luontiaika  TIMESTAMP WITH TIME ZONE    NOT NULL,
+
+    CONSTRAINT unique_suoritus_error UNIQUE
+    (
+        virheellinen_rivi,
+        virheen_rivinumero
+    )
+)
