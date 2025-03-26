19b4fb136dd7b9342785697f427b4d1669ff1f70
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-03-26 16:42:43 +0200

    first version, convertToEntity handles conversion correctly

--- server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorMappingService.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorMappingService.kt
@@ -6,6 +6,20 @@ import java.time.Instant
 
 @Service
 class YkiSuoritusErrorMappingService {
+    private inline fun <reified T> List<Pair<String, Any>>.getValueByKey(key: String?) =
+        this
+            .filter { it.first == key }
+            .map { it.second }
+            .filterIsInstance<T>()
+            .first()
+
+    private inline fun <reified T> List<Pair<String, Any>>.getValueOrNullByKey(key: String?) =
+        this
+            .filter { it.first == key }
+            .map { it.second }
+            .filterIsInstance<T>()
+            .firstOrNull()
+
     fun convertToEntityIterable(
         iterable: Iterable<CsvExportError>,
         created: Instant = Instant.now(),
@@ -14,12 +28,20 @@ class YkiSuoritusErrorMappingService {
     fun convertToEntity(
         data: CsvExportError,
         created: Instant = Instant.now(),
-    ) = YkiSuoritusErrorEntity(
-        id = null,
-        message = data::class.simpleName!!,
-        context = data.context!!,
-        exceptionMessage = data.exception.message!!,
-        stackTrace = data.exception.stackTrace!!.toString(),
-        created = created,
-    )
+    ): YkiSuoritusErrorEntity {
+        val csv = data.context!!.split(",")
+
+        return YkiSuoritusErrorEntity(
+            id = null,
+            oid = csv[0],
+            hetu = csv[1],
+            nimi = csv[3] + " " + csv[4],
+            lastModified = runCatching { Instant.parse(csv[11]) }.getOrNull(),
+            virheellinenKentta = data.keyValues.getValueOrNullByKey<String>("field"),
+            virheellinenArvo = data.keyValues.getValueOrNullByKey<Any>("value").toString(),
+            virheellinenRivi = data.context,
+            virheenRivinumero = data.keyValues.getValueByKey<Int>("lineNumber"),
+            virheenLuontiaika = Instant.now(),
+        )
+    }
 }
