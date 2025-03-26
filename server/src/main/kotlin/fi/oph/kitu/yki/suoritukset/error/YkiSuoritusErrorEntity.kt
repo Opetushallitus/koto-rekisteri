ff9d9f9c6238b66720edf501e67c727d15909f46
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-03-26 10:40:20 +0200

    refactor(yki): YkiSuoritusErrorEntity

--- server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorEntity.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/error/YkiSuoritusErrorEntity.kt
@@ -4,13 +4,22 @@ import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.time.Instant
 
+/**
+ * Represents Yki Suoritus Error Entity.
+ *
+ * virheelllinenRivi and virheenRivinumero forms a unique constraint.
+ */
 @Table(name = "yki_suoritus_error")
 data class YkiSuoritusErrorEntity(
     @Id
     val id: Long?,
-    val message: String,
-    val context: String,
-    val exceptionMessage: String,
-    val stackTrace: String,
-    val created: Instant,
+    val oid: String?,
+    val hetu: String?,
+    val nimi: String?,
+    val lastModified: Instant?,
+    val virheellinenKentta: String?,
+    val virheellinenArvo: String?,
+    val virheellinenRivi: String,
+    val virheenRivinumero: Int,
+    val virheenLuontiaika: Instant,
 )
