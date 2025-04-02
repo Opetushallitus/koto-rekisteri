93d9f5e930717aca206e97b97b468c389b9fe884
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2025-04-02 17:52:17 +0300

    wip oppija mess

--- server/src/main/resources/db/migration/V29__create_koto_suoritus_error_table.sql
+++ server/src/main/resources/db/migration/V29__create_koto_suoritus_error_table.sql
@@ -1,13 +1,6 @@
 CREATE TABLE koto_suoritus_error
 (
-    id                  SERIAL PRIMARY KEY,
-    suorittajan_oid     TEXT,
-    hetu                TEXT,
-    nimi                TEXT,
-    last_modified       TIMESTAMP WITH TIME ZONE,
-    virheellinen_kentta TEXT,
-    virheellinen_arvo   TEXT,
-    virheellinen_rivi   TEXT                     NOT NULL,
-    virheen_rivinumero  INTEGER                  NOT NULL,
-    virheen_luontiaika  TIMESTAMP WITH TIME ZONE NOT NULL
+    id              SERIAL PRIMARY KEY,
+    suorittajan_oid TEXT,
+    suorituksen_oid TEXT
 )
