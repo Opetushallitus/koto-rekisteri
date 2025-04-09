e3fe4fc298a64b9b4a8a03799db32de6d7c1661c
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-04-09 13:41:31 +0300

    test(e2e/yki-error): add run import to the test

--- e2e/models/dbSchedulerPage.ts
+++ e2e/models/dbSchedulerPage.ts
@@ -1,5 +1,5 @@
 import BasePage from "./BasePage"
-import { Page } from "@playwright/test"
+import { Locator, Page } from "@playwright/test"
 import { Config } from "../config"
 
 export default class DbSchedulerPage extends BasePage {
@@ -9,5 +9,29 @@ export default class DbSchedulerPage extends BasePage {
 
   async open() {
     await this.goto("/db-scheduler")
+  }
+
+  getRowYkiImport() {
+    return this.page
+      .getByText("YKI-import", { exact: true })
+      .locator("..")
+      .locator("..")
+      .locator("..") // go up 3 levels
+  }
+
+  getRerunButton(row: Locator) {
+    return row.getByRole("button", { name: "Rerun" })
+  }
+
+  getRunButton(row: Locator) {
+    return row.getByRole("button", { name: "Run" })
+  }
+
+  getRefreshButton() {
+    return this.page.getByRole("button", { name: "Refresh" })
+  }
+
+  getNotificationIndicator() {
+    return this.getRefreshButton().locator("..").locator("text=1")
   }
 }
