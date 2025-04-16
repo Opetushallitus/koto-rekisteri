e43732c491803dda6621ac793fa65e7b5aba198c
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-04-16 10:39:31 +0300

    Revert "test(e2e/yki-error): add run import to the test"
    This reverts commit e3fe4fc298a64b9b4a8a03799db32de6d7c1661c.

--- e2e/tests/yki/yki-suoritukset-error.spec.ts
+++ e2e/tests/yki/yki-suoritukset-error.spec.ts
@@ -5,41 +5,17 @@ const fs = node_fs.promises
 describe('"YKI Suoritukset Error" -page', () => {
   beforeEach(async ({ db, basePage, ykiSuoritusError }) => {
     await db.withEmptyDatabase()
+
+    await ykiSuoritusError.insert(db, "missingOid")
+
+    await basePage.login()
   })
 
   test("yki suoritukset error page is navigable via suoritukset - page", async ({
     page,
-    basePage,
-    dbSchedulerPage,
     ykiSuorituksetPage,
     ykiSuorituksetErrorPage,
   }) => {
-    // Part 1 - Run import
-
-    await basePage.login()
-    await dbSchedulerPage.open()
-
-    // Find correct row
-    const row = dbSchedulerPage.getRowYkiImport()
-
-    // Expect there is a failed run, so Rerun
-    const rerunButton = dbSchedulerPage.getRerunButton(row)
-    await rerunButton.click()
-
-    // The same button should have now text Run
-    const runButton = dbSchedulerPage.getRunButton(row)
-    await expect(runButton).toBeVisible(/*{ timeout: 5000 }*/)
-
-    // Refresh the page
-    const refreshButton = dbSchedulerPage.getRefreshButton()
-    await refreshButton.click()
-
-    // There should be now a notification about the error
-    // This is also an indicator, that the import was run.
-    const notification = dbSchedulerPage.getNotificationIndicator()
-    await expect(notification).toBeVisible({ timeout: 10000 })
-
-    // Part 2 - Navigate to errors
     await ykiSuorituksetPage.open()
     await ykiSuorituksetPage.getErrorLink().click()
 
