f77589b7a7e706aba5fcf389a1c5d351e472ab22
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-04-17 18:31:43 +0300

    wip: attempt2 with mockito

--- server/src/test/kotlin/fi/oph/kitu/e2e/YkiServiceTests.kt
+++ server/src/test/kotlin/fi/oph/kitu/e2e/YkiServiceTests.kt
@@ -3,21 +3,25 @@ package fi.oph.kitu.e2e
 import fi.oph.kitu.dev.YkiController
 import fi.oph.kitu.yki.YkiService
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito.mock
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection
 import org.springframework.http.ResponseEntity
-import org.springframework.test.annotation.DirtiesContext
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.web.client.RestClient
 import org.testcontainers.containers.PostgreSQLContainer
 import org.testcontainers.junit.jupiter.Container
 import org.testcontainers.junit.jupiter.Testcontainers
 import java.time.Instant
+import java.time.format.DateTimeFormatter
 
+// @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+// @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest
 @Testcontainers
-// @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class YkiServiceTests(
     @Autowired val ykiService: YkiService,
     @Autowired val ykiDevController: YkiController,
@@ -32,25 +36,27 @@ class YkiServiceTests(
                 .withUrlParam("stringtype", "unspecified")!!
     }
 
+    @MockitoBean
+    @Qualifier("solkiRestClient")
+    private lateinit var solkiRestClient: RestClient
+
     // @Order(1)
     @Test
     fun `Happy path for Yki suoritukset import`() {
-        ykiDevController.setResponse(
-            endpoint = "/yki/import/suoritukset",
-            response =
-                ResponseEntity.ok(
-                    """
-                    "1.2.246.562.24.20281155246","010180-9026","N","Öhman-Testi","Ranja Testi","EST","Testikuja 5","40100","Testilä","testi@testi.fi",183424,2024-12-13T07:10:13Z,2024-09-01,"fin","YT","1.2.246.562.10.14893989377","Jyväskylän yliopisto, Soveltavan kielentutkimuksen keskus",2024-12-13,5,5,,5,5,,2024-12-14,"OPH-5000-1234",1,1,"Suorituksesta jäänyt viimeinen tehtävä arvioimatta. Arvioinnin jälkeen puhumisen taitotasoa 6.",2024-12-14
-                    "1.2.246.562.24.59267607404","010116A9518","M","Kivinen-Testi","Petro Testi","EST","Testikuja 10","40100","Testinsuu","testi.petro@testi.fi",183425,2024-12-13T06:54:38Z,2024-09-01,"fin","YT","1.2.246.562.10.14893989377","Jyväskylän yliopisto, Soveltavan kielentutkimuksen keskus",2024-12-09,6,6,,6,6,,2024-11-01,"OPH-5002-2024",4,0,,2024-12-09
-                    ,"010180-9026","N","Öhman-Testi","Ranja Testi","EST","Testikuja 5","40100","Testilä","testi@testi.fi",183424,2024-10-30T13:53:56Z,2024-09-01,"fin","YT","1.2.246.562.10.14893989377","Jyväskylän yliopisto, Soveltavan kielentutkimuksen keskus",2024-11-14,5,5,,5,5,,,,0,0,,
-                    "1.2.246.562.24.59267607404","010116A9518","CORRUPTED","Kivinen-Testi","Petro Testi","","Testikuja 10","40100","Testinsuu","testi.petro@testi.fi",183425,2024-10-30T13:55:09Z,2024-09-01,"fin","YT","1.2.246.562.10.14893989377","Jyväskylän yliopisto, Soveltavan kielentutkimuksen keskus",2024-10-30,6,6,,6,6,,,,0,0,,
-                    """.trimIndent(),
-                ),
-        )
+        val request = mock(RestClient.RequestHeadersUriSpec::class.java)
+        val responseSpec = mock(RestClient.ResponseSpec::class.java)
+        val entityResponse = mock(ResponseEntity::class.java) as ResponseEntity<String>
 
-        assertThrows<RuntimeException> {
-            ykiService.importYkiSuoritukset(Instant.now())
-        }
+        val from = Instant.now()
+        val url = "suoritukset?m=${DateTimeFormatter.ISO_INSTANT.format(from)}"
+
+        whenever(solkiRestClient.get()).thenReturn(request)
+        whenever(request.uri("/some-endpoint")).thenReturn(request)
+        whenever(request.retrieve()).thenReturn(responseSpec)
+        whenever(responseSpec.toEntity(String::class.java)).thenReturn(entityResponse)
+        whenever(entityResponse.body).thenReturn("mocked response")
+
+        ykiService.importYkiSuoritukset(from)
     }
 
     // @Order(2)
