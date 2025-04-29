da1e87f1c72bb909b814da6ee1fa89e8fea731c5
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-04-29 12:53:07 +0300

    test(yki-suoritus-error): test and wrap repository saveAll with @WithSpan

--- server/src/test/kotlin/fi/oph/kitu/yki/YkiSuoritusErrorTests.kt
+++ server/src/test/kotlin/fi/oph/kitu/yki/YkiSuoritusErrorTests.kt
@@ -87,8 +87,45 @@ class YkiSuoritusErrorTests(
         assertEquals(0, serializationErrors.size)
     }
 
+    @Test fun `saving errors work correctly`() {
+        // Arrange
+        val errors =
+            listOf(
+                SimpleCsvExportError(
+                    lineNumber = 2,
+                    context =
+                        """
+                        ,\"010180-9026\",\"N\",\"Öhman-Testi\",\"Ranja Testi\",\"EST\",\"Testikuja 5\",\"40100\",\"Testilä\",\"testi@testi.fi\",183424,2024-10-30T13:53:56Z,2024-09-01,\"fin\",\"YT\",\"1.2.246.562.10.14893989377\",\"Jyväskylän yliopisto, Soveltavan kielentutkimuksen keskus\",2024-11-14,5,5,,5,5,,,,0,0,,
+                        """.trimIndent(),
+                    exception =
+                        RuntimeException(
+                            """
+                            Cannot construct instance of `fi.oph.kitu.yki.suoritukset.YkiSuoritusCsv`, problem: Parameter specified as non-null is null: method fi.oph.kitu.yki.suoritukset.YkiSuoritusCsv.<init>, parameter suorittajanOID
+                                at [Source: (StringReader); line: 3, column: 270]
+                            """.trimIndent(),
+                        ),
+                ),
+            )
+
+        // Act
+        repository.save(
+            // Existing error
+            generateRandomYkiSuoritusErrorEntity().copy(
+                virheenLuontiaika = Instant.parse("2025-03-06T10:50:00.00Z"),
+            ),
+        )
+
+        // Assert
+        val errorsInDatabase = repository.findAll()
+        assertEquals(2, errorsInDatabase.count())
+
+        val spans = inMemorySpanExporter.finishedSpanItems
+        val span = spans.find { it.name == "CustomYkiSuoritusErrorRepositoryImpl.saveAll" }
+        assertNotNull(spans)
+    }
+
     @Test
-    fun `new csv error will be appended to database`() {
+    fun `handleErrors will append new csv errors to the database`() {
         // Arrange
         val errors =
             listOf(
