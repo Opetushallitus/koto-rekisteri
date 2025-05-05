e9e503f9591e56074f5df77afc634cdd8cfbc072
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2025-05-05 15:38:41 +0300

    Revert "Use simple local pretty log formatter"
    This reverts commit 47ac221db6fdd2986318e83cb80fdce82c107222.

--- scripts/start_local_server.sh
+++ scripts/start_local_server.sh
@@ -8,8 +8,10 @@ get_secret() {
   aws secretsmanager get-secret-value --secret-id "$1" --output text --query SecretString
 }
 
+require_command humanlog
+
 require_env SPRING_PROFILES_ACTIVE
 require_env KOTLIN_POST_PROCESS_FILE
 
 cd "$REPO_ROOT"/server
-./mvnw spring-boot:run
+./mvnw spring-boot:run | humanlog --truncate-length 9999
