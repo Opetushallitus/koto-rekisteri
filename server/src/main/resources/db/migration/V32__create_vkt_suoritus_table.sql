a3f405d2d42a1c802d94c653073d09cb8689a5cf
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2025-05-07 13:54:46 +0300

    Add separate VKT tutkinto & osakoe tables

--- server/src/main/resources/db/migration/V32__create_vkt_suoritus_table.sql
+++ server/src/main/resources/db/migration/V32__create_vkt_suoritus_table.sql
@@ -1,6 +1,15 @@
 CREATE TYPE vkt_tutkintokieli AS ENUM ('FIN', 'SWE');
-CREATE TYPE vkt_taitotaso AS ENUM ('Erinomainen', 'HyväTyydyttävä');
+CREATE TYPE vkt_taitotaso AS ENUM ('Erinomainen', 'HyväJaTyydyttävä');
 CREATE TYPE vkt_arvosana AS ENUM ('Erinomainen', 'Hyvä', 'Tyydyttävä', 'Hylätty');
+CREATE TYPE vkt_osakokeen_tyyppi AS ENUM (
+    'Puhuminen',
+    'Kirjoittaminen',
+    'PuheenYmmärtäminen',
+    'TekstinYmmärtäminen');
+CREATE TYPE vkt_tutkinnon_tyyppi AS ENUM (
+    'SuullinenTaito',
+    'KirjallinenTaito',
+    'YmmärtämisenTaito');
 
 CREATE TABLE vkt_suoritus
 (
@@ -10,20 +19,28 @@ CREATE TABLE vkt_suoritus
     etunimi                             TEXT                NOT NULL,
     sukunimi                            TEXT                NOT NULL,
     tutkintokieli                       vkt_tutkintokieli   NOT NULL,
-    tutkintopaiva                       DATE                NOT NULL,
     ilmoittautumisen_tila               TEXT                NOT NULL,
-    ilmoittautunut_puhuminen            BOOLEAN             NOT NULL,
-    ilmoittautunut_puheen_ymmartaminen  BOOLEAN             NOT NULL,
-    ilmoittautunut_kirjoittaminen       BOOLEAN             NOT NULL,
-    ilmoittautunut_tekstin_ymmartaminen BOOLEAN             NOT NULL,
     suorituskaupunki                    TEXT                NOT NULL,
     taitotaso                           vkt_taitotaso       NOT NULL,
-    suorituksen_vastaanottaja           TEXT,
-    puhuminen                           vkt_arvosana,
-    puheen_ymmartaminen                 vkt_arvosana,
-    kirjoittaminen                      vkt_arvosana,
-    tekstin_ymmartaminen                vkt_arvosana,
-    suullinen_taito                     vkt_arvosana,
-    kirjallinen_taito                   vkt_arvosana,
-    ymmartamisen_taito                  vkt_arvosana
+    suorituksen_vastaanottaja           TEXT
+);
+
+CREATE TABLE vkt_osakoe
+(
+    id              SERIAL PRIMARY KEY,
+    suoritus_id     INT                     REFERENCES vkt_suoritus(id),
+    tyyppi          vkt_osakokeen_tyyppi    NOT NULL,
+    tutkintopaiva   DATE                    NOT NULL,
+    arviointipaiva  DATE,
+    arvosana        vkt_arvosana
+);
+
+CREATE TABLE vkt_tutkinto
+(
+    id              SERIAL PRIMARY KEY,
+    suoritus_id     INT                     REFERENCES vkt_suoritus(id),
+    tyyppi          vkt_tutkinnon_tyyppi    NOT NULL,
+    tutkintopaiva   DATE                    NOT NULL,
+    arviointipaiva  DATE,
+    arvosana        vkt_arvosana
 )
