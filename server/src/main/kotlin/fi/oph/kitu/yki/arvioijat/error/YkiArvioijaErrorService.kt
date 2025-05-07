bd2dca3303b53c72bdf47b4e51883d673648d535
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-05-07 10:51:21 +0300

    feat(yki-arvioija-error): add yki arvioija error view

--- server/src/main/kotlin/fi/oph/kitu/yki/arvioijat/error/YkiArvioijaErrorService.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/arvioijat/error/YkiArvioijaErrorService.kt
@@ -1,10 +1,14 @@
 package fi.oph.kitu.yki.arvioijat.error
 
+import fi.oph.kitu.SortDirection
 import fi.oph.kitu.csvparsing.CsvExportError
 import fi.oph.kitu.csvparsing.setSerializationErrorToAttributes
+import fi.oph.kitu.findAllSorted
+import fi.oph.kitu.logging.AuditLogger
 import fi.oph.kitu.logging.setAttribute
 import fi.oph.kitu.logging.use
 import io.opentelemetry.api.trace.Tracer
+import io.opentelemetry.instrumentation.annotations.WithSpan
 import org.springframework.stereotype.Service
 
 @Service
@@ -12,7 +16,11 @@ class YkiArvioijaErrorService(
     private val mappingService: YkiArvioijaErrorMappingService,
     private val repository: YkiArvioijaErrorRepository,
     private val tracer: Tracer,
+    private val auditLogger: AuditLogger,
 ) {
+    @WithSpan
+    fun countErrors(): Long = repository.count()
+
     fun handleErrors(errors: List<CsvExportError>): Boolean =
         tracer
             .spanBuilder("YkiArvioijaErrorService.handleErrors")
@@ -32,5 +40,19 @@ class YkiArvioijaErrorService(
                 }
 
                 return@use true
+            }
+
+    @WithSpan
+    fun getErrors(
+        orderBy: YkiArvioijaErrorColumn = YkiArvioijaErrorColumn.VirheenLuontiaika,
+        orderByDirection: SortDirection = SortDirection.ASC,
+    ): List<YkiArvioijaErrorEntity> =
+        repository
+            .findAllSorted(orderBy.entityName, orderByDirection)
+            .toList()
+            .also {
+                auditLogger.logAll("Yki arvioija errors viewed", it) { error ->
+                    arrayOf("arvioija.error.id" to error.id)
+                }
             }
 }
