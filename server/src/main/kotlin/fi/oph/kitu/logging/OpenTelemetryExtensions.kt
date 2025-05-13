c3c0f990b53df17c8401110243fc4495dbf54a2d
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-05-13 13:54:38 +0300

    mark TypedResult.Failure as Error in OpenTelemetry

--- server/src/main/kotlin/fi/oph/kitu/logging/OpenTelemetryExtensions.kt
+++ server/src/main/kotlin/fi/oph/kitu/logging/OpenTelemetryExtensions.kt
@@ -1,12 +1,15 @@
 package fi.oph.kitu.logging
 
+import fi.oph.kitu.setAttributesForTypedResult
 import io.opentelemetry.api.trace.Span
 import io.opentelemetry.context.Scope
 
 inline fun <T> Span.use(block: (Span) -> T): T {
     val scope: Scope = this.makeCurrent()
     return try {
-        block(this)
+        block(this).also {
+            this.setAttributesForTypedResult(it)
+        }
     } catch (e: Throwable) {
         this.recordException(e)
         throw e
