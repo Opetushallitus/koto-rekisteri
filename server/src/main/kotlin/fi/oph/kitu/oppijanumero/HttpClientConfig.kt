c187e6c286a88dba5df8d363abc2059e78a38c63
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2025-05-14 15:49:41 +0300

    Add OpenTelemetry instrumentation to java.net HttpClient

--- server/src/main/kotlin/fi/oph/kitu/oppijanumero/HttpClientConfig.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/HttpClientConfig.kt
@@ -1,5 +1,7 @@
 package fi.oph.kitu.oppijanumero
 
+import io.opentelemetry.instrumentation.javahttpclient.JavaHttpClientTelemetry
+import io.opentelemetry.sdk.OpenTelemetrySdk
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.net.CookieManager
@@ -9,10 +11,17 @@ import java.time.Duration
 @Configuration
 class HttpClientConfig {
     @Bean("oppijanumeroHttpClient")
-    fun HttpClient(): HttpClient =
-        HttpClient
-            .newBuilder()
-            .cookieHandler(CookieManager()) // sends JSESSIONID Cookie between the requests
-            .connectTimeout(Duration.ofSeconds(10))
+    fun HttpClient(openTelemetry: OpenTelemetrySdk): HttpClient {
+        val httpClient =
+            HttpClient
+                .newBuilder()
+                .cookieHandler(CookieManager()) // sends JSESSIONID Cookie between the requests
+                .connectTimeout(Duration.ofSeconds(10))
+                .build()
+
+        return JavaHttpClientTelemetry
+            .builder(openTelemetry)
             .build()
+            .newHttpClient(httpClient)
+    }
 }
