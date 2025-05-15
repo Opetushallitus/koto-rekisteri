0b4211a2cd30598c3fd28284f34532a49c062fec
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-05-15 10:36:13 +0300

    test(oppijanumero): add unit test for 4xx errors

--- server/src/test/kotlin/fi/oph/kitu/TypedResultExtensions.kt
+++ server/src/test/kotlin/fi/oph/kitu/TypedResultExtensions.kt
@@ -1,7 +1,20 @@
 package fi.oph.kitu
 
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
 /**
  * Specify that the result is expected to be a success. If it is a failure, the error will be thrown.
  */
 fun <Value, Error> TypedResult<Value, *>.mustBeSuccess(message: String = "must be success"): TypedResult<Value, Error> =
     TypedResult.Success(getOrThrow())
+
+inline fun <reified ExpectedError : Throwable> assertFailureIsThrowable(
+    typedResult: TypedResult<*, *>,
+    errorMessage: String?,
+) {
+    assertTrue(typedResult.isFailure)
+    assertTrue(typedResult is TypedResult.Failure)
+    assertTrue(typedResult.error is ExpectedError)
+    assertEquals(typedResult.error.message, errorMessage)
+}
--- server/src/test/kotlin/fi/oph/kitu/oppijanumero/OppijanumeroServiceTests.kt
+++ server/src/test/kotlin/fi/oph/kitu/oppijanumero/OppijanumeroServiceTests.kt
@@ -4,6 +4,7 @@ import HttpResponseMock
 import com.fasterxml.jackson.databind.ObjectMapper
 import fi.oph.kitu.Oid
 import fi.oph.kitu.TypedResult
+import fi.oph.kitu.assertFailureIsThrowable
 import fi.oph.kitu.logging.MockTracer
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
@@ -96,7 +97,7 @@ class OppijanumeroServiceTests {
     }
 
     @Test
-    fun `oppijanumero service returns error`() {
+    fun `oppijanumero service does not find user`() {
         // Facade
         val response: TypedResult<HttpResponse<String>, CasError> =
             TypedResult.Success(
@@ -135,5 +136,50 @@ class OppijanumeroServiceTests {
                     "123",
                 ).getOrThrow()
         }
+    }
+
+    @Test
+    fun `oppijanumero service received bad request`() {
+        // Facade
+        val response: TypedResult<HttpResponse<String>, CasError> =
+            TypedResult.Success(
+                HttpResponseMock(
+                    statusCode = 409,
+                    body =
+                        """
+                        {
+                            "timestamp": 1734962667439,
+                            "status":404,
+                            "error":"Conflict",
+                            "path":"/oppijanumerorekisteri-service/yleistunniste/hae"
+                        }
+                        """.trimIndent(),
+                ),
+            )
+        // System under test
+        val oppijanumeroService =
+            OppijanumeroServiceImpl(
+                casAuthenticatedService =
+                    CasAuthenticatedServiceMock(response),
+                objectMapper = ObjectMapper(),
+                tracer = MockTracer(),
+            )
+        oppijanumeroService.serviceUrl = "http://localhost:8080/oppijanumero-service"
+
+        val result =
+            oppijanumeroService.getOppijanumero(
+                Oppija(
+                    "Magdalena Testi",
+                    "Sallinen-Testi",
+                    "Magdalena",
+                    "010866-9260",
+                ),
+                "123",
+            )
+
+        assertFailureIsThrowable<OppijanumeroException>(
+            result,
+            "Oppijanumeron haku epäonnistui (409): Jotkin Moodle-käyttäjän '123' tunnistetiedoista (hetu, etunimet, kutsumanimi, sukunimi) ovat virheellisiä.",
+        )
     }
 }
