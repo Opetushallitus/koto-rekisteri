1d288816548735b85a54bac9ad930db4b6c7bd04
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2025-05-16 13:46:13 +0300

    WIP

--- server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiSuoritusError.kt
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiSuoritusError.kt
@@ -1,5 +1,6 @@
 package fi.oph.kitu.kotoutumiskoulutus
 
+import fi.oph.kitu.Oid
 import org.springframework.data.annotation.Id
 import org.springframework.data.relational.core.mapping.Table
 import java.time.Instant
@@ -11,6 +12,7 @@ data class KielitestiSuoritusError(
     val suorittajanOid: String?,
     val hetu: String?,
     val nimi: String,
+    val schoolOid: Oid?,
     val virheenLuontiaika: Instant,
     val viesti: String,
     val virheellinenKentta: String?,
