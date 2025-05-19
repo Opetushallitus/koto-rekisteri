dae7dbb786b378f09d0273d4c4bfe905fb38a1f3
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2025-05-19 13:06:22 +0300

    Drop old columns from koto_suoritus

---
+++ server/src/main/resources/db/migration/V36__drop_teacher_results_from_koto_suoritus.sql
@@ -0,0 +1,5 @@
+ALTER TABLE koto_suoritus
+    DROP COLUMN luetun_ymmartaminen_result_teacher,
+    DROP COLUMN kuullun_ymmartaminen_result_teacher,
+    DROP COLUMN puhe_result_teacher,
+    DROP COLUMN kirjottaminen_result_teacher;
