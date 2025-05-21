97605a2c1f8552d877162a1cbbfdd8de180359b9
Inari Tommiska <inari.tommiska@reaktor.com>
2025-05-21 17:04:30 +0300

    wip: sorting tests

--- e2e/tests/kotoutumiskoulutus/kielitesti-suoritukset.spec.ts
+++ e2e/tests/kotoutumiskoulutus/kielitesti-suoritukset.spec.ts
@@ -1,10 +1,5 @@
-import {
-  beforeAll,
-  beforeEach,
-  describe,
-  expect,
-  test,
-} from "../../fixtures/baseFixture"
+import { beforeEach, describe, expect, test } from "../../fixtures/baseFixture"
+import { enumerate } from "../../util/arrays"
 
 describe("Kotoutumiskoulutuksen kielitesti -page", () => {
   beforeEach(async ({ db, kotoSuoritus, basePage }) => {
@@ -63,4 +58,62 @@ describe("Kotoutumiskoulutuksen kielitesti -page", () => {
     await expect(thirdSuoritus).toBeVisible()
     await expect(thirdSuoritus).toContainText(magdalena.firstNames)
   })
+
+  const sortTestCases = [
+    {
+      column: "Sukunimi",
+      tableColumnIndex: 0,
+      order: Array<string>(
+        "Välimaa-Testi",
+        "Torvinen-Testi",
+        "Sallinen-Testi",
+        "Laasonen-Testi",
+      ),
+    },
+    {
+      column: "Etunimet",
+      tableColumnIndex: 1,
+      order: Array<string>(
+        "Toni Testi",
+        "Magdalena Testi",
+        "Eino Testi",
+        "Anniina Testi",
+      ),
+    },
+    {
+      column: "Sähköposti",
+      tableColumnIndex: 2,
+      order: Array<string>(
+        "devnull-6@oph.fi",
+        "devnull-14@oph.fi",
+        "devnull-12@oph.fi",
+        "devnull-10@oph.fi",
+      ),
+    },
+  ] as const
+  for (const testCase of sortTestCases) {
+    const { column, tableColumnIndex, order } = testCase
+    const reverseOrder = [...order].reverse()
+
+    test(`registry data can be sorted by "${column}"`, async ({
+      kielitestiSuorituksetPage: page,
+    }) => {
+      await page.open()
+
+      const sortByLink = page.getTableColumnHeaderLink(column)
+      await sortByLink.click()
+
+      for (const [expected, row] of enumerate(order)) {
+        const actualValue = page.getSuoritusColumn(row, tableColumnIndex)
+        await expect(actualValue).toHaveText(expected)
+      }
+
+      await sortByLink.click()
+
+      for (const [expected, row] of enumerate(reverseOrder)) {
+        const actualValue = page.getSuoritusColumn(row, tableColumnIndex)
+        await expect(actualValue).toHaveText(expected)
+      }
+    })
+  }
 })
