583987fe2c2870eb7c5fa8779f0c6ae276eb846e
Inari Tommiska <inari.tommiska@reaktor.com>
2025-05-26 17:43:25 +0300

    tests for koto error table sorting

--- e2e/models/kotoutumiskoulutus/KielitestiErrorPage.ts
+++ e2e/models/kotoutumiskoulutus/KielitestiErrorPage.ts
@@ -21,12 +21,25 @@ export default class KielitestiErrorPage extends BasePage {
     return this.getContent().getByRole("table")
   }
 
+  getErrorRow() {
+    return this.getContent().getByTestId("virhe-summary-row")
+  }
+
   async getErrorTableBody() {
     const errorsTable = this.getErrorsTable()
     return errorsTable.locator(".virheet")
   }
 
   async getErrorRows() {
-    return this.getContent().getByTestId("virhe-summary-row").all()
+    return this.getErrorRow().all()
+  }
+
+  getSuoritusColumn(rowIndex: number, columnIndex: number) {
+    const row = this.getErrorRow().nth(rowIndex)
+    return row.getByRole("cell").nth(columnIndex)
+  }
+
+  getTableColumnHeaderLink(text: string) {
+    return this.getErrorsTable().getByRole("link", { name: text })
   }
 }
