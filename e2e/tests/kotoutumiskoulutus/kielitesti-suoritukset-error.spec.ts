f6034d3a296f05d19cf92e1321d09a41ee48fa1d
Inari Tommiska <inari.tommiska@reaktor.com>
2025-05-26 17:03:10 +0300

    refact: clean up koto error page test selectors

--- e2e/tests/kotoutumiskoulutus/kielitesti-suoritukset-error.spec.ts
+++ e2e/tests/kotoutumiskoulutus/kielitesti-suoritukset-error.spec.ts
@@ -1,8 +1,6 @@
-import node_fs from "node:fs"
 import { beforeEach, describe, expect, test } from "../../fixtures/baseFixture"
 import { fixtureData } from "../../fixtures/kotoError"
 
-const fs = node_fs.promises
 describe('"Koto Suoritukset" -page', () => {
   beforeEach(async ({ db, basePage, kotoSuoritusError }) => {
     await db.withEmptyDatabase()
@@ -23,11 +21,10 @@ describe('"Koto Suoritukset" -page', () => {
     expect(page.url()).toContain(kielitestiErrorPage.url)
 
     const errors = await kielitestiErrorPage.getErrorRows()
-    await expect(errors.length).toEqual(1)
+    expect(errors).toHaveLength(1)
   })
 
   test("koto suoritus error is displayed properly", async ({
-    page,
     kielitestiErrorPage,
   }) => {
     await kielitestiErrorPage.open()
@@ -35,36 +32,35 @@ describe('"Koto Suoritukset" -page', () => {
     const errors = await kielitestiErrorPage.getErrorTableBody()
 
     const virheFixture = fixtureData.suoritusVirhe
-    const hetuCell = await errors.getByRole("cell", { name: virheFixture.hetu })
-    const nimiCell = await errors.getByText(virheFixture.nimi)
-    const schoolOidCell = await errors.getByText(virheFixture.schoolOid)
-    const teacherEmailCell = await errors.getByText(virheFixture.teacherEmail)
-    const virheenLuontiaikaCell = await errors.getByText(
+    const hetuCell = errors.getByRole("cell", { name: virheFixture.hetu })
+    const nimiCell = errors.getByText(virheFixture.nimi)
+    const schoolOidCell = errors.getByText(virheFixture.schoolOid)
+    const teacherEmailCell = errors.getByText(virheFixture.teacherEmail)
+    const virheenLuontiaikaCell = errors.getByText(
       virheFixture.virheenLuontiaika,
     )
-    const viestiCell = await errors.getByText(virheFixture.viesti)
-    const virheellinenKenttaCell = await errors.getByText(
+    const viestiCell = errors.getByText(virheFixture.viesti)
+    const virheellinenKenttaCell = errors.getByText(
       virheFixture.virheellinenKentta,
       { exact: true },
     )
-    const virheellinenArvoCell = await errors.getByText(
-      virheFixture.virheellinenArvo,
-    )
+    const virheellinenArvoCell = errors.getByText(virheFixture.virheellinenArvo)
 
-    expect(await hetuCell.getAttribute("headers")).toEqual("hetu")
-    expect(await nimiCell.getAttribute("headers")).toEqual("nimi")
-    expect(await schoolOidCell.getAttribute("headers")).toEqual("schoolOid")
-    expect(await teacherEmailCell.getAttribute("headers")).toEqual(
-      "teacherEmail",
-    )
-    expect(await virheenLuontiaikaCell.getAttribute("headers")).toEqual(
+    await expect(hetuCell).toHaveAttribute("headers", "hetu")
+    await expect(nimiCell).toHaveAttribute("headers", "nimi")
+    await expect(schoolOidCell).toHaveAttribute("headers", "schoolOid")
+    await expect(teacherEmailCell).toHaveAttribute("headers", "teacherEmail")
+    await expect(virheenLuontiaikaCell).toHaveAttribute(
+      "headers",
       "virheenLuontiaika",
     )
-    expect(await viestiCell.getAttribute("headers")).toEqual("viesti")
-    expect(await virheellinenKenttaCell.getAttribute("headers")).toEqual(
+    await expect(viestiCell).toHaveAttribute("headers", "viesti")
+    await expect(virheellinenKenttaCell).toHaveAttribute(
+      "headers",
       "virheellinenKentta",
     )
-    expect(await virheellinenArvoCell.getAttribute("headers")).toEqual(
+    await expect(virheellinenArvoCell).toHaveAttribute(
+      "headers",
       "virheellinenArvo",
     )
   })
@@ -81,6 +77,6 @@ describe('"Koto Suoritukset" -page', () => {
     expect(page.url()).toContain(kielitestiErrorPage.url)
 
     const errors = await kielitestiErrorPage.getErrorRows()
-    await expect(errors.length).toEqual(2)
+    expect(errors).toHaveLength(2)
   })
 })
