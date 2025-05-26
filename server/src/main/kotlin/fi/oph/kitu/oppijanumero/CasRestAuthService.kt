2735758161662486e984f6200e80686282627694
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-05-26 12:29:08 +0300

    wip: isLoginToCas

--- server/src/main/kotlin/fi/oph/kitu/oppijanumero/CasRestAuthService.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/CasRestAuthService.kt
@@ -2,7 +2,11 @@ package fi.oph.kitu.oppijanumero
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import fi.oph.kitu.TypedResult
+import fi.oph.kitu.logging.use
+import io.opentelemetry.api.trace.Tracer
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
@@ -12,10 +16,23 @@ import java.net.URI
 
 @Service
 class CasRestAuthService(
+    val tracer: Tracer,
+    val casService: CasRestService,
     @Qualifier("casRestClient")
     val restCient: RestClient,
     val objectMapper: ObjectMapper,
 ) {
+    fun authenticateToCas(): TypedResult<*, CasError> =
+        tracer
+            .spanBuilder("CasAuthenticatedServiceImpl.authenticateToCas")
+            .startSpan()
+            .use {
+                casService
+                    .getGrantingTicket()
+                    .flatMap(casService::getServiceTicket)
+                    .flatMap(casService::sendAuthenticationRequest)
+            }
+
     final inline fun <Request : Any, reified Response : Any> authenticatedPost(
         uri: URI,
         body: Request,
@@ -27,21 +44,35 @@ class CasRestAuthService(
         // and the restClient should take those into considerations.
         val bodyAsString = objectMapper.writeValueAsString(body)
 
-        val resp =
-            try {
-                restCient
-                    .post()
-                    .uri(uri)
-                    .body(bodyAsString)
-                    .accept(accept)
-                    .retrieve()
-                    .toEntity<Response>()
-            } catch (e: Throwable) {
-                println(e)
-                e.printStackTrace()
-                throw e
+        val response =
+            restCient
+                .post()
+                .uri(uri)
+                .body(bodyAsString)
+                .accept(accept)
+                .retrieve()
+                .toEntity<Response>()
+
+        if (isLoginToCas(response)) {
+            println("Authenticate to CAS")
+        } else if (response.statusCode == HttpStatus.UNAUTHORIZED) {
+            println("Authenticate to CAS")
+        }
+
+        return TypedResult.Success(response)
+    }
+
+    /** Check if the service that was called, redirected the response into CAS */
+    fun isLoginToCas(response: ResponseEntity<*>): Boolean {
+        if (response.statusCode == HttpStatus.FOUND) {
+            val location = response.headers.getFirst(HttpHeaders.LOCATION)
+            if (location == null) {
+                return false
             }
 
-        return TypedResult.Success(resp)
+            val hasCasLogin = location.contains("/cas/login")
+            return hasCasLogin
+        }
+        return false
     }
 }
---
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/CasRestService.kt
@@ -0,0 +1,15 @@
+package fi.oph.kitu.oppijanumero
+
+import org.springframework.stereotype.Service
+
+@Service
+class CasRestService {
+    fun getGrantingTicket() {
+    }
+
+    fun getServiceTicket() {
+    }
+
+    fun sendAuthenticationRequest() {
+    }
+}
