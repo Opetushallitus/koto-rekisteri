354a131785b1c7f7ed9e1baec45f39cef89aba52
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-05-26 12:49:36 +0300

    wip: getGrantingTicket

--- server/src/main/kotlin/fi/oph/kitu/oppijanumero/CasRestService.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/CasRestService.kt
@@ -1,15 +1,54 @@
 package fi.oph.kitu.oppijanumero
 
+import fi.oph.kitu.TypedResult
+import io.opentelemetry.instrumentation.annotations.WithSpan
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.toEntity
+import java.net.URLEncoder
 
 @Service
-class CasRestService {
-    fun getGrantingTicket() {
-    }
+class CasRestService(
+    @Qualifier("casRestClient")
+    val restClient: RestClient,
+) {
+    @Value("\${kitu.palvelukayttaja.username}")
+    private lateinit var onrUsername: String
 
-    fun getServiceTicket() {
-    }
+    @Value("\${kitu.palvelukayttaja.password}")
+    private lateinit var onrPassword: String
 
     fun sendAuthenticationRequest() {
+    }
+
+    @WithSpan
+    fun getGrantingTicket(): TypedResult<String, CasError> {
+        // Step 2 - form a request
+        val username = URLEncoder.encode(onrUsername, "UTF-8")
+        val password = URLEncoder.encode(onrPassword, "UTF-8")
+        val body = "username=$username&password=$password"
+
+        val response =
+            restClient
+                .post()
+                .body(body)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .retrieve()
+                .toEntity<String>()
+
+        val ticket =
+            response.headers.location
+                ?.path
+                ?.substringAfterLast("/")
+
+        return if (response.statusCode == HttpStatus.OK && ticket != null) {
+            TypedResult.Success(ticket)
+        } else {
+            TypedResult.Failure(CasError.GrantingTicketError("Unable to get granting ticket"))
+        }
     }
 }
