ab00ce254f16214d0006992e8d861941047cb98d
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-05-28 13:41:51 +0300

    Add ttl to koodisto cache

--- server/src/main/kotlin/fi/oph/kitu/koodisto/KoodistoService.kt
+++ server/src/main/kotlin/fi/oph/kitu/koodisto/KoodistoService.kt
@@ -1,27 +1,31 @@
 package fi.oph.kitu.koodisto
 
+import fi.oph.kitu.Cache
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestClient
 import org.springframework.web.client.toEntity
+import kotlin.time.Duration.Companion.hours
 
 @Service
 class KoodistoService(
     @Qualifier("koodistopalveluRestClient")
     private val restClient: RestClient,
 ) {
-    private val koodistot: MutableMap<String, List<KoodistopalveluKoodiviite>> = mutableMapOf()
+    fun getKoodiviitteet(koodistoUri: String): List<KoodistopalveluKoodiviite>? = cachedKoodistot.get(koodistoUri)
 
-    fun getKoodiviitteet(koodistoUri: String): List<KoodistopalveluKoodiviite>? {
-        if (koodistot.containsKey(koodistoUri)) return koodistot[koodistoUri]
-        return restClient
+    private fun fetchKoodisto(koodistoUri: String): List<KoodistopalveluKoodiviite>? =
+        restClient
             .get()
             .uri("codeelement/codes/{uri}", mapOf("uri" to koodistoUri))
             .accept(MediaType.APPLICATION_JSON)
             .retrieve()
             .toEntity<List<KoodistopalveluKoodiviite>>()
             .body
-            ?.also { koodistot[koodistoUri] = it }
-    }
+
+    private val cachedKoodistot =
+        Cache<String, List<KoodistopalveluKoodiviite>>(ttl = 1.hours) {
+            fetchKoodisto(it)
+        }
 }
