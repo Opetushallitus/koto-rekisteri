116bc01e0738b8bae05648b8283ecac47b2aef1d
Inari Tommiska <inari.tommiska@reaktor.com>
2025-06-02 11:55:18 +0300

    yki error sort tests

--- e2e/tests/yki/yki-suoritukset-error.spec.ts
+++ e2e/tests/yki/yki-suoritukset-error.spec.ts
@@ -1,7 +1,6 @@
-import * as node_fs from "node:fs"
 import { beforeEach, describe, expect, test } from "../../fixtures/baseFixture"
+import { enumerate } from "../../util/arrays"
 
-const fs = node_fs.promises
 describe('"YKI Suoritukset" -page', () => {
   beforeEach(async ({ db, basePage, ykiSuoritusError }) => {
     await db.withEmptyDatabase()
@@ -21,7 +20,63 @@ describe('"YKI Suoritukset" -page', () => {
 
     expect(page.url()).toContain(ykiSuorituksetErrorPage.url)
 
-    const errors = await ykiSuorituksetErrorPage.getErrorRow()
+    const errors = ykiSuorituksetErrorPage.getErrorRow()
     await expect(errors).toHaveCount(1)
   })
+
+  const sortTestCases = [
+    {
+      column: "oppijanumero",
+      tableColumnIndex: 0,
+      order: Array<string>(
+        "1.2.246.562.24.33342764709",
+        "1.2.246.562.24.59267607404",
+        "arvo puuttuu",
+      ),
+    },
+    {
+      column: "hetu",
+      tableColumnIndex: 1,
+      order: Array<string>("010116A9518", "010180-9026", "010866-9260"),
+    },
+    {
+      column: "nimi",
+      tableColumnIndex: 2,
+      order: Array<string>(
+        '"Kivinen-Testi" "Petro Testi"',
+        '"Öhman-Testi" "Ranja Testi"',
+        '"Sallinen-Testi" "Magdalena Testi"',
+      ),
+    },
+  ]
+  for (const testCase of sortTestCases) {
+    const { column, tableColumnIndex, order } = testCase
+    const reverseOrder = [...order].reverse()
+
+    test(`registry data can be sorted by "${column}"`, async ({
+      ykiSuorituksetErrorPage: page,
+      ykiSuoritusError,
+      db,
+    }) => {
+      await ykiSuoritusError.insert(db, "weirdError")
+      await ykiSuoritusError.insert(db, "invalidGender")
+
+      await page.open()
+
+      const sortByLink = page.getTableColumnHeaderLink(column)
+      await sortByLink.click()
+
+      for (const [expected, row] of enumerate(order)) {
+        const actualValue = page.getSuoritusColumn(row, tableColumnIndex)
+        await expect(actualValue).toContainText(expected)
+      }
+
+      await sortByLink.click()
+
+      for (const [expected, row] of enumerate(reverseOrder)) {
+        const actualValue = page.getSuoritusColumn(row, tableColumnIndex)
+        await expect(actualValue).toContainText(expected)
+      }
+    })
+  }
 })
