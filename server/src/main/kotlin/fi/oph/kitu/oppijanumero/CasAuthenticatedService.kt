2f8f29fb019a23cf5225f2feb235221b652b6653
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-06-03 13:03:45 +0300

    wip: refactor CasAuthenticatedService to use RestClient
    implementation works but tests do not work

--- server/src/main/kotlin/fi/oph/kitu/oppijanumero/CasAuthenticatedService.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/CasAuthenticatedService.kt
@@ -1,87 +1,91 @@
 package fi.oph.kitu.oppijanumero
 
 import fi.oph.kitu.TypedResult
-import fi.oph.kitu.logging.use
-import io.opentelemetry.api.trace.Tracer
+import fi.oph.kitu.retrieveEntitySafely
 import io.opentelemetry.instrumentation.annotations.WithSpan
 import org.springframework.beans.factory.annotation.Qualifier
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import org.springframework.web.client.RestClient
 
 interface CasAuthenticatedService {
-    fun sendRequest(requestBuilder: HttpRequest.Builder): TypedResult<HttpResponse<String>, CasError>
+    fun <Request : Any, Response> post(
+        endpoint: String,
+        body: Request,
+        contentType: MediaType,
+        responseType: Class<Response>,
+    ): TypedResult<ResponseEntity<Response>, CasError>
 }
 
 @Service
 class CasAuthenticatedServiceImpl(
-    @Qualifier("oppijanumeroHttpClient")
-    private val httpClient: HttpClient,
+    @Qualifier("oppijanumeroRestClient")
+    private val restClient: RestClient,
     private val casService: CasService,
-    private val tracer: Tracer,
 ) : CasAuthenticatedService {
-    @Value("\${kitu.oppijanumero.callerid}")
-    private lateinit var callerId: String
-
-    private fun authenticateToCas(): TypedResult<URI, CasError> =
-        tracer
-            .spanBuilder("CasAuthenticatedServiceImpl.authenticateToCas")
-            .startSpan()
-            .use {
-                casService
-                    .getGrantingTicket()
-                    .flatMap(casService::getServiceTicket)
-                    .flatMap(casService::verifyServiceTicket)
-            }
-
     @WithSpan
-    override fun sendRequest(requestBuilder: HttpRequest.Builder): TypedResult<HttpResponse<String>, CasError> {
-        requestBuilder
-            .header("Caller-Id", callerId)
-            .header("CSRF", "CSRF")
-            .header("Cookie", "CSRF=CSRF")
-        val request = requestBuilder.build()
-        val response = httpClient.send(request, HttpResponse.BodyHandlers.ofString())
+    override fun <Request : Any, Response> post(
+        endpoint: String,
+        body: Request,
+        contentType: MediaType,
+        responseType: Class<Response>,
+    ): TypedResult<ResponseEntity<Response>, CasError> {
+        val response =
+            restClient
+                .post()
+                .uri(endpoint)
+                .body(body)
+                .contentType(contentType)
+                .retrieveEntitySafely(responseType)
 
-        if (isLoginToCas(response)) {
-            // Oppijanumerorekisteri ohjaa CAS kirjautumissivulle, jos autentikaatiota
-            // ei ole tehty. Luodaan uusi CAS ticket ja yritetään uudelleen.
-            return authenticateToCas() // gets JSESSIONID Cookie and it will be used in the next request below
-                .flatMap {
-                    val authenticatedRequest = requestBuilder.build()
-                    val authenticatedResponse =
-                        httpClient.send(
-                            authenticatedRequest,
-                            HttpResponse.BodyHandlers.ofString(),
-                        )
-
-                    TypedResult.Success(authenticatedResponse)
-                }
-        } else if (response.statusCode() == 401) {
-            // Oppijanumerorekisteri vastaa HTTP 401 kun sessio on vanhentunut.
-            // HUOM! Oppijanumerorekisteri vastaa HTTP 401 myös jos käyttöoikeudet eivät riitä.
-            return authenticateToCas() // gets JSESSIONID Cookie and it will be used in the next request below
-                .flatMap {
-                    val authenticatedRequest = requestBuilder.build()
-                    val authenticatedResponse =
-                        httpClient.send(authenticatedRequest, HttpResponse.BodyHandlers.ofString())
-
-                    TypedResult.Success(authenticatedResponse)
-                }
+        // Shouldn't happen, unless pppijanumero don't behave as expected
+        if (response == null) {
+            return TypedResult.Failure(
+                CasError.CasAuthServiceError("Received null ResponseEntity on the first request"),
+            )
         }
 
-        // loput statuskoodit oletetaan johtuvan kutsuttuvasta rajapinnasta
-        return TypedResult.Success(response)
+        if (!requiresLogin(response)) {
+            return TypedResult.Success(response)
+        }
+
+        return casService
+            // authenticate to CAS
+            .getGrantingTicket()
+            .flatMap(casService::getServiceTicket)
+            .flatMap(casService::verifyServiceTicket)
+            .flatMap { newURI ->
+                val response =
+                    restClient
+                        .post()
+                        .uri(newURI)
+                        .body(body)
+                        .contentType(contentType)
+                        .retrieveEntitySafely(responseType)
+
+                if (response == null) {
+                    TypedResult.Failure(
+                        CasError.CasAuthServiceError("Received null ResponseEntity after authentication"),
+                    )
+                } else {
+                    TypedResult.Success(response)
+                }
+            }
     }
 
-    private fun isLoginToCas(response: HttpResponse<*>): Boolean {
-        if (response.statusCode() == 302) {
-            val header = response.headers().firstValue("Location")
-            return header.map { location: String -> location.contains("/cas/login") }.orElse(false)
+    private fun requiresLogin(response: ResponseEntity<*>): Boolean {
+        if (response.statusCode == HttpStatus.FOUND) {
+            return response.headers
+                .getFirst(HttpHeaders.LOCATION)
+                ?.contains("/cas/login")
+                ?: false
         }
-        return false
+
+        // Oppijanumerorekisteri vastaa HTTP 401 kun sessio on vanhentunut.
+        // HUOM! Oppijanumerorekisteri vastaa HTTP 401 myös jos käyttöoikeudet eivät riitä.
+        return response.statusCode == HttpStatus.UNAUTHORIZED
     }
 }
