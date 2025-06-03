adad62b340d874ebf0d8c36308154bbfdbc08020
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-06-03 10:40:26 +0300

    Mock data generating for VKT

--- server/src/main/kotlin/fi/oph/kitu/mock/OidGenerator.kt
+++ server/src/main/kotlin/fi/oph/kitu/mock/OidGenerator.kt
@@ -1,6 +1,7 @@
 package fi.oph.kitu.mock
 
 import fi.oph.kitu.Oid
+import kotlin.random.Random
 
 /**
  * Generates random user OID under node class 1.2.246.562.240.
@@ -8,11 +9,11 @@ import fi.oph.kitu.Oid
  * Note: Node class 240 is the correct class for users,
  * but these values should not be used in production.
  */
-fun generateRandomUserOid(): Oid =
+fun generateRandomUserOid(random: Random = Random): Oid =
     Oid
         .parse(
             "1.2.246.562.240.${
-                (0..99999999999).random().toString().padStart(11, '0')
+                (0..99999999999).random(random).toString().padStart(11, '0')
             }",
         ).getOrThrow()
 
@@ -22,10 +23,10 @@ fun generateRandomUserOid(): Oid =
  * Note: Node class 10 is the correct class for organizations,
  * but these values should not be used in production.
  */
-fun generateRandomOrganizationOid() =
+fun generateRandomOrganizationOid(random: Random = Random) =
     Oid
         .parse(
             "1.2.246.562.100.${
-                (0..99999999999).random().toString().padStart(11, '0')
+                (0..99999999999).random(random).toString().padStart(11, '0')
             }",
         ).getOrThrow()
---
+++ server/src/main/kotlin/fi/oph/kitu/mock/VktSuoritusMockGenerator.kt
@@ -0,0 +1,129 @@
+package fi.oph.kitu.mock
+
+import fi.oph.kitu.koodisto.Koodisto
+import fi.oph.kitu.vkt.VktArvionti
+import fi.oph.kitu.vkt.VktKirjoittamisenKoe
+import fi.oph.kitu.vkt.VktOsakoe
+import fi.oph.kitu.vkt.VktPuheenYmmartamisenKoe
+import fi.oph.kitu.vkt.VktPuhumisenKoe
+import fi.oph.kitu.vkt.VktSuoritus
+import fi.oph.kitu.vkt.VktSuoritusEntity
+import fi.oph.kitu.vkt.VktTekstinYmmartamisenKoe
+import fi.oph.kitu.vkt.VktValidation
+import fi.oph.kitu.vkt.tiedonsiirtoschema.Lahdejarjestelma
+import fi.oph.kitu.vkt.tiedonsiirtoschema.LahdejarjestelmanTunniste
+import fi.oph.kitu.vkt.tiedonsiirtoschema.OidOppija
+import fi.oph.kitu.vkt.tiedonsiirtoschema.OidString
+import fi.oph.kitu.yki.Sukupuoli
+import java.time.LocalDate
+import kotlin.random.Random
+
+class VktSuoritusMockGenerator(
+    seed: Int = 0,
+) {
+    private val random = Random(seed)
+    private var index = 0
+
+    fun generateRandomVktSuoritusEntity(): VktSuoritusEntity {
+        index += 1
+        return VktValidation.enrich(randomSuoritus(index)).toVktSuoritusEntity(randomOppija())
+    }
+
+    fun randomSuoritus(index: Int): VktSuoritus {
+        val taitotaso = Koodisto.VktTaitotaso.entries.random(random)
+        return VktSuoritus(
+            taitotaso = taitotaso,
+            kieli = randomKieli(),
+            suorituksenVastaanottaja = null,
+            suorituspaikkakunta = "091",
+            osat =
+                randomOsakokeet(
+                    taitotaso,
+                    getRandomLocalDate(
+                        LocalDate.of(2000, 1, 1),
+                        LocalDate.of(2025, 1, 1),
+                    ),
+                ),
+            lahdejarjestelmanId =
+                LahdejarjestelmanTunniste(
+                    "$index",
+                    Lahdejarjestelma.KIOS,
+                ),
+        )
+    }
+
+    fun randomOppija() =
+        OidOppija(
+            oid = OidString.from(generateRandomUserOid(random)),
+            etunimet =
+                generateRandomFirstnames(
+                    Sukupuoli.entries.random(random),
+                    random,
+                ).let { "${it.first} ${it.second}" },
+            sukunimi = surnames.random(random),
+        )
+
+    fun randomKieli(): Koodisto.Tutkintokieli =
+        listOf(
+            Koodisto.Tutkintokieli.FIN,
+            Koodisto.Tutkintokieli.SWE,
+        ).random(random)
+
+    fun randomOsakokeet(
+        taso: Koodisto.VktTaitotaso,
+        pvm: LocalDate,
+        tyypit: List<Koodisto.VktOsakoe> = Koodisto.VktOsakoe.entries,
+    ): List<VktOsakoe> {
+        val kokeet = randomTutkintopaiva(taso, pvm, tyypit)
+        val hylatytKokeet = kokeet.filter { it.arviointi?.arvosana == Koodisto.VktArvosana.Hylätty }
+
+        return if (hylatytKokeet.isNotEmpty()) {
+            kokeet + randomOsakokeet(taso, pvm.plusDays(90), hylatytKokeet.map { it.tyyppi })
+        } else {
+            kokeet
+        }
+    }
+
+    fun randomTutkintopaiva(
+        taso: Koodisto.VktTaitotaso,
+        pvm: LocalDate,
+        tyypit: List<Koodisto.VktOsakoe>,
+    ): List<VktOsakoe> {
+        val arvosanat = validArvosanat(taso)
+        val arviointiPvm = listOf(pvm.plusDays(60), pvm.plusDays(50), null).random(random)
+
+        return listOf(
+            VktKirjoittamisenKoe(
+                tutkintopaiva = pvm,
+                arviointi = arviointiPvm?.let { VktArvionti(arvosanat.random(random), it) },
+            ),
+            VktTekstinYmmartamisenKoe(
+                tutkintopaiva = pvm,
+                arviointi = arviointiPvm?.let { VktArvionti(arvosanat.random(random), it) },
+            ),
+            VktPuhumisenKoe(
+                tutkintopaiva = pvm,
+                arviointi = arviointiPvm?.let { VktArvionti(arvosanat.random(random), it) },
+            ),
+            VktPuheenYmmartamisenKoe(
+                tutkintopaiva = pvm,
+                arviointi = arviointiPvm?.let { VktArvionti(arvosanat.random(random), it) },
+            ),
+        ).filter { tyypit.contains(it.tyyppi) }
+    }
+
+    fun validArvosanat(taso: Koodisto.VktTaitotaso) =
+        when (taso) {
+            Koodisto.VktTaitotaso.Erinomainen ->
+                listOf(
+                    Koodisto.VktArvosana.Erinomainen,
+                    Koodisto.VktArvosana.Hylätty,
+                )
+            Koodisto.VktTaitotaso.HyväJaTyydyttävä ->
+                listOf(
+                    Koodisto.VktArvosana.Hyvä,
+                    Koodisto.VktArvosana.Tyydyttävä,
+                    Koodisto.VktArvosana.Hylätty,
+                )
+        }
+}
