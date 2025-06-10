8f754a5980db058ed3100bc33f90be6e78a10467
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-06-10 09:52:43 +0300

    Oppijanumerorekisteri henkilo DTO

--- server/src/main/kotlin/fi/oph/kitu/oppijanumero/OppijanumeroService.kt
+++ server/src/main/kotlin/fi/oph/kitu/oppijanumero/OppijanumeroService.kt
@@ -131,4 +131,62 @@ class OppijanumeroServiceImpl(
                         },
                     )
             }
+
+data class OppijanumerorekisteriHenkilo(
+    val oidHenkilo: String?,
+    val hetu: String?,
+    val kaikkiHetut: List<String>?,
+    val passivoitu: Boolean?,
+    val etunimet: String?,
+    val kutsumanimi: String?,
+    val sukunimi: String?,
+    val aidinkieli: Kieli?,
+    val asiointiKieli: Kieli?,
+    val kansalaisuus: List<Kansalaisuus>?,
+    val kasittelijaOid: String?,
+    val syntymaaika: LocalDate?,
+    val sukupuoli: String?,
+    val kotikunta: String?,
+    val oppijanumero: String?,
+    val turvakielto: Boolean?,
+    val eiSuomalaistaHetua: Boolean?,
+    val yksiloity: Boolean?,
+    val yksiloityVTJ: Boolean?,
+    val yksilointiYritetty: Boolean?,
+    val duplicate: Boolean?,
+    val created: OffsetDateTime?,
+    val modified: OffsetDateTime?,
+    val vtjsynced: OffsetDateTime?,
+    val yhteystiedotRyhma: List<Yhteystietoryhma>?,
+    val yksilointivirheet: List<Yksilointivirhe>?,
+    val passinumero: List<String>?,
+) {
+    fun hetut() = listOfNotNull(hetu) + (kaikkiHetut ?: emptyList())
+
+    data class Kieli(
+        val kieliKoodi: String?,
+        val kieliTyyppi: String?,
+    )
+
+    data class Kansalaisuus(
+        val kansalaisuusKoodi: String?,
+    )
+
+    data class Yhteystietoryhma(
+        val id: Long?,
+        val ryhmaKuvaus: String?,
+        val ryhmaAlkuperaTieto: String?,
+        val readOnly: Boolean?,
+        val yhteystieto: List<Yhteystieto>?,
+    ) {
+        data class Yhteystieto(
+            val yhteystietoTyyppi: String?,
+            val yhteystietoArvo: String?,
+        )
+    }
+
+    data class Yksilointivirhe(
+        val yksilointivirheTila: String?,
+        val uudelleenyritysAikaleima: OffsetDateTime?,
+    )
 }
