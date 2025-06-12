69db5e7aca048cd062c790ede67e52645ab8788a
Inari Tommiska <inari.tommiska@reaktor.com>
2025-06-12 15:28:39 +0300

    use base people fixture for koto error tests

--- e2e/fixtures/kotoError.ts
+++ e2e/fixtures/kotoError.ts
@@ -1,7 +1,6 @@
 import SQL from "sql-template-strings"
 import { TestDB } from "./baseFixture"
-
-export type Oid = `${number}.${number}.${number}.${number}.${number}.${number}`
+import { FixturePerson, Oid, peopleFixture } from "./basePeopleFixture"
 
 export interface KotoError {
   suorittajanOid: string
@@ -15,33 +14,37 @@ export interface KotoError {
   virheellinenArvo: string
 }
 
-const createError = ({
-  suorittajanOid = "1.2.246.562.24.33342764709",
-  hetu,
-  nimi,
-  schoolOid,
-  teacherEmail = "opettaja@testi.oph.fi",
-  virheenLuontiaika = "2024-11-22T10:49:49Z",
-  viesti,
-  virheellinenKentta,
-  virheellinenArvo,
-}: Partial<KotoError>) => ({
-  suorittajanOid,
-  hetu,
-  nimi,
-  schoolOid,
-  teacherEmail,
-  virheenLuontiaika,
-  viesti,
-  virheellinenKentta,
-  virheellinenArvo,
-})
+type CreateErrorArgs = Partial<
+  Omit<KotoError, "hetu" | "suorittajanOid" | "nimi">
+>
+
+const createError = (
+  person: FixturePerson,
+  {
+    schoolOid,
+    teacherEmail = "opettaja@testi.oph.fi",
+    virheenLuontiaika = "2024-11-22T10:49:49Z",
+    viesti,
+    virheellinenKentta,
+    virheellinenArvo,
+  }: CreateErrorArgs,
+) => {
+  const p = peopleFixture[person]
+  return {
+    suorittajanOid: p.oppijanumero,
+    hetu: p.hetu,
+    nimi: `${p.etunimet} ${p.sukunimi}`,
+    schoolOid,
+    teacherEmail,
+    virheenLuontiaika,
+    viesti,
+    virheellinenKentta,
+    virheellinenArvo,
+  }
+}
 
 export const fixtureData = {
-  suoritusVirhe: createError({
-    hetu: "010180-9026",
-    suorittajanOid: "1.2.246.562.24.20281155246",
-    nimi: "Öhman Testi Ranja Testi",
+  suoritusVirhe: createError("ranja", {
     schoolOid: "1.2.246.562.10.1234567890",
     teacherEmail: "opettaja@testi.oph.fi",
     viesti:
@@ -50,10 +53,7 @@ export const fixtureData = {
     virheellinenArvo: "virheellinen arvosana",
     virheenLuontiaika: "2024-11-22T10:49:49Z",
   }),
-  virhePetro: createError({
-    hetu: "010116A9518",
-    suorittajanOid: "1.2.246.562.24.59267607404",
-    nimi: "Kivinen-Testi Petro Testi",
+  virhePetro: createError("petro", {
     schoolOid: "1.2.246.562.10.1234567891",
     teacherEmail: "toinen-opettaja@testi.oph.fi",
     viesti:
@@ -62,10 +62,7 @@ export const fixtureData = {
     virheellinenArvo: "tyhjää täynnä",
     virheenLuontiaika: "2025-05-26T12:34:56Z",
   }),
-  virheMagdalena: createError({
-    hetu: "010866-9260",
-    suorittajanOid: "1.2.246.562.24.33342764709",
-    nimi: "Sallinen-Testi Magdalena Testi",
+  virheMagdalena: createError("magdalena", {
     schoolOid: "1.2.246.562.10.0987654321",
     teacherEmail: "yksi-opettajista@testi.oph.fi",
     viesti: "testiviesti, ei tekstiviesti",
@@ -73,7 +70,7 @@ export const fixtureData = {
     virheellinenArvo: "en kerro, arvaa!",
     virheenLuontiaika: "2042-12-22T22:42:42Z",
   }),
-  withNullValues: createError({
+  withNullValues: {
     suorittajanOid: null,
     hetu: null,
     nimi: " ",
@@ -82,7 +79,8 @@ export const fixtureData = {
     viesti: "Unexpected error",
     virheellinenKentta: null,
     virheellinenArvo: null,
-  }),
+    virheenLuontiaika: "2024-11-22T10:49:49Z",
+  },
 } as const
 
 const insertQuery = (virhe: KotoError) => SQL`
