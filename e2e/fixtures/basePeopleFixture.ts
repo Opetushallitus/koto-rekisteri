7b2cae750993fff9d247aa1a84024ffc2bf87be4
Inari Tommiska <inari.tommiska@reaktor.com>
2025-06-12 15:13:47 +0300

    refact: use base people fixture for koto tests

--- e2e/fixtures/basePeopleFixture.ts
+++ e2e/fixtures/basePeopleFixture.ts
@@ -56,6 +56,34 @@ export const peopleFixture = {
       postitoimipaikka: "Testilä",
     },
   }),
+  fanni: createPerson({
+    hetu: "010100A9846",
+    sukupuoli: "N",
+    etunimet: "Fanni Testi",
+    sukunimi: "Vesala-Testi",
+    email: "devnull-3@oph.fi",
+    oppijanumero: "1.2.246.562.24.74064782358",
+    osoite: {
+      // FIXME:
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
+    },
+  }),
+  eino: createPerson({
+    hetu: "010100A999N",
+    sukupuoli: "M",
+    etunimet: "Eino Testi",
+    sukunimi: "Välimaa-Testi",
+    oppijanumero: "1.2.246.562.24.67409348034",
+    email: "devnull-10@oph.fi",
+    osoite: {
+      // FIXME:
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
+    },
+  }),
   petro: createPerson({
     hetu: "010116A9518",
     sukupuoli: "M",
@@ -67,6 +95,118 @@ export const peopleFixture = {
       katuosoite: "Testikuja 10",
       postinumero: "40200",
       postitoimipaikka: "Testinsuu",
+    },
+  }),
+  pernilla: createPerson({
+    hetu: "010100A9868",
+    sukupuoli: "N",
+    etunimet: "Pernilla Testi",
+    sukunimi: "Pulkkinen-Testi",
+    email: "devnull-4@oph.fi",
+    oppijanumero: "1.2.246.562.24.83611868632",
+    osoite: {
+      // FIXME
+      katuosoite: "",
+      postitoimipaikka: "",
+      postinumero: "",
+    },
+  }),
+  kalervo: createPerson({
+    hetu: "010100A995J",
+    sukupuoli: "M",
+    etunimet: "Kalervo Testi",
+    sukunimi: "Paasonen-Testi",
+    email: "devnull-5@oph.fi",
+    oppijanumero: "1.2.246.562.24.61390710528",
+    osoite: {
+      // FIXME
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
+    },
+  }),
+  toni: createPerson({
+    hetu: "010100A9857",
+    sukupuoli: "M",
+    etunimet: "Toni Testi",
+    sukunimi: "Laasonen-Testi",
+    email: "devnull-6@oph.fi",
+    oppijanumero: "1.2.246.562.24.16014275446",
+    osoite: {
+      // FIXME
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
+    },
+  }),
+  amalia: createPerson({
+    hetu: "010100A990C",
+    sukupuoli: "M",
+    etunimet: "Amalia Testi",
+    sukunimi: "Andersson-Testi",
+    email: "devnull-7@oph.fi",
+    oppijanumero: "1.2.246.562.24.64655158835",
+    osoite: {
+      // FIXME
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
+    },
+  }),
+  topi: createPerson({
+    hetu: "010100A991D",
+    sukupuoli: "M",
+    etunimet: "Topi Testi",
+    sukunimi: "Takkinen-Testi",
+    email: "devnull-8@oph.fi",
+    oppijanumero: "1.2.246.562.24.19650463538",
+    osoite: {
+      // FIXME
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
+    },
+  }),
+  tobias: createPerson({
+    hetu: "010100A993F",
+    sukupuoli: "M",
+    etunimet: "Tobias Testi",
+    sukunimi: "Saarela-Testi",
+    email: "devnull-9@oph.fi",
+    oppijanumero: "1.2.246.562.24.67738436206",
+    osoite: {
+      // FIXME
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
+    },
+  }),
+  silja: createPerson({
+    hetu: "010180-918P",
+    sukupuoli: "N",
+    etunimet: "Silja Testi",
+    sukunimi: "Haverinen-Testi",
+    email: "devnull-11@oph.fi",
+    oppijanumero: "1.2.246.562.24.27639310186",
+    osoite: {
+      // FIXME
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
+    },
+  }),
+  anniina: createPerson({
+    hetu: "010180-922U",
+    sukupuoli: "M",
+    etunimet: "Anniina Testi",
+    sukunimi: "Torvinen-Testi",
+    email: "devnull-12@oph.fi",
+    oppijanumero: "1.2.246.562.24.24941612410",
+    osoite: {
+      // FIXME
+      katuosoite: "",
+      postinumero: "",
+      postitoimipaikka: "",
     },
   }),
   magdalena: createPerson({
--- e2e/fixtures/kotoSuoritus.ts
+++ e2e/fixtures/kotoSuoritus.ts
@@ -1,9 +1,6 @@
 import SQL from "sql-template-strings"
 import { TestDB } from "./baseFixture"
-
-export type Oid = `${number}.${number}.${number}.${number}.${number}.${number}`
-export type OidQuoted = `"${Oid}"`
-export type Email = `${string}@${string}.${string}`
+import { Email, FixturePerson, Oid, peopleFixture } from "./basePeopleFixture"
 
 export interface KotoSuoritus {
   firstNames: string
@@ -22,117 +19,60 @@ export interface KotoSuoritus {
   courseName: string
 }
 
-const createSuoritus = ({
-  firstNames,
-  lastName,
-  oppijanumero,
-  schoolOid = "1.2.3.4.5.6",
-  email,
-  preferredName,
-  luetunYmmartaminenResult = "A1",
-  kuullunYmmartaminenResult = "B1",
-  puheResult = "75",
-  kirjoittaminenResult = "B1",
-  teacherEmail = "opettaja@testi.oph.fi",
-  timeCompleted = "2024-11-22 10:49:49",
-  courseId = 32,
-  courseName = "Integraatio testaus",
-}: Partial<KotoSuoritus>) => ({
-  firstNames,
-  lastName,
-  preferredName: preferredName ?? firstNames.split(" ")[0],
-  oppijanumero,
-  schoolOid,
-  email,
-  timeCompleted,
-  luetunYmmartaminenResult,
-  kuullunYmmartaminenResult,
-  kirjoittaminenResult,
-  puheResult,
-  teacherEmail,
-  courseId,
-  courseName,
-})
+type CreateSuoritusArgs = Partial<
+  Omit<
+    KotoSuoritus,
+    "firstNames" | "lastName" | "preferredName" | "email" | "oppijanumero"
+  >
+>
+
+const createSuoritus = (
+  person: FixturePerson,
+  {
+    schoolOid = "1.2.3.4.5.6",
+    luetunYmmartaminenResult = "A1",
+    kuullunYmmartaminenResult = "B1",
+    puheResult = "75",
+    kirjoittaminenResult = "B1",
+    teacherEmail = "opettaja@testi.oph.fi",
+    timeCompleted = "2024-11-22 10:49:49",
+    courseId = 32,
+    courseName = "Integraatio testaus",
+  }: CreateSuoritusArgs,
+) => {
+  const p = peopleFixture[person]
+  return {
+    firstNames: p.etunimet,
+    lastName: p.sukunimi,
+    preferredName: p.kutsumanimi,
+    oppijanumero: p.oppijanumero,
+    email: p.email,
+    schoolOid,
+    timeCompleted,
+    luetunYmmartaminenResult,
+    kuullunYmmartaminenResult,
+    kirjoittaminenResult,
+    puheResult,
+    teacherEmail,
+    courseId,
+    courseName,
+  }
+}
 
 export const fixtureData = {
-  ranja: createSuoritus({
-    firstNames: "Ranja Testi",
-    lastName: "Öhman-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-1@oph.fi",
-  }),
-  fanni: createSuoritus({
-    firstNames: "Fanni Testi",
-    lastName: "Vesala-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-3@oph.fi",
-  }),
-  eino: createSuoritus({
-    firstNames: "Eino Testi",
-    lastName: "Välimaa-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-10@oph.fi",
-  }),
-  petro: createSuoritus({
-    firstNames: "Petro Testi",
-    lastName: "Kivinen-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-2@oph.fi",
-  }),
-  pernilla: createSuoritus({
-    firstNames: "Pernilla Testi",
-    lastName: "Pulkkinen-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-4@oph.fi",
-  }),
-  kalervo: createSuoritus({
-    firstNames: "Kalervo Testi",
-    lastName: "Paasonen-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-5@oph.fi",
-  }),
-  toni: createSuoritus({
-    firstNames: "Toni Testi",
-    lastName: "Laasonen-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-6@oph.fi",
-  }),
-  amalia: createSuoritus({
-    firstNames: "Amalia Testi",
-    lastName: "Andersson-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-7@oph.fi",
-  }),
-  topi: createSuoritus({
-    firstNames: "Topi Testi",
-    lastName: "Takkinen-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-8@oph.fi",
-  }),
-  tobias: createSuoritus({
-    firstNames: "Tobias Testi",
-    lastName: "Saarela-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-9@oph.fi",
-  }),
-  silja: createSuoritus({
-    firstNames: "Silja Testi",
-    lastName: "Haverinen-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-11@oph.fi",
-  }),
-  anniina: createSuoritus({
-    firstNames: "Anniina Testi",
-    lastName: "Torvinen-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-12@oph.fi",
-  }),
-  magdalena: createSuoritus({
-    firstNames: "Magdalena Testi",
-    lastName: "Sallinen-Testi",
-    oppijanumero: "1.2.246.562.24.33342764709",
-    email: "devnull-14@oph.fi",
-  }),
+  ranja: createSuoritus("ranja", {}),
+  fanni: createSuoritus("fanni", {}),
+  eino: createSuoritus("eino", {}),
+  petro: createSuoritus("petro", {}),
+  pernilla: createSuoritus("pernilla", {}),
+  kalervo: createSuoritus("kalervo", {}),
+  toni: createSuoritus("toni", {}),
+  amalia: createSuoritus("amalia", {}),
+  topi: createSuoritus("topi", {}),
+  tobias: createSuoritus("tobias", {}),
+  silja: createSuoritus("silja", {}),
+  anniina: createSuoritus("anniina", {}),
+  magdalena: createSuoritus("magdalena", {}),
 } as const
 
 const insertQuery = (suoritus: KotoSuoritus) => SQL`
