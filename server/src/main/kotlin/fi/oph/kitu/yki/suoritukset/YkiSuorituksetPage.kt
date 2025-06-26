e42dbbb80c478ec8f4201a2cd06e003c74c241e4
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-06-26 16:08:20 +0300

    refactor page navigation links to method

--- server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/YkiSuorituksetPage.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/suoritukset/YkiSuorituksetPage.kt
@@ -7,6 +7,7 @@ import fi.oph.kitu.html.Page
 import fi.oph.kitu.html.error
 import fi.oph.kitu.html.input
 import kotlinx.html.ButtonType
+import kotlinx.html.FlowContent
 import kotlinx.html.FormMethod
 import kotlinx.html.InputType
 import kotlinx.html.TR
@@ -54,8 +55,34 @@ object YkiSuorituksetPage {
         paging: Paging,
         versionHistory: Boolean,
         errorsCount: Long,
-    ): String =
-        Page.renderHtml(
+    ): String {
+        fun FlowContent.navigationLink(
+            search: String? = null,
+            includeVersionHistory: Boolean? = null,
+            page: Int? = null,
+            sortColumnStr: String? = null,
+            sortDirectionEnum: SortDirection? = null,
+            ariaLabel: String? = null,
+            innerText: String,
+        ) {
+            a(
+                href = "suoritukset?${mapOf(
+                    "search" to (search ?: paging.searchStrUrl),
+                    "includeVersionHistory" to (includeVersionHistory ?: versionHistory),
+                    "page" to (page ?: paging.currentPage),
+                    "sortColumn" to (sortColumnStr ?: sortColumn),
+                    "sortDirection" to (sortDirectionEnum ?: sortDirection),
+                ).toUrlParams()}",
+            ) {
+                if (ariaLabel != null) {
+                    attributes["aria-label"] = ariaLabel
+                }
+
+                +innerText
+            }
+        }
+
+        return Page.renderHtml(
             Navigation.getBreadcrumbs("/yki/suoritukset"),
         ) {
             if (errorsCount > 0) {
@@ -65,128 +92,122 @@ object YkiSuorituksetPage {
                         +"Katso virheet"
                     }
                 }
+            }
 
-                form(
-                    action = "",
-                    method = FormMethod.get,
-                    classes = "grid center-vertically",
-                ) {
-                    fieldSet {
-                        attributes["role"] = "search"
-                        input(
-                            id = "search",
-                            type = InputType.text,
-                            name = "search",
-                            value = paging.searchStr,
-                            placeholder = "Oppijanumero, henkilötunnus tai hakusana",
-                        ) {
-                            button(type = ButtonType.submit) {
-                                +"Suodata"
-                            }
+            form(
+                action = "",
+                method = FormMethod.get,
+                classes = "grid center-vertically",
+            ) {
+                fieldSet {
+                    attributes["role"] = "search"
+                    input(
+                        id = "search",
+                        type = InputType.text,
+                        name = "search",
+                        value = paging.searchStr,
+                        placeholder = "Oppijanumero, henkilötunnus tai hakusana",
+                    ) {
+                        button(type = ButtonType.submit) {
+                            +"Suodata"
                         }
                     }
-                    fieldSet {
-                        label {
-                            input(
-                                id = "versionHistory",
-                                type = InputType.checkBox,
-                                name = "versionHistory",
-                                checked = versionHistory,
-                            ) {
-                                +"Näytä versiohistoria"
+                }
+                fieldSet {
+                    label {
+                        input(
+                            id = "versionHistory",
+                            type = InputType.checkBox,
+                            name = "versionHistory",
+                            checked = versionHistory,
+                        ) {
+                            +"Näytä versiohistoria"
+                        }
+                    }
+                }
+            }
+
+            article(classes = "overflow-auto") {
+                header {
+                    nav {
+                        ul {
+                            li {
+                                +"Suorituksia yhteensä: ${paging.totalEntries}"
+                            }
+                            li {
+                                a(
+                                    href = "/yki/api/suoritukset?includeVersionHistory=$versionHistory",
+                                ) {
+                                    attributes["download"] = ""
+                                    +"Lataa tiedot CSV:nä"
+                                }
                             }
                         }
                     }
                 }
 
-                article(classes = "overflow-auto") {
-                    header {
-                        nav {
-                            ul {
-                                li {
-                                    +"Suorituksia yhteensä: ${paging.totalEntries}"
-                                }
-                                li {
-                                    a(
-                                        href = "/yki/api/suoritukset?includeVersionHistory=$versionHistory",
-                                    ) {
-                                        attributes["download"] = ""
-                                        +"Lataa tiedot CSV:nä"
-                                    }
+                table {
+                    thead {
+                        tr {
+                            for (cell in header) {
+                                th {
+                                    this@renderHtml.navigationLink(
+                                        sortColumnStr = cell.column.urlParam,
+                                        sortDirectionEnum = cell.sortDirection,
+                                        innerText = "${cell.column.uiHeaderValue} ${cell.symbol}",
+                                    )
                                 }
                             }
                         }
                     }
-
-                    table {
-                        thead {
+                    for (suoritus in suoritukset) {
+                        tbody(classes = "suoritus") {
                             tr {
-                                for (cell in header) {
-                                    th {
-                                        a(
-                                            href = "suoritukset?${
-                                                mapOf(
-                                                    "search" to paging.searchStrUrl,
-                                                    "includeVersionHistory" to versionHistory,
-                                                    "sortColumn" to cell.column.urlParam,
-                                                    "sortDirection" to cell.sortDirection,
-                                                ).toUrlParams()}",
-                                        ) {
-                                            +"${cell.column.uiHeaderValue} ${cell.symbol}"
-                                        }
-                                    }
-                                }
+                                cell(suoritus.suorittajanOID)
+                                cell(suoritus.sukunimi)
+                                cell(suoritus.etunimet)
+                                cell(suoritus.sukupuoli.name)
+                                cell(suoritus.hetu)
+                                cell(suoritus.kansalaisuus)
+                                cell("${suoritus.katuosoite}, ${suoritus.postinumero} ${suoritus.postitoimipaikka}")
+                                cell(suoritus.email)
+                                cell(suoritus.suoritusId)
+                                cell(suoritus.tutkintopaiva)
+                                cell(suoritus.tutkintokieli.name)
+                                cell(suoritus.tutkintotaso.name)
+                                cell(suoritus.jarjestajanTunnusOid)
+                                cell(suoritus.jarjestajanNimi)
+                                cell(suoritus.arviointipaiva)
+                                cell(suoritus.tekstinYmmartaminen)
+                                cell(suoritus.kirjoittaminen)
+                                cell(suoritus.rakenteetJaSanasto)
+                                cell(suoritus.puheenYmmartaminen)
+                                cell(suoritus.puhuminen)
+                                cell(suoritus.yleisarvosana)
                             }
-                        }
-                        for (suoritus in suoritukset) {
-                            tbody(classes = "suoritus") {
-                                tr {
-                                    cell(suoritus.suorittajanOID)
-                                    cell(suoritus.sukunimi)
-                                    cell(suoritus.etunimet)
-                                    cell(suoritus.sukupuoli.name)
-                                    cell(suoritus.hetu)
-                                    cell(suoritus.kansalaisuus)
-                                    cell("${suoritus.katuosoite}, ${suoritus.postinumero} ${suoritus.postitoimipaikka}")
-                                    cell(suoritus.email)
-                                    cell(suoritus.suoritusId)
-                                    cell(suoritus.tutkintopaiva)
-                                    cell(suoritus.tutkintokieli.name)
-                                    cell(suoritus.tutkintotaso.name)
-                                    cell(suoritus.jarjestajanTunnusOid)
-                                    cell(suoritus.jarjestajanNimi)
-                                    cell(suoritus.arviointipaiva)
-                                    cell(suoritus.tekstinYmmartaminen)
-                                    cell(suoritus.kirjoittaminen)
-                                    cell(suoritus.rakenteetJaSanasto)
-                                    cell(suoritus.puheenYmmartaminen)
-                                    cell(suoritus.puhuminen)
-                                    cell(suoritus.yleisarvosana)
-                                }
 
-                                if (suoritus.tarkistusarvioinninSaapumisPvm != null) {
-                                    tr {
-                                        td {
-                                            attributes["colspan"] = "13"
-                                            details {
-                                                summary { +"Näytä tarkistusarvioinnin tiedot" }
-                                                table {
-                                                    tr {
-                                                        th { +"Saapumispäivä" }
-                                                        th { +"Asiatunnus" }
-                                                        th { +"Osakokeet" }
-                                                        th { +"Arvosana muuttui?" }
-                                                        th { +"Perustelu" }
-                                                        th { +"Käsittelypäivä" }
-                                                    }
-                                                    tr {
-                                                        cell(suoritus.tarkistusarvioinninSaapumisPvm)
-                                                        cell(suoritus.tarkistusarvioinninAsiatunnus)
-                                                        cell(suoritus.tarkistusarvioidutOsakokeet)
-                                                        cell(suoritus.arvosanaMuuttui)
-                                                        cell(suoritus.perustelu)
-                                                        cell(suoritus.tarkistusarvioinninKasittelyPvm)
-                                                    }
+                            if (suoritus.tarkistusarvioinninSaapumisPvm != null) {
+                                tr {
+                                    td {
+                                        attributes["colspan"] = "13"
+                                        details {
+                                            summary { +"Näytä tarkistusarvioinnin tiedot" }
+                                            table {
+                                                tr {
+                                                    th { +"Saapumispäivä" }
+                                                    th { +"Asiatunnus" }
+                                                    th { +"Osakokeet" }
+                                                    th { +"Arvosana muuttui?" }
+                                                    th { +"Perustelu" }
+                                                    th { +"Käsittelypäivä" }
+                                                }
+                                                tr {
+                                                    cell(suoritus.tarkistusarvioinninSaapumisPvm)
+                                                    cell(suoritus.tarkistusarvioinninAsiatunnus)
+                                                    cell(suoritus.tarkistusarvioidutOsakokeet)
+                                                    cell(suoritus.arvosanaMuuttui)
+                                                    cell(suoritus.perustelu)
+                                                    cell(suoritus.tarkistusarvioinninKasittelyPvm)
                                                 }
                                             }
                                         }
@@ -196,27 +217,20 @@ object YkiSuorituksetPage {
                         }
                     }
                 }
+            }
 
-                footer {
-                    nav {
-                        attributes["aria-label"] = "Suoritusten sivutus"
+            footer {
+                nav {
+                    attributes["aria-label"] = "Suoritusten sivutus"
 
-                        ul(classes = "paging") {
-                            li {
-                                // Navigate to previous page
-                                a(
-                                    href = "suoritukset?${mapOf(
-                                        "search" to paging.searchStrUrl,
-                                        "includeVersionHistory" to versionHistory,
-                                        "page" to paging.previousPage,
-                                        "sortColumn" to sortColumn,
-                                        "sortDirection" to sortDirection,
-                                    ).toUrlParams()}",
-                                ) {
-                                    attributes["aria-label"] = "Edellinen sivu"
-                                    +"◀"
-                                }
-                            }
+                    ul(classes = "paging") {
+                        li {
+                            // Navigate to previous page
+                            navigationLink(
+                                page = paging.previousPage,
+                                ariaLabel = "Edellinen sivu",
+                                innerText = "◀",
+                            )
                         }
                         li {
                             // Show the number of the current page
@@ -224,24 +238,23 @@ object YkiSuorituksetPage {
                         }
                         li {
                             // Navigate to next page
-                            a(
-                                href = "suoritukset?${mapOf(
-                                    "search" to paging.searchStrUrl,
-                                    "includeVersionHistory" to versionHistory,
-                                    "page" to paging.nextPage,
-                                    "sortColumn" to sortColumn,
-                                    "sortDirection" to sortDirection,
-                                ).toUrlParams()}",
-                            ) {
-                                attributes["aria-label"] = "Seuraava sivu"
-                                +"▶"
-                            }
+                            navigationLink(
+                                page = paging.nextPage,
+                                ariaLabel = "Seuraava sivu",
+                                innerText = "▶",
+                            )
                         }
                     }
                 }
             }
         }
+    }
 
+    fun <T> TR.cell(value: T? = null) {
+        td {
+            +(value?.toString() ?: "")
+        }
+    }
 
     fun <K, V> Map<K, V>.toUrlParams(): String =
         this
@@ -254,10 +267,4 @@ object YkiSuorituksetPage {
                     )
                 }
             }
-
-    fun <T> TR.cell(value: T? = null) {
-        td {
-            +(value?.toString() ?: "")
-        }
-    }
 }
