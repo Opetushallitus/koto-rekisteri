f35ee5ec888706a6542af8cbd9b58436bf1a7f10
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-07-04 16:50:38 +0300

    wip: YkiArvioijaPage, containing only errorsArticle

---
+++ server/src/main/kotlin/fi/oph/kitu/yki/ErrorsArticle.kt
@@ -0,0 +1,20 @@
+package fi.oph.kitu.yki.html
+
+import fi.oph.kitu.html.error
+import kotlinx.html.FlowContent
+import kotlinx.html.a
+import kotlinx.html.br
+
+fun FlowContent.errorsArticle(
+    errorsCount: Long,
+    errorPage: String,
+) {
+    if (errorsCount > 0) {
+        error("Järjestelmässä on $errorsCount virhettä.") {
+            br()
+            a(errorPage) {
+                +"Katso virheet"
+            }
+        }
+    }
+}
--- server/src/main/kotlin/fi/oph/kitu/yki/arvioijat/YkiArvioijaPage.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/arvioijat/YkiArvioijaPage.kt
@@ -1,6 +1,21 @@
 package fi.oph.kitu.yki.arvioijat
 
+import fi.oph.kitu.SortDirection
+import fi.oph.kitu.html.Navigation
+import fi.oph.kitu.html.Page
+import fi.oph.kitu.yki.html.errorsArticle
+import kotlin.String
+
 object YkiArvioijaPage {
-    fun render() {
-    }
+    fun render(
+        arvioijat: List<YkiArvioijaEntity>,
+        sortColumn: YkiArvioijaColumn,
+        sortDirection: SortDirection,
+        errorsCount: Long,
+    ): String =
+        Page.renderHtml(
+            Navigation.getBreadcrumbs("/yki/arvioijat"),
+        ) {
+            this.errorsArticle(errorsCount, "/yki/arvioijat/virheet")
+        }
 }
