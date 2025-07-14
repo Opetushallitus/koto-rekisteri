392e1d8f563903981ef9146db26bdbe9d4240402
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-07-14 14:32:28 +0300

    refactor koto-error to kotlinx

--- server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiViewController.kt
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiViewController.kt
@@ -1,13 +1,10 @@
 package fi.oph.kitu.kotoutumiskoulutus
 
 import fi.oph.kitu.SortDirection
-import fi.oph.kitu.generateHeader
-import io.opentelemetry.instrumentation.annotations.WithSpan
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseBody
-import org.springframework.web.servlet.ModelAndView
 
 @Controller
 @RequestMapping("koto-kielitesti", produces = ["text/html"])
@@ -32,14 +29,14 @@ class KielitestiViewController(
         )
 
     @GetMapping("/suoritukset/virheet")
-    @WithSpan
+    @ResponseBody
     fun virheetView(
         sortColumn: KielitestiSuoritusErrorColumn = KielitestiSuoritusErrorColumn.VirheenLuontiaika,
         sortDirection: SortDirection = SortDirection.DESC,
-    ): ModelAndView =
-        ModelAndView("koto-kielitesti-virheet")
-            .addObject("header", generateHeader<KielitestiSuoritusErrorColumn>(sortColumn, sortDirection))
-            .addObject("sortColumn", sortColumn.urlParam)
-            .addObject("sortDirection", sortDirection)
-            .addObject("virheet", suoritusService.getErrors(sortColumn, sortDirection))
+    ): String =
+        KielitestiSuoritusErrorPage.render(
+            sortColumn = sortColumn,
+            sortDirection = sortDirection,
+            errors = suoritusService.getErrors(sortColumn, sortDirection),
+        )
 }
