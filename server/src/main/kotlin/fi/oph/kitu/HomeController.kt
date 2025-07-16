65495cbb5ba16a7e0d568ca78e9dbdee169dc219
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-07-16 11:54:20 +0300

    replace 404 and generic error page with a single error page

--- server/src/main/kotlin/fi/oph/kitu/HomeController.kt
+++ server/src/main/kotlin/fi/oph/kitu/HomeController.kt
@@ -3,12 +3,16 @@ package fi.oph.kitu
 import fi.oph.kitu.html.Navigation.mainNavigation
 import fi.oph.kitu.html.Page
 import fi.oph.kitu.html.testId
+import jakarta.servlet.RequestDispatcher
+import jakarta.servlet.http.HttpServletRequest
 import kotlinx.html.a
 import kotlinx.html.h1
 import kotlinx.html.h2
 import kotlinx.html.li
+import kotlinx.html.p
 import kotlinx.html.section
 import kotlinx.html.ul
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ResponseBody
@@ -32,4 +36,18 @@ class HomeController {
                 }
             }
         }
+
+    @GetMapping("/error", produces = ["text/html"])
+    @ResponseBody
+    fun error(request: HttpServletRequest): String =
+        Page.renderHtml(breadcrumbs = emptyList()) {
+            h1 { +"Kielitutkintorekisteri" }
+            p {
+                val status = request.getStatusCode()
+                +"${request.requestURL} ${status.value()} ${status.reasonPhrase}"
+            }
+        }
+
+    fun HttpServletRequest.getStatusCode() =
+        HttpStatus.valueOf(this.getAttribute(RequestDispatcher.ERROR_STATUS_CODE) as Int)
 }
