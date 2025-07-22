1f60ae581e7955fcc670713aadbd629807f29d74
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2025-07-22 14:42:21 +0300

    Fix links to controllers, again

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiApiController.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiApiController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.io.ByteArrayInputStream
 
 @RestController
-@RequestMapping("yki/api")
+@RequestMapping("/yki/api")
 class YkiApiController(
     private val service: YkiService,
 ) {
