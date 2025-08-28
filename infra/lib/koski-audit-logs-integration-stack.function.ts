ad5f60767a1edf96e8e4333d85d3f563e5b9b2f6
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-08-28 16:46:05 +0300

    send data to koski/dev

--- infra/lib/koski-audit-logs-integration-stack.function.ts
+++ infra/lib/koski-audit-logs-integration-stack.function.ts
@@ -1,15 +1,32 @@
 import { gunzipSync } from "node:zlib"
 import { CloudWatchLogsEvent } from "aws-lambda"
+import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs"
 
 export const handler = async (event: CloudWatchLogsEvent) => {
   const payload = Buffer.from(event.awslogs.data, "base64")
   const decompressed = gunzipSync(payload)
   const { logEvents } = JSON.parse(decompressed.toString())
 
-  // TODO: Send to SQS
+  const client = new SQSClient({ region: "REGION" })
+  const command = new SendMessageCommand({
+    QueueUrl:
+      // TODO: Get the queue url from Parameter store
+      "https://sqs.eu-west-1.amazonaws.com/500150530292/oma-opintopolku-loki-audit-queue",
+    MessageBody: logEvents,
+  })
 
-  return {
-    statusCode: 200,
-    message: "ok",
+  try {
+    const data = await client.send(command)
+    return {
+      statusCode: 200,
+      message: "ok",
+      data,
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      message: "unknown error",
+      error,
+    }
   }
 }
