b41cdadd737ce4d04e08e8adcd901efae4dca5c8
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-08-29 13:41:52 +0300

    get queue url from parameter store

--- infra/lib/koski-audit-logs-integration-stack.ts
+++ infra/lib/koski-audit-logs-integration-stack.ts
@@ -7,9 +7,14 @@ import {
 } from "aws-cdk-lib/aws-logs"
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs"
 import { Runtime } from "aws-cdk-lib/aws-lambda"
+import { StringParameter } from "aws-cdk-lib/aws-ssm"
 
 export interface KoskiAuditLogsIntegrationStackProps extends StackProps {
   serviceAuditLogGroup: LogGroup
+  koski: {
+    region: string
+    account: string
+  }
 }
 
 export class KoskiAuditLogsIntegrationStack extends Stack {
@@ -19,17 +24,31 @@ export class KoskiAuditLogsIntegrationStack extends Stack {
     props: KoskiAuditLogsIntegrationStackProps,
   ) {
     super(scope, id, props)
+    const { serviceAuditLogGroup, koski } = props
+
     const sendAuditLogsToKoskiLambda = new NodejsFunction(this, "function", {
       runtime: Runtime.NODEJS_LATEST,
     })
 
     new SubscriptionFilter(this, "sendAuditLogsToKoskiSubscriptionFilter", {
-      logGroup: props.serviceAuditLogGroup,
+      logGroup: serviceAuditLogGroup,
       filterName: "sendAuditLogsToKoski",
       filterPattern: FilterPattern.allEvents(),
       destination: new aws_logs_destinations.LambdaDestination(
         sendAuditLogsToKoskiLambda,
       ),
     })
+
+    const auditQueueParam = new StringParameter(
+      this,
+      "KoskiAuditLogsIntegrationParams",
+      {
+        parameterName:
+          "/kitu/koski-integration/oma-opintopolku-loki-audit-queue",
+        stringValue: `https://sqs.${koski.region}.amazonaws.com/${koski.account}/oma-opintopolku-loki-audit-queue`,
+      },
+    )
+
+    auditQueueParam.grantRead(sendAuditLogsToKoskiLambda)
   }
 }
