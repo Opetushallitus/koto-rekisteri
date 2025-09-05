c9127e86c1c69cbf002a68f12fdd478c28341850
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-09-05 17:02:19 +0300

    add logging to sts

--- infra/lib/koski-audit-logs-integration/assumeRole.ts
+++ infra/lib/koski-audit-logs-integration/assumeRole.ts
@@ -9,6 +9,34 @@ export const getAssumeRole = async (
 ): Promise<AssumeRoleCommandOutput> => {
   const RoleArn = `arn:aws:iam::${koskiAccountId}:role/kitu-sqs-sender`
   const RoleSessionName = "OmaOpintopolkuLokiLambdaSession"
+  console.log(
+    `trying to get role with RoleArn: ${RoleArn} and RoleSessionName: ${RoleSessionName}`,
+  )
+
   const sts = new STSClient({ region })
-  return await sts.send(new AssumeRoleCommand({ RoleArn, RoleSessionName }))
+  console.log("sts client created")
+
+  const output = await sts.send(
+    new AssumeRoleCommand({ RoleArn, RoleSessionName }),
+  )
+  console.log("sts received:", {
+    AssumeRoleUser: {
+      Arn: output.AssumedRoleUser?.Arn,
+      AssumedRoleId: output.AssumedRoleUser?.AssumedRoleId,
+    },
+    Credentials: {
+      $metadata: output.$metadata,
+      "AccessKeyId-first3": output.Credentials?.AccessKeyId?.substring(0, 3),
+      "SecretAccessKey-first3": output.Credentials?.SecretAccessKey?.substring(
+        0,
+        3,
+      ),
+      Expiration: output.Credentials?.Expiration,
+      "SessionToken-first3": output.Credentials?.SessionToken?.substring(0, 3),
+    },
+    SourceIdentity: output.SourceIdentity,
+    PackedPolicySize: output.PackedPolicySize,
+  })
+
+  return output
 }
