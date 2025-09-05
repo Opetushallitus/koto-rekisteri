d28c0ccf45a2d7b45a5a1910accd3b48b7ae52ca
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-09-05 13:35:19 +0300

    wip: send correct data to koski

--- infra/lib/koski-audit-logs-integration/parser.ts
+++ infra/lib/koski-audit-logs-integration/parser.ts
@@ -1,8 +1,15 @@
 import { CloudWatchLogsEvent } from "aws-lambda"
 import { gunzipSync } from "zlib"
 
-export const parse = (event: CloudWatchLogsEvent) => {
+export const parse = (event: CloudWatchLogsEvent): CloudWatchData => {
   const decoded = Buffer.from(event.awslogs.data, "base64")
   const decompressed = gunzipSync(decoded)
   return JSON.parse(decompressed.toString())
+}
+
+export const getAuditLogEntry = (data: CloudWatchData): AuditLogEntry[] => {
+  return data.logEvents.map((logEvent) => {
+    const kituLogEvent = JSON.parse(logEvent.message) as KituLogEvent
+    return JSON.parse(kituLogEvent.message) as AuditLogEntry
+  })
 }
