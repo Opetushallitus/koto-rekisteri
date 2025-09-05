5ec418b0ee3af320b1c9e28c106f4e624132c030
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-09-05 12:12:26 +0300

    send data to koski/dev

--- infra/lib/koski-audit-logs-integration/handler.ts
+++ infra/lib/koski-audit-logs-integration/handler.ts
@@ -1,12 +1,30 @@
 import { CloudWatchLogsEvent } from "aws-lambda"
 import { parse } from "./parser"
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs"
 
 export const handler = async (event: CloudWatchLogsEvent) => {
   const { logEvents } = parse(event)
-  // TODO: Send to SQS
 
-  return {
-    statusCode: 200,
-    message: "ok",
+  const sqs = new SQSClient({ region: "eu-west-1" })
+  const command = new SendMessageCommand({
+    QueueUrl:
+      // TODO: Get the queue url from Parameter store
+      "https://sqs.eu-west-1.amazonaws.com/500150530292/oma-opintopolku-loki-audit-queue",
+    MessageBody: logEvents,
+  })
+
+  try {
+    const data = await sqs.send(command)
+    return {
+      statusCode: 200,
+      message: "ok",
+      data,
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      message: "unknown error",
+      error,
+    }
   }
 }
