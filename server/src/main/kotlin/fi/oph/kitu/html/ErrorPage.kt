471b4a09e07b4092acfe0f0b4097d4307f6edb4c
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-09-11 12:06:38 +0300

    Not found- ja bad request -virhesivut

--- server/src/main/kotlin/fi/oph/kitu/html/ErrorPage.kt
+++ server/src/main/kotlin/fi/oph/kitu/html/ErrorPage.kt
@@ -1,39 +1,77 @@
 package fi.oph.kitu.html
 
+import kotlinx.html.FlowContent
 import kotlinx.html.a
 import kotlinx.html.h1
 import kotlinx.html.p
 import kotlinx.html.pre
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 
 object ErrorPage {
-    fun render(
+    fun error(
         error: Throwable,
         traceId: String?,
         traceUrl: String?,
         isLocal: Boolean,
-    ): String =
-        Page.renderHtml(emptyList()) {
-            h1 {
-                if (isLocal) {
-                    +error.toString()
-                } else {
-                    +"Internal server error"
-                }
-            }
-
-            traceId?.let {
-                p {
-                    +"Trace ID: "
-                    if (traceUrl != null) {
-                        a(href = traceUrl, target = "_blank") { +it }
+    ): ResponseEntity<String> =
+        ResponseEntity(
+            Page.renderHtml {
+                h1 {
+                    if (isLocal) {
+                        +error.toString()
                     } else {
-                        +it
+                        +"Internal server error"
                     }
                 }
-            }
 
-            if (isLocal) {
-                pre { +error.stackTraceToString() }
+                traceInfo(traceId, traceUrl)
+
+                if (isLocal) {
+                    pre { +error.stackTraceToString() }
+                }
+            },
+            HttpStatus.INTERNAL_SERVER_ERROR,
+        )
+
+    fun notFound(
+        traceId: String?,
+        traceUrl: String?,
+    ): ResponseEntity<String> =
+        ResponseEntity(
+            Page.renderHtml {
+                h1 { +"Sivua ei löydy" }
+                traceInfo(traceId, traceUrl)
+            },
+            HttpStatus.NOT_FOUND,
+        )
+
+    fun badRequest(
+        traceId: String?,
+        traceUrl: String?,
+    ): ResponseEntity<String> =
+        ResponseEntity(
+            Page.renderHtml {
+                h1 { +"Virheellinen pyyntö" }
+                p { +"Tarkista että esimerkiksi sivun osoitteen kaikki parametrit on kirjoitettu oikein." }
+                traceInfo(traceId, traceUrl)
+            },
+            HttpStatus.BAD_REQUEST,
+        )
+
+    fun FlowContent.traceInfo(
+        traceId: String?,
+        traceUrl: String?,
+    ) {
+        traceId?.let {
+            p {
+                +"Trace ID: "
+                if (traceUrl != null) {
+                    a(href = traceUrl, target = "_blank") { +it }
+                } else {
+                    +it
+                }
             }
         }
+    }
 }
