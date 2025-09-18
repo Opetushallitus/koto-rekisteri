c3f38816ff68b369610e1cb77a5904f2f44dcae5
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2025-09-18 20:22:26 +0300

    Configure Otel resource detectors explicitly
    For reasons unknown to me, if we let Spring Boot actuator's autoconfigure take care of this, it will only load Spring's own resource detectors, and if we try to rely on the otel sdk's own autoconfiguration, well, then nothing gets loaded.

--- server/src/main/kotlin/fi/oph/kitu/observability/OpenTelemetryResourceConfiguration.kt
+++ server/src/main/kotlin/fi/oph/kitu/observability/OpenTelemetryResourceConfiguration.kt
@@ -2,44 +2,58 @@ package fi.oph.kitu.observability
 
 import fi.oph.kitu.logging.add
 import io.opentelemetry.contrib.aws.resource.EcsResourceProvider
-import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties
-import io.opentelemetry.sdk.autoconfigure.spi.internal.ConditionalResourceProvider
+import io.opentelemetry.instrumentation.resources.ContainerResourceProvider
+import io.opentelemetry.instrumentation.resources.HostIdResourceProvider
+import io.opentelemetry.instrumentation.resources.HostResourceProvider
+import io.opentelemetry.instrumentation.resources.ManifestResourceProvider
+import io.opentelemetry.instrumentation.resources.OsResourceProvider
+import io.opentelemetry.instrumentation.resources.ProcessResourceProvider
+import io.opentelemetry.instrumentation.resources.ProcessRuntimeResourceProvider
+import io.opentelemetry.instrumentation.spring.autoconfigure.internal.resources.DistroVersionResourceProvider
+import io.opentelemetry.instrumentation.spring.autoconfigure.internal.resources.SpringResourceProvider
+import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider
 import io.opentelemetry.sdk.resources.Resource
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import java.util.Optional
 
 @Configuration
 class OpenTelemetryResourceConfiguration {
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
+    // Specify resource providers explicitly instead of depending on OpenTelemetry's or Spring Boot actuator's autoconfiguration. The latter only detects its own resource providers and I can't get the former to do anything (possibly because Spring overrides it).
     @Bean
-    fun ecsResourceProvider(): ConditionalResourceProvider =
-        object : ConditionalResourceProvider {
-            private val ecsResourceProvider =
-                EcsResourceProvider().also { provider ->
-                    logger.atInfo().log("EcsResourceProvider loaded")
-                }
+    fun resourceProviders(buildProperties: Optional<BuildProperties>): Set<ResourceProvider> =
+        setOf(
+            EcsResourceProvider(),
+            SpringResourceProvider(buildProperties),
+            DistroVersionResourceProvider(),
+            ProcessRuntimeResourceProvider(),
+            HostResourceProvider(),
+            HostIdResourceProvider(),
+            OsResourceProvider(),
+            ProcessResourceProvider(),
+            ContainerResourceProvider(),
+            ManifestResourceProvider(),
+        )
+}
 
-            override fun createResource(config: ConfigProperties): Resource? =
-                ecsResourceProvider.createResource(config).also { resource ->
-                    logger.atInfo().add("attributes" to resource.attributes).log("EcsResourceProvider created")
-                }
+@Configuration
+class OpenTelemetryListener(
+    resourceProviders: Set<ResourceProvider>,
+    resource: Resource,
+) {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
-            override fun shouldApply(
-                config: ConfigProperties,
-                existing: Resource,
-            ): Boolean =
-                ecsResourceProvider.shouldApply(config, existing).let { result ->
-                    logger
-                        .atInfo()
-                        .add(
-                            "shouldApply" to result,
-                            "config" to config,
-                            "existing" to existing.attributes,
-                        ).log("EcsResourceProvider shouldApply")
-                    return true // override shouldApply
-                }
-        }
+    init {
+        logger
+            .atInfo()
+            .add(
+                "providers" to resourceProviders,
+                "resource" to resource.attributes,
+            ).log("Initialized OpenTelemetry resource")
+    }
 }
