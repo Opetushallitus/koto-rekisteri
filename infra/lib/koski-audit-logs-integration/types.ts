bd92a93544874737b4cafea39a68b436feb114d6
Saku K <6057704+saku-koodari@users.noreply.github.com>
2025-09-25 12:29:03 +0300

    simplify and correct types on infra folder

--- infra/lib/koski-audit-logs-integration/types.ts
+++ infra/lib/koski-audit-logs-integration/types.ts
@@ -1,46 +1,29 @@
-// Our audit logs are on this format
 type CloudWatchData = {
-  messageType: string
-  owner: string
-  logGroup: string
-  logStream: string
-  subscriptionFilters: string[]
   logEvents: {
-    id: string
-    timestamp: number
-    message: string
+    message: string // JSON containing KituLogEvent
   }[]
 }
-// CloudWatchData.logEvents.message
+
 type KituLogEvent = {
-  "@timestamp": string // "2025-09-01T07:19:33.694326330Z",
-  log: any // { "level":"INFO", "logger":"auditLogger" },
-  process: any // { "pid":1, "thread":{ "name":"HEARTBEAT-0" } },
-  service: any // { "name":"kitu", "version":"0.0.1-SNAPSHOT", "node":{} },
-  message: string
-  ecs: any // {"version":"8.11"}}
+  message: string // JSON containing AuditLogEntry
 }
 
-// The data we are trying to send to koski
 type AuditLogEntry = {
   version: number
   logSeq: number
-  type: string
-  // TODO: environment
   bootTime: string
+  type: string
+  environment: string
   hostname: string
   timestamp: string
   serviceName: string
   applicationType: string
   user: {
     oid: string
-    ip: string
-    session: string
-    userAgent: string
   }
-  operation: "KielitestiSuoritusViewed"
-  target: { oppijaHenkiloOid: "1.2.246.562.198.88975028874" }
-  // TODO: organizationOid
-  // TODO: operation
-  changes: []
+  target: {
+    oppijaHenkiloOid: string
+  }
+  organizationOid: string
+  operation: string
 }
