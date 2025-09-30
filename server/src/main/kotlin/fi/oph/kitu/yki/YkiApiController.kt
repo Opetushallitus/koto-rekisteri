2634e89fab908f312bee4244002d4077b02bdf5c
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2025-09-30 16:21:50 +0300

    Add new API endpoint

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiApiController.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiApiController.kt
@@ -1,10 +1,12 @@
 package fi.oph.kitu.yki
 
+import fi.oph.kitu.Oid
 import org.springframework.core.io.InputStreamResource
 import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -15,6 +17,17 @@ import java.io.ByteArrayInputStream
 class YkiApiController(
     private val service: YkiService,
 ) {
+    data class AddSuorituksetRequest(
+        val suoritukset: List<Suoritus>,
+    ) {
+        data class Suoritus(
+            val suorittajanOppijanumero: Oid,
+        )
+    }
+
+    @PostMapping("/suoritukset")
+    fun addSuoritukset(suoritukset: AddSuorituksetRequest): ResponseEntity<String> = ResponseEntity.ok("ok man")
+
     @GetMapping("/suoritukset", produces = ["text/csv"])
     fun getSuorituksetAsCsv(
         @RequestParam("includeVersionHistory", required = false) includeVersionHistory: Boolean?,
