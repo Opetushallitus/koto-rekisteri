127961e0f9cae284d1ac8bec286661eb0566afac
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-10-07 12:27:16 +0300

    Korjaa arvosanan/poiston asettaminen, jos osakokeita on vain yksi

---
+++ server/src/main/kotlin/fi/oph/kitu/List.kt
@@ -0,0 +1,11 @@
+package fi.oph.kitu
+
+inline fun <reified T> List<T>.growToSize(
+    expectedSize: Int,
+    value: T,
+): List<T> =
+    if (expectedSize > size) {
+        this + List(expectedSize - size) { value }
+    } else {
+        this
+    }
