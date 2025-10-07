3c2205a5c4141b5232749915bfc0b0fd0a3533b0
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-10-07 14:47:04 +0300

    Tallenna yki-suoritus tietokantaan

--- server/src/main/kotlin/fi/oph/kitu/yki/YkiTiedonsiirtoController.kt
+++ server/src/main/kotlin/fi/oph/kitu/yki/YkiTiedonsiirtoController.kt
@@ -1,7 +1,10 @@
 package fi.oph.kitu.yki
 
 import fi.oph.kitu.tiedonsiirtoschema.Henkilosuoritus
+import fi.oph.kitu.validation.ValidationService
 import fi.oph.kitu.yki.suoritukset.YkiSuoritus
+import fi.oph.kitu.yki.suoritukset.YkiSuoritusEntity
+import fi.oph.kitu.yki.suoritukset.YkiSuoritusRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,12 +13,20 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/yki")
-class YkiTiedonsiirtoController {
+class YkiTiedonsiirtoController(
+    private val validationService: ValidationService,
+    private val ykiSuoritusRepository: YkiSuoritusRepository,
+) {
     @PutMapping("/solki")
     fun putHenkilosuoritus(
         @RequestBody json: String,
     ): ResponseEntity<*> =
         Henkilosuoritus.deserializationAtEndpoint<YkiSuoritus>(json) { data ->
-            // TODO: Tallenna data
+            val enrichedSuoritus = validationService.validateAndEnrich(data.suoritus).getOrThrow()
+            val henkilosuoritus = Henkilosuoritus(data.henkilo, enrichedSuoritus)
+            val entity =
+                henkilosuoritus.toEntity<YkiSuoritusEntity>()
+                    ?: throw RuntimeException("Failed to convert HenkiloSuoritus to YkiSuoritusEntity")
+            ykiSuoritusRepository.save(entity)
         }
 }
