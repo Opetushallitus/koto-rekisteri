6ab17446c3b3a123f20e1de9c93ddbb679c9930c
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-10-14 13:02:51 +0300

    Luonnos yki-arvioijan tiedonsiirtotietomallista

---
+++ server/src/main/kotlin/fi/oph/kitu/yki/arvioijat/YkiArvioija.kt
@@ -0,0 +1,46 @@
+package fi.oph.kitu.yki.arvioijat
+
+import fi.oph.kitu.Oid
+import fi.oph.kitu.yki.Tutkintokieli
+import fi.oph.kitu.yki.Tutkintotaso
+import java.time.LocalDate
+import java.time.OffsetDateTime
+
+data class YkiArvioija(
+    val arvioijaOid: Oid,
+    val henkilotunnus: String,
+    val sukunimi: String,
+    val etunimet: String,
+    val sahkopostiosoite: String?,
+    val katuosoite: String,
+    val postinumero: String,
+    val postitoimipaikka: String,
+    val ensimmainenRekisterointipaiva: LocalDate,
+    val kaudenAlkupaiva: LocalDate?,
+    val kaudenPaattymispaiva: LocalDate?,
+    val jatkorekisterointi: Boolean,
+    val tila: YkiArvioijaTila,
+    val kieli: Tutkintokieli,
+    val tasot: Set<Tutkintotaso>,
+) {
+    fun toEntity(): YkiArvioijaEntity =
+        YkiArvioijaEntity(
+            id = null,
+            rekisteriintuontiaika = OffsetDateTime.now(),
+            arvioijanOppijanumero = arvioijaOid.toString(),
+            henkilotunnus = henkilotunnus,
+            sukunimi = sukunimi,
+            etunimet = etunimet,
+            sahkopostiosoite = sahkopostiosoite,
+            katuosoite = katuosoite,
+            postinumero = postinumero,
+            postitoimipaikka = postitoimipaikka,
+            ensimmainenRekisterointipaiva = ensimmainenRekisterointipaiva,
+            kaudenAlkupaiva = kaudenAlkupaiva,
+            kaudenPaattymispaiva = kaudenPaattymispaiva,
+            jatkorekisterointi = jatkorekisterointi,
+            tila = tila,
+            kieli = kieli,
+            tasot = tasot,
+        )
+}
