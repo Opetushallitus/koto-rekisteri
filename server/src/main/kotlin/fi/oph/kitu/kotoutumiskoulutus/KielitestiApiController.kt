e5199d368f3094e73793d09e73a63b67624e280b
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-10-21 10:29:36 +0300

    Koto-kielitestien tietojen palautus csv-muodossa

---
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiApiController.kt
@@ -0,0 +1,36 @@
+package fi.oph.kitu.kotoutumiskoulutus
+
+import fi.oph.kitu.i18n.isoDate
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.core.io.InputStreamResource
+import org.springframework.core.io.Resource
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.io.ByteArrayInputStream
+import java.time.LocalDate
+
+@RestController
+@RequestMapping("/koto-kielitesti/api")
+@Tag(name = "Kotoutumiskoulutuksen kielitesti, sisäiset rajapinnat")
+class KielitestiApiController(
+    private val service: KoealustaService,
+) {
+    @GetMapping("/suoritukset", produces = ["text/csv"])
+    fun getSuorituksetAsCsv(): ResponseEntity<Resource> =
+        ResponseEntity
+            .ok()
+            .contentType(MediaType.parseMediaType("text/csv"))
+            .header("Content-Disposition", "attachment; filename=koto-suoritukset-${LocalDate.now().isoDate()}.csv")
+            .body(
+                InputStreamResource(
+                    ByteArrayInputStream(
+                        service
+                            .generateSuorituksetCsvStream()
+                            .toByteArray(),
+                    ),
+                ),
+            )
+}
