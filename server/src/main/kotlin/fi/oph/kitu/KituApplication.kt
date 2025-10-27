63413b5a994b1fc76435a07b5c2e848c47685cac
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2025-10-27 13:45:32 +0200

    Enable Spring security annotations

--- server/src/main/kotlin/fi/oph/kitu/KituApplication.kt
+++ server/src/main/kotlin/fi/oph/kitu/KituApplication.kt
@@ -3,9 +3,11 @@ package fi.oph.kitu
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 
 @SpringBootApplication
 @EnableAsync
+@EnableMethodSecurity
 class KituApplication
 
 fun main(args: Array<String>) {
