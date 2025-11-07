51334a456e2a6a822ab8bda9b26ab7870e3096b4
Ilmari Vacklin <ilmari.vacklin@reaktor.com>
2025-11-07 18:05:07 +0200

    Add trace ID to all responses

---
+++ server/src/main/kotlin/fi/oph/kitu/observability/OpenTelemetryTraceIdControllerAdvice.kt
@@ -0,0 +1,31 @@
+package fi.oph.kitu.observability
+
+import io.opentelemetry.api.trace.Span
+import org.springframework.core.MethodParameter
+import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+
+@ControllerAdvice
+class OpenTelemetryTraceIdAdvice : ResponseBodyAdvice<Any> {
+    public override fun supports(
+        returnType: MethodParameter,
+        converterType: Class<out HttpMessageConverter<*>>,
+    ): Boolean = true
+
+    public override fun beforeBodyWrite(
+        body: Any?,
+        returnType: MethodParameter,
+        selectedContentType: MediaType,
+        selectedConverterType: Class<out HttpMessageConverter<*>>,
+        request: ServerHttpRequest,
+        response: ServerHttpResponse,
+    ): Any? {
+        val currentSpan = Span.current()
+        response.headers.set("X-Trace-Id", currentSpan.spanContext.traceId)
+        return body
+    }
+}
