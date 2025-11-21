9cef43ccf63f9063c8dc7b12fbf0cf9538e295a6
Tintti Rahikainen <tintti.rahikainen@reaktor.com>
2025-11-21 15:13:08 +0200

    Add custom OAuth2AuthorizedClientManager for scheduled tasks to work

--- server/src/main/kotlin/fi/oph/kitu/oauth2client/OAuth2RestClientConfiguration.kt
+++ server/src/main/kotlin/fi/oph/kitu/oauth2client/OAuth2RestClientConfiguration.kt
@@ -4,7 +4,11 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
 import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor
 import org.springframework.web.client.RestClient
 
@@ -13,6 +17,26 @@ import org.springframework.web.client.RestClient
 class OAuth2RestClientConfiguration {
     @Value("\${kitu.palvelukayttaja.callerid}")
     private lateinit var callerId: String
+
+    @Bean
+    fun authorizedClientManager(
+        clientRegistrationRepository: ClientRegistrationRepository,
+        clientService: OAuth2AuthorizedClientService,
+    ): OAuth2AuthorizedClientManager {
+        val authorizedClientProvider =
+            OAuth2AuthorizedClientProviderBuilder
+                .builder()
+                .refreshToken()
+                .clientCredentials()
+                .build()
+
+        val authorizedClientManager =
+            AuthorizedClientServiceOAuth2AuthorizedClientManager(clientRegistrationRepository, clientService)
+
+        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
+
+        return authorizedClientManager
+    }
 
     @Bean
     fun oauth2RestClient(
