b29be06232b61395c47f69dfc058a5e96ca2017f
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-12-02 10:50:00 +0200

    Algoritmi erilaisten etunimi-sukunimikombinaatioiden generointiin

--- server/src/main/kotlin/fi/oph/kitu/List.kt
+++ server/src/main/kotlin/fi/oph/kitu/List.kt
@@ -11,3 +11,7 @@ inline fun <reified T> List<T>.growToSize(
     }
 
 inline fun <reified T> List<T>.intersects(other: List<T>): Boolean = intersect(other).isNotEmpty()
+
+fun <T> List<T>.removeAtIndex(index: Int): List<T> = this.filterIndexed { i, _ -> i != index }
+
+fun <T> List<T>.splitAt(index: Int): Pair<List<T>, List<T>> = this.subList(0, index) to this.subList(index, this.size)
