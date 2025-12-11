661d383425d288b0d49c218c1aa30df30b2f8589
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2025-12-11 09:44:38 +0200

    Refaktoroi organisaatioiden nimien näyttäminen osaksi KielitestiSuoritusErrorColumn-luokkaa

--- server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiSuoritusErrorColumn.kt
+++ server/src/main/kotlin/fi/oph/kitu/kotoutumiskoulutus/KielitestiSuoritusErrorColumn.kt
@@ -1,27 +1,33 @@
 package fi.oph.kitu.kotoutumiskoulutus
 
+import fi.oph.kitu.Oid
 import fi.oph.kitu.html.DisplayTableEnum
 import fi.oph.kitu.html.json
+import fi.oph.kitu.i18n.LocalizedString
 import fi.oph.kitu.i18n.finnishDateTimeUTC
 import fi.oph.kitu.toJsonNode
 import kotlinx.html.FlowContent
 import kotlinx.html.details
+import kotlinx.html.div
+import kotlinx.html.small
 import kotlinx.html.summary
 
 enum class KielitestiSuoritusErrorColumn(
     override val entityName: String,
     override val uiHeaderValue: String,
     override val urlParam: String,
-    val renderValue: FlowContent.(KielitestiSuoritusError) -> Unit,
+    val renderValue: (Map<Oid, LocalizedString>) -> FlowContent.(KielitestiSuoritusError) -> Unit,
 ) : DisplayTableEnum {
     Henkilötunnus(
         entityName = "hetu",
         uiHeaderValue = "Henkilötunnus",
         urlParam = "henkilötunnus",
         renderValue = {
-            if (!it.hetu.isNullOrEmpty()) {
-                attributes["headers"] = "hetu"
-                +it.hetu
+            {
+                if (!it.hetu.isNullOrEmpty()) {
+                    attributes["headers"] = "hetu"
+                    +it.hetu
+                }
             }
         },
     ),
@@ -30,17 +36,28 @@ enum class KielitestiSuoritusErrorColumn(
         uiHeaderValue = "Nimi",
         urlParam = "nimi",
         renderValue = {
-            attributes["headers"] = "nimi"
-            +it.nimi
+            {
+                attributes["headers"] = "nimi"
+                +it.nimi
+            }
         },
     ),
     SchoolOid(
         entityName = "schoolOid",
         uiHeaderValue = "Organisaatio",
         urlParam = "schooloid",
-        renderValue = {
-            attributes["headers"] = "schoolOid"
-            +it.schoolOid?.toString().orEmpty()
+        renderValue = { organisaatioidenNimet ->
+            {
+                it.schoolOid?.let { oid ->
+                    organisaatioidenNimet[oid]?.let { name ->
+                        div { +name.toString() }
+                    }
+                    small {
+                        attributes["headers"] = "schoolOid"
+                        +it.schoolOid.toString()
+                    }
+                }
+            }
         },
     ),
     TeacherEmail(
@@ -48,8 +65,10 @@ enum class KielitestiSuoritusErrorColumn(
         uiHeaderValue = "Opettajan sähköpostiosoite",
         urlParam = "teacheremail",
         renderValue = {
-            attributes["headers"] = "teacherEmail"
-            +it.teacherEmail.orEmpty()
+            {
+                attributes["headers"] = "teacherEmail"
+                +it.teacherEmail.orEmpty()
+            }
         },
     ),
     VirheenLuontiaika(
@@ -57,8 +76,10 @@ enum class KielitestiSuoritusErrorColumn(
         uiHeaderValue = "Virheen luontiaika",
         urlParam = "virheenluontiaika",
         renderValue = {
-            attributes["headers"] = "virheenLuontiaika"
-            +it.virheenLuontiaika.finnishDateTimeUTC()
+            {
+                attributes["headers"] = "virheenLuontiaika"
+                +it.virheenLuontiaika.finnishDateTimeUTC()
+            }
         },
     ),
     Viesti(
@@ -66,14 +87,16 @@ enum class KielitestiSuoritusErrorColumn(
         uiHeaderValue = "Virheviesti",
         urlParam = "viesti",
         renderValue = {
-            attributes["headers"] = "viesti"
-            if (it.lisatietoja != null) {
-                details {
-                    summary { +it.viesti }
-                    json(it.lisatietoja.toJsonNode())
+            {
+                attributes["headers"] = "viesti"
+                if (it.lisatietoja != null) {
+                    details {
+                        summary { +it.viesti }
+                        json(it.lisatietoja.toJsonNode())
+                    }
+                } else {
+                    +it.viesti
                 }
-            } else {
-                +it.viesti
             }
         },
     ),
@@ -82,8 +105,10 @@ enum class KielitestiSuoritusErrorColumn(
         uiHeaderValue = "Ratkaisuehdotus",
         urlParam = "onrLisatietoja",
         renderValue = {
-            attributes["headers"] = "ratkaisuehdotus"
-            +it.onrLisatietoja.orEmpty()
+            {
+                attributes["headers"] = "ratkaisuehdotus"
+                +it.onrLisatietoja.orEmpty()
+            }
         },
     ),
     VirheellinenKentta(
@@ -91,8 +116,10 @@ enum class KielitestiSuoritusErrorColumn(
         uiHeaderValue = "Virheellinen kenttä",
         urlParam = "virheellinenkentta",
         renderValue = {
-            attributes["headers"] = "virheellinenKentta"
-            +it.virheellinenKentta.orEmpty()
+            {
+                attributes["headers"] = "virheellinenKentta"
+                +it.virheellinenKentta.orEmpty()
+            }
         },
     ),
     VirheellinenArvo(
@@ -100,8 +127,10 @@ enum class KielitestiSuoritusErrorColumn(
         uiHeaderValue = "Virheellinen arvo",
         urlParam = "virheellinenarvo",
         renderValue = {
-            attributes["headers"] = "virheellinenArvo"
-            +it.virheellinenArvo.orEmpty()
+            {
+                attributes["headers"] = "virheellinenArvo"
+                +it.virheellinenArvo.orEmpty()
+            }
         },
     ),
 }
