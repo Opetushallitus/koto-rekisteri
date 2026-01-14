06852d973929f41d637a24a0e5082694be1e8b4b
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2026-01-14 11:08:26 +0200

    Poista käännetty tietokantadokumentaatio

--- scripts/check-formatting.sh
+++ scripts/check-formatting.sh
@@ -2,9 +2,6 @@
 
 set -eux
 
-prettier --check e2e
-prettier --check infra
-prettier --check scripts
-prettier --check server
+prettier --check .
 (cd server && ktlint)
 shellcheck -P ./scripts ./scripts/*.sh
