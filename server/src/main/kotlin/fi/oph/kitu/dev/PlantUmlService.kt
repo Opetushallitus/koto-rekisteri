7d459a86192547ac4a2b270a32d4f894c6f0ec04
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2026-01-19 09:16:52 +0200

    Hio kaavioista siistempiä

--- server/src/main/kotlin/fi/oph/kitu/dev/PlantUmlService.kt
+++ server/src/main/kotlin/fi/oph/kitu/dev/PlantUmlService.kt
@@ -1,7 +1,13 @@
 package fi.oph.kitu.dev
 
+import org.springframework.beans.factory.annotation.AnnotatedBeanDefinition
+import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.beans.factory.config.ConfigurableListableBeanFactory
+import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Controller
+import org.springframework.stereotype.Repository
 import org.springframework.stereotype.Service
+import org.springframework.web.bind.annotation.RestController
 
 @Service
 class PlantUmlService(
@@ -24,7 +30,13 @@ class PlantUmlService(
                 rows.add(umlItem.toString())
                 beanFactory.getDependenciesForBean(beanName).forEach { dep ->
                     val depName = runCatching { beanFactory.getBean(dep).javaClass.name }.getOrNull()
-                    rows.add("${umlItem.name} --> ${makeCleanName(dep, depName)}")
+                    val cleanDepName = makeCleanName(dep, depName)
+                    if (cleanDepName == umlItem.factory) {
+                        rows.add("$cleanDepName *-- ${umlItem.name}")
+                    } else {
+                        beanFactory.getUmlItem(dep)?.let { rows.add(it.toString()) }
+                        rows.add("${umlItem.name} --> $cleanDepName")
+                    }
                 }
             }
         }
@@ -32,7 +44,16 @@ class PlantUmlService(
         return if (rows.isEmpty()) {
             null
         } else {
-            (listOf("@startuml") + rows.distinct() + listOf("@enduml")).joinToString("\n")
+            (
+                listOf("@startuml") +
+                    rows.distinct() +
+                    listOf(
+                        "hide members",
+                        "hide methods",
+                        "hide <<Method>> circle",
+                        "@enduml",
+                    )
+            ).joinToString("\n")
         }
     }
 }
@@ -44,7 +65,22 @@ fun ConfigurableListableBeanFactory.getUmlItem(beanName: String): UmlItem? {
         makeCleanName(
             beanName,
             className,
-        )?.let { name -> UmlItem(name, UmlItemType.CLASS_BEAN) }
+        )?.let { name ->
+            UmlItem(
+                name,
+                if (isAnnotatedConfiguration(bd)) {
+                    UmlItemType.CONFIGURATION_BEAN
+                } else if (isAnnotatedService(bd)) {
+                    UmlItemType.SERVICE_BEAN
+                } else if (isAnnotatedRepository(bd)) {
+                    UmlItemType.REPOSITORY_BEAN
+                } else if (isAnnotatedController(bd)) {
+                    UmlItemType.CONTROLLER_BEAN
+                } else {
+                    UmlItemType.GENERIC_BEAN
+                },
+            )
+        }
     }
         ?: UmlItem(
             beanName,
@@ -61,18 +97,30 @@ data class UmlItem(
     val type: UmlItemType,
     val factory: String? = null,
 ) {
-    override fun toString(): String =
-        listOfNotNull(
-            "$type ${factory?.let { it.substringBeforeLast(".") + "." }.orEmpty()}$name",
-            factory?.let { "$it *-- $name" },
-        ).joinToString("\n")
+    fun fullName(): String {
+        val fullName =
+            factory?.let {
+                "${factory.substringBeforeLast(".")}.$name"
+            } ?: name
+        return listOfNotNull(
+            fullName,
+            type.stereotype?.let { "<<$it>>" },
+        ).joinToString(" ")
+    }
+
+    override fun toString(): String = "${type.pumlType} ${fullName()}"
 }
 
 enum class UmlItemType(
     val pumlType: String,
+    val stereotype: String? = null,
 ) {
-    CLASS_BEAN("class"),
-    METHOD_BEAN("circle"),
+    GENERIC_BEAN("class"),
+    CONFIGURATION_BEAN("class", "Configuration"),
+    SERVICE_BEAN("class", "Service"),
+    REPOSITORY_BEAN("class", "Repository"),
+    CONTROLLER_BEAN("class", "Controller"),
+    METHOD_BEAN("class", "Method"),
     ;
 
     override fun toString(): String = pumlType
@@ -84,7 +132,7 @@ fun makeCleanName(
 ): String? =
     (
         className?.let { cn ->
-            if (cn.startsWith("jdk.")) {
+            if (cn.startsWith("jdk.") || cn.startsWith("java.")) {
                 null
             } else if (cn.startsWith("fi.oph.kitu")) {
                 cn.replace("fi.oph.kitu.", "Kielitutkintorekisteri.")
@@ -94,8 +142,31 @@ fun makeCleanName(
                 "OpenTelemetry.${cn.substringAfterLast(".")}"
             } else if (cn.startsWith("com.github.kagkarlsson.scheduler")) {
                 "Scheduler.${cn.substringAfterLast(".")}"
+            } else if (cn.startsWith("com.fasterxml.jackson")) {
+                "Jackson.${cn.substringAfterLast(".")}"
+            } else if (cn.startsWith("org.apereo.cas")) {
+                "CAS.${cn.substringAfterLast(".")}"
             } else {
                 cn
             }
         } ?: name
     ).substringBefore("$$")
+
+fun isAnnotatedConfiguration(beanDefinition: BeanDefinition): Boolean =
+    beanDefinition is AnnotatedBeanDefinition &&
+        beanDefinition.metadata.hasAnnotation(Configuration::class.java.name)
+
+fun isAnnotatedService(beanDefinition: BeanDefinition): Boolean =
+    beanDefinition is AnnotatedBeanDefinition &&
+        beanDefinition.metadata.hasAnnotation(Service::class.java.name)
+
+fun isAnnotatedRepository(beanDefinition: BeanDefinition): Boolean =
+    beanDefinition is AnnotatedBeanDefinition &&
+        beanDefinition.metadata.hasAnnotation(Repository::class.java.name)
+
+fun isAnnotatedController(beanDefinition: BeanDefinition): Boolean =
+    beanDefinition is AnnotatedBeanDefinition &&
+        (
+            beanDefinition.metadata.hasAnnotation(RestController::class.java.name) ||
+                beanDefinition.metadata.hasAnnotation(Controller::class.java.name)
+        )
