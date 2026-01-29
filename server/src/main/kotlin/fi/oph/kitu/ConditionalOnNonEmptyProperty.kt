a72ad0c085c93ea6cc400f7514a221e55f4cee17
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2026-01-29 12:36:55 +0200

    Erota yki- ja vkt-siirto Koskeen omiksi skeduloinneikseen

---
+++ server/src/main/kotlin/fi/oph/kitu/ConditionalOnNonEmptyProperty.kt
@@ -0,0 +1,30 @@
+package fi.oph.kitu
+
+import org.springframework.context.annotation.Condition
+import org.springframework.context.annotation.ConditionContext
+import org.springframework.context.annotation.Conditional
+import org.springframework.core.type.AnnotatedTypeMetadata
+
+@Target(AnnotationTarget.CLASS, AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@Conditional(NonEmptyPropertyCondition::class)
+annotation class ConditionalOnNonEmptyProperty(
+    val name: String,
+)
+
+class NonEmptyPropertyCondition : Condition {
+    override fun matches(
+        context: ConditionContext,
+        metadata: AnnotatedTypeMetadata,
+    ): Boolean {
+        val attributes =
+            metadata
+                .getAnnotationAttributes(ConditionalOnNonEmptyProperty::class.java.name)
+                ?: return false
+
+        val propertyName = attributes["name"] as String
+        val value = context.environment.getProperty(propertyName)
+
+        return !value.isNullOrBlank()
+    }
+}
