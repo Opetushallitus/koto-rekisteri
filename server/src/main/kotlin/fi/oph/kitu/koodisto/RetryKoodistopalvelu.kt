c9ac1b15f6ed9d5fec60f5caaacb5ad47bbd779a
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2026-03-26 10:14:43 +0200

    Lisää uudelleenyritykset Koodistopalveluun

---
+++ server/src/main/kotlin/fi/oph/kitu/koodisto/RetryKoodistopalvelu.kt
@@ -0,0 +1,23 @@
+package fi.oph.kitu.koodisto
+
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import org.springframework.web.client.HttpServerErrorException
+import org.springframework.web.client.ResourceAccessException
+
+@Target(AnnotationTarget.FUNCTION)
+@Retention(AnnotationRetention.RUNTIME)
+@Retryable(
+    retryFor = [
+        ResourceAccessException::class,
+        HttpServerErrorException::class,
+    ],
+    maxAttempts = 3,
+    backoff =
+        Backoff(
+            delay = 1000,
+            multiplier = 2.0,
+            maxDelay = 5000,
+        ),
+)
+annotation class RetryKoodistopalvelu
