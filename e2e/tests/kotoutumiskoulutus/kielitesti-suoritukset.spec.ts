7a0cf64db173e3f2491518c9bf55522e137fcabf
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2026-04-21 13:07:32 +0300

    Korjaa testi

--- e2e/tests/kotoutumiskoulutus/kielitesti-suoritukset.spec.ts
+++ e2e/tests/kotoutumiskoulutus/kielitesti-suoritukset.spec.ts
@@ -252,22 +252,18 @@ describe("Kotoutumiskoulutuksen kielitesti -page", () => {
       await expect(suoritukset).toHaveCount(2)
     })
 
-    // test("Henkilötietojen rajaaminen suorituksilta", async ({
-    //   kielitestiSuorituksetPage,
-    // }) => {
-    //   await kielitestiSuorituksetPage.open()
-    //   const dialog = await kielitestiSuorituksetPage.openFilterDialog()
-    //   await dialog.hideHenkilotiedot(true)
-    //   await dialog.submit()
-    //
-    //   const headers = await kielitestiSuorituksetPage.page
-    //     .getByRole("columnheader")
-    //     .all()
-    //   headers.forEach((header) => {
-    //     expect(header).not.toHaveText("Sukunimi")
-    //     expect(header).not.toHaveText("Etunimet")
-    //   })
-    // })
+    test("Henkilötietojen rajaaminen suorituksilta", async ({
+      kielitestiSuorituksetPage,
+    }) => {
+      await kielitestiSuorituksetPage.open()
+      const dialog = await kielitestiSuorituksetPage.openFilterDialog()
+      await dialog.hideHenkilotiedot(true)
+      await dialog.submit()
+
+      const headers = kielitestiSuorituksetPage.page.getByRole("columnheader")
+      await expect(headers.filter({ hasText: "Sukunimi" })).toHaveCount(0)
+      await expect(headers.filter({ hasText: "Etunimet" })).toHaveCount(0)
+    })
 
     test("Henkilötietojen rajaaminen suorituksilta rajaa henkilötiedot pois csv:ltä", async ({
       page,
