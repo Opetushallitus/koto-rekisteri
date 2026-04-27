470025a923f359490dc0754bc29dd20743796afd
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2026-04-27 12:11:04 +0300

    Lisää mock koodistoon kunta-koodisto

--- server/src/main/kotlin/fi/oph/kitu/koodisto/MockKoodistoService.kt
+++ server/src/main/kotlin/fi/oph/kitu/koodisto/MockKoodistoService.kt
@@ -7,34 +7,55 @@ import org.springframework.stereotype.Service
 @Profile("test | e2e | local-opintopolku")
 class MockKoodistoService : KoodistoService {
     override fun getKoodiviitteet(koodistoUri: String): List<KoodistopalveluKoodiviite>? =
-        if (koodistoUri == "maatjavaltiot1") {
-            listOf(
-                KoodistopalveluKoodiviite(
-                    koodiUri = "maatjavaltiot1",
-                    koodiArvo = "FIN",
-                    versio = 2,
-                    metadata =
-                        listOf(
-                            KoodistopalveluKoodiviiteMetadata(
-                                nimi = "Suomi",
-                                kieli = KoodistopalveluLanguage.FI,
+        when (koodistoUri) {
+            "maatjavaltiot1" -> {
+                listOf(
+                    KoodistopalveluKoodiviite(
+                        koodiUri = "maatjavaltiot1",
+                        koodiArvo = "FIN",
+                        versio = 2,
+                        metadata =
+                            listOf(
+                                KoodistopalveluKoodiviiteMetadata(
+                                    nimi = "Suomi",
+                                    kieli = KoodistopalveluLanguage.FI,
+                                ),
                             ),
-                        ),
-                ),
-                KoodistopalveluKoodiviite(
-                    koodiUri = "maatjavaltiot1",
-                    koodiArvo = "EST",
-                    versio = 2,
-                    metadata =
-                        listOf(
-                            KoodistopalveluKoodiviiteMetadata(
-                                nimi = "Viro",
-                                kieli = KoodistopalveluLanguage.FI,
+                    ),
+                    KoodistopalveluKoodiviite(
+                        koodiUri = "maatjavaltiot1",
+                        koodiArvo = "EST",
+                        versio = 2,
+                        metadata =
+                            listOf(
+                                KoodistopalveluKoodiviiteMetadata(
+                                    nimi = "Viro",
+                                    kieli = KoodistopalveluLanguage.FI,
+                                ),
                             ),
-                        ),
-                ),
-            )
-        } else {
-            null
+                    ),
+                )
+            }
+
+            "kunta" -> {
+                listOf(
+                    KoodistopalveluKoodiviite(
+                        koodiUri = "kunta",
+                        koodiArvo = "091",
+                        versio = 2,
+                        metadata =
+                            listOf(
+                                KoodistopalveluKoodiviiteMetadata(
+                                    nimi = "Helsinki",
+                                    kieli = KoodistopalveluLanguage.FI,
+                                ),
+                            ),
+                    ),
+                )
+            }
+
+            else -> {
+                null
+            }
         }
 }
