2371104809544a2b9a7521c057e9f4cabaf57d05
Ilkka Hänninen <ilkka.hanninen@reaktor.com>
2026-06-12 10:49:14 +0300

    Tee täysi yki-poikkeamien tarkastus aina viikonloppuisin

--- server/src/test/kotlin/fi/oph/kitu/yki/AnomalyCheckLookbackTest.kt
+++ server/src/test/kotlin/fi/oph/kitu/yki/AnomalyCheckLookbackTest.kt
@@ -7,24 +7,13 @@ import kotlin.test.assertEquals
 
 class AnomalyCheckLookbackTest {
     @Test
-    fun `kuun 1 päivänä tarkistetaan koko vuoden ajalta`() {
-        assertEquals(Period.ofYears(1), anomalyCheckLookback(LocalDate.of(2026, 1, 1)))
-        assertEquals(Period.ofYears(1), anomalyCheckLookback(LocalDate.of(2026, 6, 1)))
-    }
-
-    @Test
-    fun `sunnuntaina tarkistetaan kolmen kuukauden ajalta`() {
-        assertEquals(Period.ofMonths(3), anomalyCheckLookback(LocalDate.of(2026, 6, 7)))
+    fun `lauantaina tarkistetaan  koko vuoden ajalta`() {
+        assertEquals(Period.ofYears(1), anomalyCheckLookback(LocalDate.of(2026, 6, 6)))
     }
 
     @Test
     fun `arkipäivänä tarkistetaan yhden kuukauden ajalta`() {
         assertEquals(Period.ofMonths(1), anomalyCheckLookback(LocalDate.of(2026, 6, 8)))
-        assertEquals(Period.ofMonths(1), anomalyCheckLookback(LocalDate.of(2026, 6, 13)))
-    }
-
-    @Test
-    fun `kuun 1 päivän sääntö voittaa kun se osuu sunnuntaille`() {
-        assertEquals(Period.ofYears(1), anomalyCheckLookback(LocalDate.of(2026, 3, 1)))
+        assertEquals(Period.ofMonths(1), anomalyCheckLookback(LocalDate.of(2026, 6, 14)))
     }
 }
